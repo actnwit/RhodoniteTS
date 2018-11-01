efee7901543e597aec0628ea19df79e61711f257
Yuki Shimada <emadurandal@gmail.com>
2018-11-01 09:39:36 +0900

    Add rollup settings

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import typescript from 'rollup-plugin-typescript2';
+import nodeResolve from 'rollup-plugin-node-resolve'
+
+export default {
+  input: 'src/main.ts',
+
+  plugins: [
+    nodeResolve({ jsnext: true }),
+    typescript({
+      tsconfigOverride: {
+        compilerOptions: {
+          module: "es2015",
+          moduleResolution: "node",
+        }
+      }
+    })
+  ],
+  
+  output: {
+    file: 'dist/index.mjs',
+    format: 'es',
+  }
+
+}
