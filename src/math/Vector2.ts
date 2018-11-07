e9a63cb41354aa778bed61416565378bd756977e
Yuki Shimada <emadurandal@gmail.com>
2018-11-08 02:11:48 +0900

    Add Vector2.ts

---
+++ src/math/Vector2.ts
@@ -0,0 +1,59 @@
+type TypedArray = Int8Array | Uint8Array | Uint8ClampedArray | Int16Array |
+Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array;
+
+export default class Vector2 {
+  v: TypedArray;
+
+  constructor(x:number|TypedArray, y?:number) {
+    if (ArrayBuffer.isView(x)) {
+      this.v = ((x as any) as TypedArray);
+      return;
+    } else {
+      this.v = new Float32Array(2)
+    }
+
+    this.x = ((x as any) as number);
+    this.y = ((y as any) as number);
+  }
+
+  get className() {
+    return this.constructor.name;
+  }
+
+  clone() {
+    return new Vector2(this.x, this.y);
+  }
+
+  multiply(val:number) {
+    this.x *= val;
+    this.y *= val;
+
+    return this;
+  }
+
+  static multiply(vec2:Vector2, val:number) {
+    return new Vector2(vec2.x * val, vec2.y * val);
+  }
+
+  get x() {
+    return this.v[0];
+  }
+
+  set x(x:number) {
+    this.v[0] = x;
+  }
+
+  get y() {
+    return this.v[1];
+  }
+
+  set y(y:number) {
+    this.v[1] = y;
+  }
+
+  get raw() {
+    return this.v;
+  }
+}
+
+// GLBoost["Vector2"] = Vector2;
