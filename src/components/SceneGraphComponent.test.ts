1f5489a64ea96c1f92059a0ac3e5313d87bbd918
Yuki Shimada <emadurandal@gmail.com>
2018-12-07 23:07:11 +0900

    Update SceneGraphComponent.test

--- src/components/SceneGraphComponent.test.ts
+++ src/components/SceneGraphComponent.test.ts
@@ -1,10 +1,8 @@
 import Entity from '../core/Entity';
 import EntityRepository from '../core/EntityRepository';
-import ComponentRepository from '../core/ComponentRepository';
 import TransformComponent from './TransformComponent';
-import is from '../misc/IsUtil';
 import Vector3 from '../math/Vector3';
-import Vector4 from '../math/Vector4';
+import Matrix44 from '../math/Matrix44';
 import SceneGraphComponent from './SceneGraphComponent';
 
 function generateEntity() {
@@ -19,22 +17,35 @@ test('The EntityRepository creates a entity whose uid is 1', () => {
 });
 
 
-test('create Parent and child.', () => {
+test('create Parents and children.', () => {
+  // generate entities
   const sceneEntity = generateEntity();
   const parentEntity = generateEntity();
   const childEntity = generateEntity();
   const child2Entity = generateEntity();
+
+  // set transform info
   sceneEntity.getTransform().translate = new Vector3(1, 0, 0);
   parentEntity.getTransform().translate = new Vector3(1, 0, 0);
   childEntity.getTransform().translate = new Vector3(1, 0, 0);
   child2Entity.getTransform().translate = new Vector3(0, 1, 0);
+
+  // setup scene graph
   parentEntity.getScenGraph().addChild(childEntity.getScenGraph());
   parentEntity.getScenGraph().addChild(child2Entity.getScenGraph());
   sceneEntity.getScenGraph().addChild(parentEntity.getScenGraph());
 
   console.log(childEntity.getScenGraph().worldMatrix.toString());
   console.log(child2Entity.getScenGraph().worldMatrix.toString());
-  expect(true).toBe(true);
-  //expect(childEntity.getScenGraph.worldMatrix.isEqual(new Vector3(1, 0, 0))).toBe(true);
+  //expect(true).toBe(true);
+
+  console.log(childEntity.getScenGraph().worldMatrix.toStringApproximately());
+
+  expect(childEntity.getScenGraph().worldMatrix.isEqual(
+    new Matrix44(
+      1, 0, 0, 3,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1))).toBe(true);
 });
 
