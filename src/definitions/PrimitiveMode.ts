6294dc3757f038c0e38771f05e1c367dc62c7f9f
Yuki Shimada <emadurandal@gmail.com>
2018-12-08 01:01:23 +0900

    Update PrimitiveMode.ts

--- src/definitions/PrimitiveMode.ts
+++ src/definitions/PrimitiveMode.ts
@@ -1,8 +1,12 @@
 import { EnumIO } from "../misc/EnumIO";
 
-class PrimitiveMode implements EnumIO {
+export interface PrimitiveModeEnum extends EnumIO {
+
+}
+
+class PrimitiveModeClass implements PrimitiveModeEnum {
   static currentIndex = 0;
-  index = PrimitiveMode.currentIndex++;
+  index = PrimitiveModeClass.currentIndex++;
   readonly str: string;
 
   constructor({index, str} : {index: number, str: string}) {
@@ -19,16 +23,17 @@ class PrimitiveMode implements EnumIO {
   }
 }
 
-const Points: PrimitiveMode = new PrimitiveMode({index:0, str:'POINTS'});
-const Lines: PrimitiveMode = new PrimitiveMode({index:1, str:'LINES'});
-const LineLoop: PrimitiveMode = new PrimitiveMode({index:2, str:'LINE_LOOP'});
-const LineStrip: PrimitiveMode = new PrimitiveMode({index:3, str:'LINE_STRIP'});
-const Triangles: PrimitiveMode = new PrimitiveMode({index:4, str:'TRIANGLES'});
-const TriangleStrip: PrimitiveMode = new PrimitiveMode({index:5, str:'TRIANGLE_STRIP'});
-const TriangleFan: PrimitiveMode = new PrimitiveMode({index:6, str:'TRIANGLE_FAN'});
+const Points: PrimitiveModeEnum = new PrimitiveModeClass({index:0, str:'POINTS'});
+const Lines: PrimitiveModeEnum = new PrimitiveModeClass({index:1, str:'LINES'});
+const LineLoop: PrimitiveModeEnum = new PrimitiveModeClass({index:2, str:'LINE_LOOP'});
+const LineStrip: PrimitiveModeEnum = new PrimitiveModeClass({index:3, str:'LINE_STRIP'});
+const Triangles: PrimitiveModeEnum = new PrimitiveModeClass({index:4, str:'TRIANGLES'});
+const TriangleStrip: PrimitiveModeEnum = new PrimitiveModeClass({index:5, str:'TRIANGLE_STRIP'});
+const TriangleFan: PrimitiveModeEnum = new PrimitiveModeClass({index:6, str:'TRIANGLE_FAN'});
+
 const primitiveMode = [Points, Lines, LineLoop, LineStrip, Triangles, TriangleStrip, TriangleFan];
 
-function from({ index }: { index: number }): PrimitiveMode {
+function from({ index }: { index: number }): PrimitiveModeEnum {
   const match = primitiveMode.find(primitiveMode => primitiveMode.index === index);
   if (!match) {
     throw new Error(`Invalid PrimitiveMode index: [${index}]`);
