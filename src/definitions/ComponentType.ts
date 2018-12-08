ced07eb0e14b9f7e1749babb4502bdf5f6df26d6
Yuki Shimada <emadurandal@gmail.com>
2018-12-08 17:44:38 +0900

    Update ComponentTypes.ts

--- src/definitions/ComponentType.ts
+++ src/definitions/ComponentType.ts
@@ -4,20 +4,22 @@ export interface ComponentTypeEnum extends EnumIO {
 }
 
 class ComponentTypeClass extends EnumClass implements ComponentTypeEnum {
-  constructor({index, str} : {index: number, str: string}) {
+  readonly sizeInBytes: number;
+  constructor({index, str, sizeInBytes} : {index: number, str: string, sizeInBytes: number}) {
     super({index, str});
+    this.sizeInBytes = sizeInBytes
   }
 }
 
-const Unknown: ComponentTypeEnum = new ComponentTypeClass({index:5119, str:'UNKNOWN'});
-const Byte: ComponentTypeEnum = new ComponentTypeClass({index:5120, str:'BYTE'});
-const UnsignedByte: ComponentTypeEnum = new ComponentTypeClass({index:5121, str:'UNSIGNED_BYTE'});
-const Short: ComponentTypeEnum = new ComponentTypeClass({index:5122, str:'SHORT'});
-const UnsignedShort: ComponentTypeEnum = new ComponentTypeClass({index:5123, str:'UNSIGNED_SHORT'});
-const Int: ComponentTypeEnum = new ComponentTypeClass({index:5124, str:'INT'});
-const UnsingedInt: ComponentTypeEnum = new ComponentTypeClass({index:5125, str:'UNSIGNED_INT'});
-const Float: ComponentTypeEnum = new ComponentTypeClass({index:5126, str:'FLOAT'});
-const Double: ComponentTypeEnum = new ComponentTypeClass({index:5127, str:'DOUBLE'});
+const Unknown: ComponentTypeEnum = new ComponentTypeClass({index:5119, str:'UNKNOWN', sizeInBytes: 0});
+const Byte: ComponentTypeEnum = new ComponentTypeClass({index:5120, str:'BYTE', sizeInBytes: 1});
+const UnsignedByte: ComponentTypeEnum = new ComponentTypeClass({index:5121, str:'UNSIGNED_BYTE', sizeInBytes: 1});
+const Short: ComponentTypeEnum = new ComponentTypeClass({index:5122, str:'SHORT', sizeInBytes: 2});
+const UnsignedShort: ComponentTypeEnum = new ComponentTypeClass({index:5123, str:'UNSIGNED_SHORT', sizeInBytes: 2});
+const Int: ComponentTypeEnum = new ComponentTypeClass({index:5124, str:'INT', sizeInBytes: 4});
+const UnsingedInt: ComponentTypeEnum = new ComponentTypeClass({index:5125, str:'UNSIGNED_INT', sizeInBytes: 4});
+const Float: ComponentTypeEnum = new ComponentTypeClass({index:5126, str:'FLOAT', sizeInBytes: 4});
+const Double: ComponentTypeEnum = new ComponentTypeClass({index:5127, str:'DOUBLE', sizeInBytes: 8});
 
 const typeList = [Unknown, Byte, UnsignedByte, Short, UnsignedShort, Int, UnsingedInt, Float, Double];
 
