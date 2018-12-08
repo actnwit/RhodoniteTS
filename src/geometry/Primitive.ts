19293e2d140ab1092135de597bca9eb33da487bf
Yuki Shimada <emadurandal@gmail.com>
2018-12-08 10:51:47 +0900

    Change initial value

--- src/geometry/Primitive.ts
+++ src/geometry/Primitive.ts
@@ -2,7 +2,7 @@ import PrimitivedMode, {PrimitiveModeEnum} from '../definitions/PrimitiveMode';
 import { VertexAttributeEnum } from '../definitions/VertexAttribute';
 
 export default class Primitive {
-  __mode: PrimitiveModeEnum = PrimitivedMode.Triangles;
+  __mode: PrimitiveModeEnum = PrimitivedMode.Unknown;
   __indices: ObjectUID = 0;
   __attributes: Map<VertexAttributeEnum, ObjectUID> = new Map();
   __material: ObjectUID = 0;
