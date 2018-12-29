8647340d7a9888344306fbdb173163ca6872b2e6
Yuki Shimada <emadurandal@gmail.com>
2018-12-29 19:30:50 +0900

    Add e2e test samples

--- test/e2e/DataTextureInstancedDrawingWebGL1/test.js
+++ test/e2e/DataTextureInstancedDrawingWebGL1/test.js
@@ -1,7 +1,7 @@
-test('regression test DataTextureInstancedDrawing', async () => {
+test('regression test DataTextureInstancedDrawingWebGL1', async () => {
   jest.setTimeout(60000);
   const page = await browser.newPage();
-  await page.goto('http://localhost:8082/test/e2e/DataTextureInstancedDrawing');
+  await page.goto('http://localhost:8082/test/e2e/DataTextureInstancedDrawingWebGL1');
   await page.waitForSelector('p#rendered', {timeout: 60000});
   const canvasElement = await page.$('#world');
   const image = await canvasElement.screenshot();
