a833a41ebf4da8cdac1fe8321952ea6de0e4d2a9
Yuki Shimada <emadurandal@gmail.com>
2019-01-08 15:40:32 +0900

    Improve browsing of e2e test samples

---
+++ test/e2e/Gltf2Importer/main.js
@@ -0,0 +1,23 @@
+let p = null;
+
+const load = async function(time){
+
+  const importer = Rn.Gltf2Importer.getInstance();
+  const response = await importer.import('../../../assets/gltf/2.0/Box/glTF/Box.gltf');
+
+  if (p == null) {
+    if (response != null) {
+      const gl = document.getElementById('world').getContext('webgl');
+      gl.clearColor(1, 0, 0, 1);
+      gl.clear(gl.COLOR_BUFFER_BIT);
+    }
+
+    p = document.createElement('p');
+    p.setAttribute("id", "rendered");
+    p.innerText = 'Rendered.';
+    document.body.appendChild(p);
+  }
+}
+
+document.body.onload = load;
+
