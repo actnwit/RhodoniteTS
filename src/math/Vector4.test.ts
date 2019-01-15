285e25ec97d6b4dee1d6ce833bb13752b923b2ed
Yuki Shimada <emadurandal@gmail.com>
2019-01-15 12:19:45 +0900

    Rename a file

--- src/math/Vector4.test.ts
+++ src/math/Vector4.test.ts
@@ -2,7 +2,7 @@ import ImmutableMatrix44 from './ImmutableMatrix44';
 import Vector4 from './Vector4';
 
 
-test('ImmutableVector4 is immutable', () => {
+test('Vector4 is immutable', () => {
   const vec = new Vector4(0, 3, 4, 0);
   console.log(vec.w)
   expect(()=>{(vec as any).w = 1}).toThrowError()
