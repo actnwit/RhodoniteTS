24fd7e2dab4d15c4eb0623cf9e814b086c76dc82
Yuki Shimada <emadurandal@gmail.com>
2019-01-19 20:21:57 +0900

    Remove d.ts files

--- src/foundation/math/MathUtil.d.ts
+++
@@ -1,8 +0,0 @@
-declare function radianToDegree(rad: number): number;
-declare function degreeToRadian(deg: number): number;
-export declare const MathUtil: Readonly<{
-    radianToDegree: typeof radianToDegree;
-    degreeToRadian: typeof degreeToRadian;
-    toHalfFloat: (val: number) => number;
-}>;
-export {};
