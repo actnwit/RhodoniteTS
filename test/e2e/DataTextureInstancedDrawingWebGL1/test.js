72be250a598f648bd7f6ebfcd16108f2bad474c2
Yuki Shimada <emadurandal@gmail.com>
2019-02-06 06:01:08 +0900

    Fix test settings

--- test/e2e/DataTextureInstancedDrawingWebGL1/test.js
+++ test/e2e/DataTextureInstancedDrawingWebGL1/test.js
@@ -2,6 +2,7 @@ test('regression test DataTextureInstancedDrawingWebGL1', async () => {
   jest.setTimeout(60000);
   const page = await browser.newPage();
   await page.goto('http://localhost:8082/test/e2e/DataTextureInstancedDrawingWebGL1');
+  await page.setViewport({width: 1000, height: 1000});
   await page.waitForSelector('p#rendered', {timeout: 60000});
   const canvasElement = await page.$('#world');
   const image = await canvasElement.screenshot();
