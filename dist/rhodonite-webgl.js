db6cf75a8c50a9521bde4324e53daf02a0b489ca
Yuki Shimada <emadurandal@gmail.com>
2019-02-16 13:48:15 +0900

    Add getVertexCountAsIndicesBased and getVertexCountAsVerticesBased methods to Primitive.ts

--- dist/rhodonite-webgl.js
+++ dist/rhodonite-webgl.js
@@ -121,4 +121,4 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _get
 /***/ })
 
 }]);
-(0,eval)('this').Rn.WEBGL_VERSION='version: 0.1.1-116-g36f6-mod branch: feature/pbr';
+(0,eval)('this').Rn.WEBGL_VERSION='version: 0.1.1-118-g7515-mod branch: feature/normal-mapping';
