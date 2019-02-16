da426ee8fd891faad11e1acfbc2cea1ba23ec1f4
Yuki Shimada <emadurandal@gmail.com>
2019-02-16 14:11:40 +0900

    Fix the spec of Vector2

--- src/foundation/math/Vector2.ts
+++ src/foundation/math/Vector2.ts
@@ -1,15 +1,14 @@
+import { IVector2 } from "./IVector";
 
-class _Vector2<T extends TypedArray, S extends TypedArrayConstructor> {
+export default class Vector2 implements IVector2 {
   v: TypedArray;
-  private __typedArray: S;
 
-  constructor(typedArray:S, x:number|TypedArray, y?:number) {
-    this.__typedArray = typedArray;
+  constructor(x:number|TypedArray, y?:number) {
     if (ArrayBuffer.isView(x)) {
       this.v = ((x as any) as TypedArray);
       return;
     } else {
-      this.v = new typedArray(2)
+      this.v = new Float32Array(2)
     }
 
     this.x = ((x as any) as number);
@@ -21,7 +20,7 @@ class _Vector2<T extends TypedArray, S extends TypedArrayConstructor> {
   }
 
   clone() {
-    return new _Vector2(this.__typedArray, this.x, this.y);
+    return new Vector2(this.x, this.y);
   }
 
   multiply(val:number) {
@@ -31,7 +30,7 @@ class _Vector2<T extends TypedArray, S extends TypedArrayConstructor> {
     return this;
   }
 
-  isStrictEqual(vec:_Vector2<T, S>) {
+  isStrictEqual(vec: Vector2) {
     if (this.x === vec.x && this.y === vec.y) {
       return true;
     } else {
@@ -39,7 +38,7 @@ class _Vector2<T extends TypedArray, S extends TypedArrayConstructor> {
     }
   }
 
-  isEqual(vec: _Vector2<T, S>, delta: number = Number.EPSILON) {
+  isEqual(vec: Vector2, delta: number = Number.EPSILON) {
     if (Math.abs(vec.x - this.x) < delta &&
       Math.abs(vec.y - this.y) < delta) {
       return true;
@@ -48,9 +47,8 @@ class _Vector2<T extends TypedArray, S extends TypedArrayConstructor> {
     }
   }
 
-  
-  static multiply<T extends TypedArray, S extends TypedArrayConstructor>(typedArray:S, vec2:_Vector2<T, S>, val:number) {
-    return new _Vector2(typedArray, vec2.x * val, vec2.y * val);
+  static multiply(vec2:Vector2, val:number) {
+    return new Vector2(vec2.x * val, vec2.y * val);
   }
 
   get x() {
@@ -73,12 +71,3 @@ class _Vector2<T extends TypedArray, S extends TypedArrayConstructor> {
     return this.v;
   }
 }
-
-export class Vector2_F64 extends _Vector2<Float64Array, Float64ArrayConstructor> {
-  constructor(x:number|TypedArray, y?:number) {
-    super(Float64Array, x, y);
-  }
-}
-
-export default Vector2_F64; // Use as `import Vector2 from '**/Vector2';`
-
