65394a9afcb982b8d30a43da53d857888bd9d556
Kazuki Tanabe <k.tanabe@guncys.com>
2019-04-09 18:35:22 +0900

    add test of Vector4

--- src/foundation/math/Vector4.test.ts
+++ src/foundation/math/Vector4.test.ts
@@ -10,3 +10,14 @@ test('Vector4 is immutable', () => {
   console.log(vec.w)
   expect(vec.w).toBe(0);
 });
+
+test('Multiplication of two Vector4 works correctly', () => {
+  const vec = new Vector4(0, 3, 4, 0);
+  console.log(vec.x, vec.y, vec.z, vec.w)
+
+  const vec2 = new Vector4(0, 6, 8, 0);  
+  const result_vec = Vector4.multiplyVector(vec, vec2);
+  console.log(result_vec.x, result_vec.y, result_vec.z, result_vec.w);
+
+  expect(result_vec.isEqual(new Vector4(0, 18, 32, 0))).toBe(true);
+});
