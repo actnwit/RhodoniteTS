8752b98bb8217959ce228599823fbea02d4b1743
KaY <d.guitar.c@gmail.com>
2019-04-22 16:30:43 +0900

    Fixing a loading time of texture

--- src/foundation/importer/Gltf1Importer.ts
+++ src/foundation/importer/Gltf1Importer.ts
@@ -711,17 +711,17 @@ export default class Gltf1Importer {
       //   options.extensionLoader.setUVTransformToTexture(texture, samplerJson);
       // }
 
-      promisesToLoadResources.push(new Promise((resolve, reject)=> {
+      promisesToLoadResources.push(new Promise(async (resolve, reject) => {
         let img = new Image();
         img.crossOrigin = 'Anonymous';
-        img.src = imageUri;
+        await this._imgLoad(img, imageUri);
+
         imageJson.image = img;
+        resources.images[i] = img;
         if (imageUri.match(/^data:/)) {
           resolve(gltfJson);
         } else {
-
-          const load = (img: HTMLImageElement, response: any)=> {
-
+          const load = (img: HTMLImageElement, response: any) => {
             var bytes = new Uint8Array(response);
             var binaryData = "";
             for (var i = 0, len = bytes.byteLength; i < len; i++) {
@@ -781,6 +781,15 @@ export default class Gltf1Importer {
     let imgSrc = this._getImageType(imageType);
     let dataUrl = imgSrc + DataUtil.btoa(binaryData);
     return dataUrl;
+  }
+
+  _imgLoad(img: HTMLImageElement, imageUri: string) {
+    return new Promise(((resolve, reject) => {
+      img.onload = () => {
+        resolve();
+      };
+      img.src = imageUri;
+    }));
   }
 
   _getImageType(imageType: string) {
