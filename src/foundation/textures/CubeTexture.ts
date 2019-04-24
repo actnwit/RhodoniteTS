cf01eee02d426c3ba3abfa02a4832bf5ce937876
Yuki Shimada <emadurandal@gmail.com>
2019-04-25 06:57:34 +0900

    Support handling both of hdr and ldr_srgb

--- src/foundation/textures/CubeTexture.ts
+++ src/foundation/textures/CubeTexture.ts
@@ -1,24 +1,26 @@
 import RnObject from "../core/RnObject";
 import AbstractTexture from "./AbstractTexture";
 import WebGLResourceRepository from "../../webgl/WebGLResourceRepository";
+import { HdriFormat } from "../definitions/HdriFormat";
 
 export default class CubeTexture extends AbstractTexture {
   public baseUriToLoad?: string;
   public mipmapLevelNumber: number = 1;
-  public isHdr = false;
+  public hdriFormat = HdriFormat.LDR_SRGB;
+  public isNamePosNeg = false;
   constructor() {
     super();
   }
 
   async loadTextureImages() {
     this.__startedToLoad = true;
-    this.cgApiResourceUid = await WebGLResourceRepository.getInstance().createCubeTextureFromFiles(this.baseUriToLoad!, this.mipmapLevelNumber!, this.isHdr);
+    this.cgApiResourceUid = await WebGLResourceRepository.getInstance().createCubeTextureFromFiles(this.baseUriToLoad!, this.mipmapLevelNumber!, this.isNamePosNeg, this.hdriFormat);
     this.__isTextureReady = true;
   }
 
   loadTextureImagesAsync() {
     this.__startedToLoad = true;
-    WebGLResourceRepository.getInstance().createCubeTextureFromFiles(this.baseUriToLoad!, this.mipmapLevelNumber!, this.isHdr).then((cubeTextureUid)=>{
+    WebGLResourceRepository.getInstance().createCubeTextureFromFiles(this.baseUriToLoad!, this.mipmapLevelNumber!, this.isNamePosNeg, this.hdriFormat).then((cubeTextureUid)=>{
       this.cgApiResourceUid = cubeTextureUid;
     }).then(()=>{
       this.__isTextureReady = true;
