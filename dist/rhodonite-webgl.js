09e9b2a425c96787707a4c2fd2cef2f754635b64
Yuki Shimada <emadurandal@gmail.com>
2019-05-01 19:27:09 +0900

    Fix a bug of PBR (when cube map rotating)

--- dist/rhodonite-webgl.js
+++ dist/rhodonite-webgl.js
@@ -73,4 +73,4 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _get
 /***/ })
 
 }]);
-(0,eval)('this').Rn.WEBGL_VERSION='version: 0.1.1-439-g4f42-mod branch: master';
+(0,eval)('this').Rn.WEBGL_VERSION='version: 0.1.1-441-gb09c-mod branch: fix/improve-pbr';
