61021f8f988adcce1a4999b01a17881c5da25c08
Yuki Shimada <emadurandal@gmail.com>
2019-07-15 20:17:09 +0900

    Remove runtime error of PixelPickingTest

--- samples/test_e2e/PixelPickingTest/main.js
+++ samples/test_e2e/PixelPickingTest/main.js
@@ -1,4 +1,3 @@
-"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
@@ -34,79 +33,86 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-exports.__esModule = true;
-var load = function (time) {
-    return __awaiter(this, void 0, void 0, function () {
-        var importer, system, gl, entityRepository, cameraEntity, cameraComponent, lightEntity2, lightComponent2, response, modelConverter, rootGroup, cameraControllerComponent, p, startTime, rotationVec3, count, draw;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: return [4 /*yield*/, Rn.ModuleManager.getInstance().loadModule('webgl')];
-                case 1:
-                    _a.sent();
-                    return [4 /*yield*/, Rn.ModuleManager.getInstance().loadModule('pbr')];
-                case 2:
-                    _a.sent();
-                    importer = Rn.Gltf1Importer.getInstance();
-                    system = Rn.System.getInstance();
-                    gl = system.setProcessApproachAndCanvas(Rn.ProcessApproach.UniformWebGL1, document.getElementById('world'));
-                    entityRepository = Rn.EntityRepository.getInstance();
-                    cameraEntity = entityRepository.createEntity([Rn.TransformComponent, Rn.SceneGraphComponent, Rn.CameraComponent, Rn.CameraControllerComponent]);
-                    cameraComponent = cameraEntity.getComponent(Rn.CameraComponent);
-                    //cameraComponent.type = Rn.CameraTyp]e.Orthographic;
-                    cameraComponent.parameters = new Rn.Vector4(0.1, 1000, 90, 1);
-                    cameraEntity.getTransform().translate = new Rn.Vector3(0.0, 0, 0.5);
-                    lightEntity2 = entityRepository.createEntity([Rn.TransformComponent, Rn.SceneGraphComponent, Rn.LightComponent]);
-                    lightEntity2.getTransform().translate = new Rn.Vector3(0.0, 0.0, 10.0);
-                    lightComponent2 = lightEntity2.getComponent(Rn.LightComponent);
-                    lightComponent2.intensity = new Rn.Vector3(1, 1, 1);
-                    return [4 /*yield*/, importer["import"]('../../../assets/gltf/1.0/BoxAnimated/glTF/BoxAnimated.gltf')];
-                case 3:
-                    response = _a.sent();
-                    modelConverter = Rn.ModelConverter.getInstance();
-                    rootGroup = modelConverter.convertToRhodoniteObject(response);
-                    cameraControllerComponent = cameraEntity.getComponent(Rn.CameraControllerComponent);
-                    cameraControllerComponent.setTarget(rootGroup);
-                    p = null;
-                    Rn.CameraComponent.main = 0;
-                    startTime = Date.now();
-                    rotationVec3 = Rn.MutableVector3.one();
-                    count = 0;
-                    draw = function (time) {
-                        if (p == null && count > 0) {
-                            if (response != null) {
-                                gl.enable(gl.DEPTH_TEST);
-                                gl.viewport(0, 0, 600, 600);
-                                gl.clearColor(0.8, 0.8, 0.8, 1.0);
-                                gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var load = function (time) {
+        return __awaiter(this, void 0, void 0, function () {
+            var importer, system, gl, entityRepository, cameraEntity, cameraComponent, response, modelConverter, rootGroup, cameraControllerComponent, p, startTime, rotationVec3, count, draw;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, Rn.ModuleManager.getInstance().loadModule('webgl')];
+                    case 1:
+                        _a.sent();
+                        return [4 /*yield*/, Rn.ModuleManager.getInstance().loadModule('pbr')];
+                    case 2:
+                        _a.sent();
+                        importer = Rn.Gltf1Importer.getInstance();
+                        system = Rn.System.getInstance();
+                        gl = system.setProcessApproachAndCanvas(Rn.ProcessApproach.UniformWebGL1, document.getElementById('world'));
+                        entityRepository = Rn.EntityRepository.getInstance();
+                        cameraEntity = entityRepository.createEntity([Rn.TransformComponent, Rn.SceneGraphComponent, Rn.CameraComponent, Rn.CameraControllerComponent]);
+                        cameraComponent = cameraEntity.getComponent(Rn.CameraComponent);
+                        //cameraComponent.type = Rn.CameraTyp]e.Orthographic;
+                        cameraComponent.parameters = new Rn.Vector4(0.1, 1000, 90, 1);
+                        cameraEntity.getTransform().translate = new Rn.Vector3(0.0, 0, 0.5);
+                        return [4 /*yield*/, importer.import('../../../assets/gltf/1.0/BoxAnimated/glTF/BoxAnimated.gltf')];
+                    case 3:
+                        response = _a.sent();
+                        modelConverter = Rn.ModelConverter.getInstance();
+                        rootGroup = modelConverter.convertToRhodoniteObject(response);
+                        cameraControllerComponent = cameraEntity.getComponent(Rn.CameraControllerComponent);
+                        cameraControllerComponent.setTarget(rootGroup);
+                        p = null;
+                        Rn.CameraComponent.main = 0;
+                        startTime = Date.now();
+                        rotationVec3 = Rn.MutableVector3.one();
+                        count = 0;
+                        draw = function (time) {
+                            if (p == null && count > 0) {
+                                if (response != null) {
+                                    gl.enable(gl.DEPTH_TEST);
+                                    gl.viewport(0, 0, 600, 600);
+                                    gl.clearColor(0.8, 0.8, 0.8, 1.0);
+                                    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+                                }
+                                p = document.createElement('p');
+                                p.setAttribute("id", "rendered");
+                                p.innerText = 'Rendered.';
+                                document.body.appendChild(p);
                             }
-                            p = document.createElement('p');
-                            p.setAttribute("id", "rendered");
-                            p.innerText = 'Rendered.';
-                            document.body.appendChild(p);
-                        }
-                        if (window.isAnimating) {
-                            var date = new Date();
-                            var rotation = 0.001 * (date.getTime() - startTime);
-                            //rotationVec3.v[0] = 0.1;
-                            //rotationVec3.v[1] = rotation;
-                            //rotationVec3.v[2] = 0.1;
-                            var time_1 = (date.getTime() - startTime) / 1000;
-                            Rn.AnimationComponent.globalTime = time_1;
-                            if (time_1 > Rn.AnimationComponent.endInputValue) {
-                                startTime = date.getTime();
+                            if (window.isAnimating) {
+                                var date = new Date();
+                                var rotation = 0.001 * (date.getTime() - startTime);
+                                //rotationVec3.v[0] = 0.1;
+                                //rotationVec3.v[1] = rotation;
+                                //rotationVec3.v[2] = 0.1;
+                                var time_1 = (date.getTime() - startTime) / 1000;
+                                Rn.AnimationComponent.globalTime = time_1;
+                                if (time_1 > Rn.AnimationComponent.endInputValue) {
+                                    startTime = date.getTime();
+                                }
+                                //console.log(time);
+                                //      rootGroup.getTransform().scale = rotationVec3;
+                                //rootGroup.getTransform().translate = rootGroup.getTransform().translate;
                             }
-                            //console.log(time);
-                            //      rootGroup.getTransform().scale = rotationVec3;
-                            //rootGroup.getTransform().translate = rootGroup.getTransform().translate;
-                        }
-                        system.process();
-                        count++;
-                        requestAnimationFrame(draw);
-                    };
-                    draw(0);
-                    return [2 /*return*/];
-            }
+                            system.process();
+                            count++;
+                            requestAnimationFrame(draw);
+                        };
+                        draw(0);
+                        return [2 /*return*/];
+                }
+            });
         });
-    });
-};
-document.body.onload = load;
+    };
+    document.body.onload = load;
+});
