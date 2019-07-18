a88c4418a94132d88f5e61136708bc16881f29c2
Yuki Shimada <emadurandal@gmail.com>
2019-07-18 17:57:42 +0900

    Add envBRDFApprox GLSLShader

--- src/webgl/shaders/GLSLShader.ts
+++ src/webgl/shaders/GLSLShader.ts
@@ -396,6 +396,17 @@ export default abstract class GLSLShader {
       );
     }
 
+    // this is from https://www.unrealengine.com/blog/physically-based-shading-on-mobile
+    vec3 envBRDFApprox( vec3 SpecularColor, float Roughness, float NoV ) {
+      const vec4 c0 = vec4(-1, -0.0275, -0.572, 0.022 );
+      const vec4 c1 = vec4(1, 0.0425, 1.04, -0.04 );
+      vec4 r = Roughness * c0 + c1;
+      float a004 = min( r.x * r.x, exp2( -9.28 * NoV ) ) * r.x + r.y;
+      vec2 AB = vec2( -1.04, 1.04 ) * a004 + r.zw;
+
+      return SpecularColor * AB.x + AB.y;
+    }
+
     // GGX NDF
     float d_ggx(float NH, float alphaRoughness) {
       float roughnessSqr = alphaRoughness * alphaRoughness;
