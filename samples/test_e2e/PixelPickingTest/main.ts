4bff7e17d7a414ebe62f75dd1b15a30d6330b8dd
Yuki Shimada <emadurandal@gmail.com>
2019-07-31 04:10:30 -0700

    Update sample code of main.ts in PixelPickingTest

--- samples/test_e2e/PixelPickingTest/main.ts
+++ samples/test_e2e/PixelPickingTest/main.ts
@@ -2,17 +2,22 @@ import { RnType } from '../../../dist/types/foundation/main'
 import CameraComponent from '../../../dist/types/foundation/components/CameraComponent';
 import CameraControllerComponent from '../../../dist/types/foundation/components/CameraControllerComponent';
 import Entity from '../../../dist/types/foundation/core/Entity';
+import LightComponent from '../../../dist/types/foundation/components/LightComponent';
 
 declare const Rn: RnType;
 declare const window: any;
 
-const setupRenderPassEntityUidOutput = function(rootGroup: Entity, cameraComponent: CameraComponent) {
+
+const setupRenderPassEntityUidOutput = function(rootGroup: Entity, cameraComponent: CameraComponent, canvas: HTMLCanvasElement) {
   const renderPass = new Rn.RenderPass();
   const entityUidOutputMaterial = Rn.MaterialHelper.createEntityUIDOutputMaterial();
   Rn.WebGLStrategyUniform.setupMaterial(entityUidOutputMaterial);
 
   renderPass.material = entityUidOutputMaterial;
   renderPass.cameraComponent = cameraComponent;
+
+  const framebuffer = Rn.RenderableHelper.createTexturesForRenderTarget(canvas.clientWidth, canvas.clientHeight, 1, {});
+  renderPass.setFramebuffer(framebuffer);
   // renderPass.toClearColorBuffer = true;
   // renderPass.toClearDepthBuffer = true;
 
@@ -23,7 +28,7 @@ const setupRenderPassEntityUidOutput = function(rootGroup: Entity, cameraCompone
   return renderPass;
 }
 
-const setupRenderPassRendering = function(rootGroup: Entity, cameraComponent: CameraComponent) {
+const setupRenderPassRendering = function(rootGroup, cameraComponent) {
   const renderPass = new Rn.RenderPass();
   renderPass.cameraComponent = cameraComponent;
   renderPass.addEntities([rootGroup]);
@@ -31,67 +36,89 @@ const setupRenderPassRendering = function(rootGroup: Entity, cameraComponent: Ca
   return renderPass;
 }
 
+const pick = function(e) {
+  const x = e.offsetX;
+  const y = window.canvas.clientHeight - e.offsetY;
+  const framebuffer = window.renderPassEntityUidOutput.getFramebuffer();
+  const renderTargetTexture = framebuffer.colorAttachments[0];
+  const pickedPixel = renderTargetTexture.getPixelValueAt(x, y);
+  console.log(pickedPixel.toString());
+}
+
+let p = null;
+
 const load = async function(time){
   await Rn.ModuleManager.getInstance().loadModule('webgl');
   await Rn.ModuleManager.getInstance().loadModule('pbr');
   const importer = Rn.Gltf1Importer.getInstance();
   const system = Rn.System.getInstance();
-  const gl = system.setProcessApproachAndCanvas(Rn.ProcessApproach.UniformWebGL1, document.getElementById('world') as HTMLCanvasElement);
-
-  const entityRepository = Rn.EntityRepository.getInstance();
-
+  const canvas = document.getElementById('world') as HTMLCanvasElement;
+  window.canvas = canvas;
+  const gl = system.setProcessApproachAndCanvas(Rn.ProcessApproach.UniformWebGL1, canvas);
   const expression = new Rn.Expression();
 
+  const entityRepository = Rn.EntityRepository.getInstance();
 
   // Camera
   const cameraEntity = entityRepository.createEntity([Rn.TransformComponent, Rn.SceneGraphComponent, Rn.CameraComponent, Rn.CameraControllerComponent])
   const cameraComponent = cameraEntity.getComponent(Rn.CameraComponent) as CameraComponent;
-  cameraComponent.type = Rn.CameraType.Orthographic;
-  cameraComponent.parameters = new Rn.Vector4(0.1, 1000, 1, 1);
-  // cameraEntity.getTransform().translate = new Rn.Vector3(0.0, 0, 10);
+  //cameraComponent.type = Rn.CameraTyp]e.Orthographic;
+  cameraComponent.parameters = new Rn.Vector4(0.1, 1000, 90, 1);
+  cameraEntity.getTransform().translate = new Rn.Vector3(0.0, 0, 0.5);
 
 
+  // Lights
+  // const lightEntity = entityRepository.createEntity([Rn.TransformComponent, Rn.SceneGraphComponent, Rn.LightComponent])
+  // lightEntity.getTransform().translate = new Rn.Vector3(1.0, 100000.0, 1.0);
+  // lightEntity.getComponent(Rn.LightComponent).intensity = new Rn.Vector3(1, 1, 1);
+  const lightEntity2 = entityRepository.createEntity([Rn.TransformComponent, Rn.SceneGraphComponent, Rn.LightComponent])
+  lightEntity2.getTransform().translate = new Rn.Vector3(0.0, 0.0, 10.0);
+  (lightEntity2.getComponent(Rn.LightComponent) as LightComponent).intensity = new Rn.Vector3(1, 1, 1);
+  //lightEntity2.getTransform().rotate = new Rn.Vector3(Math.PI/2, 0, 0);
+  //lightEntity2.getComponent(Rn.LightComponent).type = Rn.LightType.Directional;
 
 
-//  const response = await importer.import('../../../assets/gltf/2.0/Box/glTF/Box.gltf');
+  //  const response = await importer.import('../../../assets/gltf/2.0/Box/glTF/Box.gltf');
   //const response = await importer.import('../../../assets/gltf/2.0/BoxTextured/glTF/BoxTextured.gltf');
-//  const response = await importer.import('../../../assets/gltf/2.0/Lantern/glTF/Lantern.gltf');
+  //  const response = await importer.import('../../../assets/gltf/2.0/Lantern/glTF/Lantern.gltf');
   //const response = await importer.import('../../../assets/gltf/2.0/WaterBottle/glTF/WaterBottle.gltf');
- //const response = await importer.import('../../../assets/gltf/2.0/CesiumMilkTruck/glTF/CesiumMilkTruck.gltf');
+  //const response = await importer.import('../../../assets/gltf/2.0/CesiumMilkTruck/glTF/CesiumMilkTruck.gltf');
   //const response = await importer.import('../../../assets/gltf/1.0/VC/glTF/VC.gltf');
   //const response = await importer.import('../../../assets/gltf/1.0/GearboxAssy/glTF/GearboxAssy.gltf');
-// const response = await importer.import('../../../assets/gltf/1.0/Buggy/glTF/Buggy.gltf');
+  // const response = await importer.import('../../../assets/gltf/1.0/Buggy/glTF/Buggy.gltf');
   //const response = await importer.import('../../../assets/gltf/2.0/FlightHelmet/glTF/FlightHelmet.gltf');
-//  const response = await importer.import('../../../assets/gltf/1.0/ReciprocatingSaw/glTF/ReciprocatingSaw.gltf');
- //const response = await importer.import('../../../assets/gltf/1.0/2CylinderEngine/glTF/2CylinderEngine.gltf');
-//  const response = await importer.import('../../../assets/gltf/1.0/Duck/glTF/Duck.gltf');
- //const response = await importer.import('../../../assets/gltf/1.0/Avocado/glTF/Avocado.gltf');
+  //  const response = await importer.import('../../../assets/gltf/1.0/ReciprocatingSaw/glTF/ReciprocatingSaw.gltf');
+  //const response = await importer.import('../../../assets/gltf/1.0/2CylinderEngine/glTF/2CylinderEngine.gltf');
+  //  const response = await importer.import('../../../assets/gltf/1.0/Duck/glTF/Duck.gltf');
+  //const response = await importer.import('../../../assets/gltf/1.0/Avocado/glTF/Avocado.gltf');
   const response = await importer.import('../../../assets/gltf/1.0/BoxAnimated/glTF/BoxAnimated.gltf');
-//const response = await importer.import('../../../assets/gltf/1.0/BrainStem/glTF/BrainStem.gltf');
+  //const response = await importer.import('../../../assets/gltf/1.0/BrainStem/glTF/BrainStem.gltf');
   const modelConverter = Rn.ModelConverter.getInstance();
   const rootGroup = modelConverter.convertToRhodoniteObject(response);
   //rootGroup.getTransform().translate = new Rn.Vector3(1.0, 0, 0);
-//  rootGroup.getTransform().rotate = new Rn.Vector3(0, 1.0, 0.0);
-//  rootGroup.getTransform().scale = new Rn.Vector3(0.01, 0.01, 0.01);
+  //  rootGroup.getTransform().rotate = new Rn.Vector3(0, 1.0, 0.0);
+  //  rootGroup.getTransform().scale = new Rn.Vector3(0.01, 0.01, 0.01);
 
-  const renderPassEntityUidOutput = setupRenderPassEntityUidOutput(rootGroup, cameraComponent);
-  const renderPassRendering = setupRenderPassRendering(rootGroup, cameraComponent);
-  // expression.addRenderPasses([renderPassEntityUidOutput]);
-  expression.addRenderPasses([renderPassRendering]);
-  // expression.addRenderPasses([renderPassEntityUidOutput, renderPassRendering]);
-  // expression.addRenderPasses([renderPassRendering]);
+
+    const renderPassEntityUidOutput = setupRenderPassEntityUidOutput(rootGroup, cameraComponent, canvas);
+    window.renderPassEntityUidOutput = renderPassEntityUidOutput;
+    const renderPassRendering = setupRenderPassRendering(rootGroup, cameraComponent);
+    // expression.addRenderPasses([renderPassEntityUidOutput]);
+    // expression.addRenderPasses([renderPassRendering]);
+    expression.addRenderPasses([renderPassEntityUidOutput, renderPassRendering]);
+    // expression.addRenderPasses([renderPassRendering]);
+
 
   // CameraComponent
   const cameraControllerComponent = cameraEntity.getComponent(Rn.CameraControllerComponent) as CameraControllerComponent;
   cameraControllerComponent.setTarget(rootGroup);
 
-  let p: HTMLParagraphElement = null;
 
   Rn.CameraComponent.main = 0;
   let startTime = Date.now();
   const rotationVec3 = Rn.MutableVector3.one();
   let count = 0;
-  const draw = function(time: number) {
+  const draw = function(time) {
 
     if (p == null && count > 0) {
       if (response != null) {
@@ -121,7 +148,7 @@ const load = async function(time){
         startTime = date.getTime();
       }
       //console.log(time);
-//      rootGroup.getTransform().scale = rotationVec3;
+  //      rootGroup.getTransform().scale = rotationVec3;
       //rootGroup.getTransform().translate = rootGroup.getTransform().translate;
     }
 
@@ -131,8 +158,10 @@ const load = async function(time){
     requestAnimationFrame(draw);
   }
 
+  document.addEventListener('mousedown', pick);
+
   draw(0);
 }
 
-document.body.onload = load;
 
+document.body.onload = load;
