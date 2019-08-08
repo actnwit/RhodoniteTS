f7b1bc85ac4279884483455b76c87745b9677595
Yuki Shimada <emadurandal@gmail.com>
2019-08-08 11:16:42 +0900

    Changes the value of maxMaterialInstanceForEachType

--- src/foundation/core/Config.ts
+++ src/foundation/core/Config.ts
@@ -1,5 +1,5 @@
 let maxEntityNumber = 5000;
 let maxLightNumberInShader = 4;
-let maxMaterialInstanceForEachType = 100;
+let maxMaterialInstanceForEachType = 400;
 
 export default { maxEntityNumber, maxLightNumberInShader, maxMaterialInstanceForEachType };
