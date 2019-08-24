a596670dd67376fa4831b84d4e34791b2cd7dab4
Yuki Shimada <emadurandal@gmail.com>
2019-08-24 20:32:34 +0900

    Limite the size of uncompressed textures.

--- src/foundation/core/Config.ts
+++ src/foundation/core/Config.ts
@@ -4,6 +4,7 @@ let maxVertexMorphNumberInShader = 41;
 let maxMaterialInstanceForEachType = 400;
 let maxSkeletonNumber = 20;
 let maxCameraNumber = 20;
+let maxSizeLimitOfNonCompressedTexture = 512;
 
 export default { maxEntityNumber, maxLightNumberInShader, maxVertexMorphNumberInShader, maxMaterialInstanceForEachType,
-  maxSkeletonNumber, maxCameraNumber };
+  maxSkeletonNumber, maxCameraNumber, maxSizeLimitOfNonCompressedTexture };
