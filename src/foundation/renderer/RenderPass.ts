53ee1d4ce2f62c40d8bba90a16164e43afe8eb3b
kFurusho <d.guitar.c@gmail.com>
2019-10-21 12:48:53 +0900

    docs: add docs of expression and render pass

--- src/foundation/renderer/RenderPass.ts
+++ src/foundation/renderer/RenderPass.ts
@@ -12,6 +12,9 @@ import System from "../system/System";
 import ModuleManager from '../system/ModuleManager';
 import Primitive from "../geometry/Primitive";
 
+/**
+ * A render pass is a collecion of the resources which is used in rendering process.
+ */
 export default class RenderPass extends RnObject {
   private __entities: Entity[] = [];
   private __sceneGraphDirectlyAdded: SceneGraphComponent[] = [];
@@ -36,6 +39,10 @@ export default class RenderPass extends RnObject {
     super();
   }
 
+  /**
+   * Add entities to draw.
+   * @param entities An array of entities.
+   */
   addEntities(entities: Entity[]) {
     for (let entity of entities) {
       const sceneGraphComponent = entity.getSceneGraph();
@@ -58,10 +65,17 @@ export default class RenderPass extends RnObject {
     this.__collectTopLevelSceneGraphComponents();
   }
 
+  /**
+   * Gets the list of entities on this render pass.
+   * @return An array of entities
+   */
   get entities(): Entity[] {
     return this.__entities;
   }
 
+  /**
+   * Clear entities on this render pass.
+   */
   clearEntities() {
     this.__meshComponents = void 0;
     this.__topLevelSceneGraphComponents = void 0;
@@ -97,29 +111,54 @@ export default class RenderPass extends RnObject {
     }
   }
 
+  /**
+   * Get all the MeshComponents list of the entities on this render pass.
+   * @return An array of MeshComponents
+   */
   get meshComponents() {
     this.__collectMeshComponents();
     return this.__meshComponents;
   }
 
+  /**
+   * Get all the highest level SceneGraphComponents list of the entities on this render pass.
+   * @return An array of SceneGraphComponents
+   */
   get sceneTopLevelGraphComponents() {
     this.__collectTopLevelSceneGraphComponents();
     return this.__topLevelSceneGraphComponents;
   }
 
+  /**
+   * Sets the target framebuffer of this render pass.
+   * If two or more render pass share a framebuffer, Rhodonite renders entities in render passes to same framebuffer.
+   * @param framebuffer A framebuffer
+   */
   setFramebuffer(framebuffer: FrameBuffer) {
     this.__frameBuffer = framebuffer;
     this.setViewport(new Vector4(0, 0, framebuffer.width, framebuffer.height));
   }
 
+  /**
+   * Gets the framebuffer if this render pass has the target framebuffer.
+   * @return A framebuffer
+   */
   getFramebuffer(): FrameBuffer | undefined {
     return this.__frameBuffer;
   }
 
+  /**
+   * Sets the viewport of this render pass.
+   * @param vec A Vector4 (Origin of coordinatesX, origin of coordinatesY, width, height).
+   */
   setViewport(vec: Vector4) {
     this.__viewport = vec;
   }
 
+  /**
+   * Gets the viewport if this render pass has the viewport.
+   * @return A Vector4 (Origin of coordinatesX, origin of coordinatesY, width, height).
+   */
   getViewport() {
     return this.__viewport;
   }
@@ -133,12 +172,26 @@ export default class RenderPass extends RnObject {
     (this.__webglRenderingStrategy as any).setupDefaultShaderSemantics(material, isPointSprite);
   }
 
+  /**
+   * Sets a material for the primitive on this render pass.
+   * If Rhodonite draw the primitive using this render pass, Rhodonite uses this material instead of the material on the primitive.
+   * @param material A material attaching to the primitive
+   * @param primitive A target primitive
+   * @param isPointSprite Set true, if the primitive is a point sprite
+   */
   setMaterialForPrimitive(material: Material, primitive: Primitive, isPointSprite: boolean = false) {
     this.__primitiveMaterial.set(primitive, material);
 
     this.__setupMaterial(material, isPointSprite);
   }
 
+  /**
+   * Sets a material for all the primitive on this render pass.
+   * For all the primitive, Rhodonite uses this material instead of the material on the primitive.
+   * Where if this render pass has a map between primitive and material by setMaterialForPrimitive, Rhodonite uses the material mapped by primitive.
+   * @param material A material attaching to the primitive
+   * @param isPointSprite Set true, if the primitive is a point sprite
+   */
   setMaterial(material: Material, isPointSprite: boolean = false) {
     this.__material = material;
 
