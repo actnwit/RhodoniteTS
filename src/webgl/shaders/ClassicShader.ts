c8998d5202565879bc5c643c4f165ab14f6b2e60
emadurandal <emadurandal@gmail.com>
2019-11-22 15:27:52 +0900

    refactor: Use shaderity for fragment shader of ClassicSingleShader

--- src/webgl/shaders/ClassicShader.ts
+++ src/webgl/shaders/ClassicShader.ts
@@ -6,7 +6,8 @@ import { CompositionTypeEnum } from "../../foundation/main";
 import { CompositionType } from "../../foundation/definitions/CompositionType";
 import ISingleShader from "./ISingleShader";
 import { WellKnownComponentTIDs } from "../../foundation/components/WellKnownComponentTIDs";
-import classicSingleShader from "../shaderity_shaders/classicSingleShader.vert";
+import classicSingleShaderVertex from "../shaderity_shaders/classicSingleShader.vert";
+import classicSingleShaderFragment from "../shaderity_shaders/classicSingleShader.frag";
 import Shaderity from "shaderity";
 
 export type AttributeNames = Array<string>;
@@ -28,7 +29,7 @@ export default class ClassicShader extends GLSLShader implements ISingleShader {
   }
 
   getVertexShaderBody(args: any) {
-    const obj = this.__shaderity.fillTemplate(classicSingleShader, {
+    const obj = this.__shaderity.fillTemplate(classicSingleShaderVertex, {
       definitions: (typeof args.definitions !== 'undefined') ? args.definitions : '',
       matricesGetters: (typeof args.matricesGetters !== 'undefined') ? args.matricesGetters : '',
       getters: (typeof args.getters !== 'undefined') ? args.getters : '',
@@ -41,137 +42,16 @@ export default class ClassicShader extends GLSLShader implements ISingleShader {
   }
 
   getPixelShaderBody(args: any) {
-    const _version = this.glsl_versionText;
-    const _in = this.glsl_fragment_in;
-    const _def_rt0 = this.glsl_rt0;
-    const _def_fragColor = this.glsl_fragColor;
-    const _texture = this.glsl_texture;
+    const obj = this.__shaderity.fillTemplate(classicSingleShaderFragment, {
+      definitions: (typeof args.definitions !== 'undefined') ? args.definitions : '',
+      getters: (typeof args.getters !== 'undefined') ? args.getters : '',
+      Config: Config,
+      WellKnownComponentTIDs: WellKnownComponentTIDs
+    });
+    const isWebGL2 = this.__webglResourceRepository?.currentWebGLContextWrapper?.isWebGL2;
+    const code = this.__shaderity.transformTo(isWebGL2 ? 'WebGL2' : 'WebGL1', obj).code;
 
-    return `${_version}
-${this.glslPrecision}
-
-${(typeof args.definitions !== 'undefined') ? args.definitions : ''}
-
-${this.prerequisites}
-
-${_in} vec3 v_color;
-${_in} vec3 v_normal_inWorld;
-${_in} vec4 v_position_inWorld;
-${_in} vec2 v_texcoord;
-${_in} vec3 v_baryCentricCoord;
-${_def_rt0}
-
-${(typeof args.getters !== 'undefined') ? args.getters : ''}
-
-void main ()
-{
-  ${this.mainPrerequisites}
-
-  // Normal
-  vec3 normal_inWorld = normalize(v_normal_inWorld);
-
-  vec4 diffuseColorFactor = get_diffuseColorFactor(materialSID, 0);
-
-
-  // diffuseColor
-  vec3 diffuseColor = vec3(0.0, 0.0, 0.0);
-  float alpha = 1.0;
-  if (v_color != diffuseColor && diffuseColorFactor.rgb != diffuseColor) {
-    diffuseColor = v_color * diffuseColorFactor.rgb;
-    alpha = diffuseColorFactor.a;
-  } else if (v_color == diffuseColor) {
-    diffuseColor = diffuseColorFactor.rgb;
-    alpha = diffuseColorFactor.a;
-  } else if (diffuseColorFactor.rgb == diffuseColor) {
-    diffuseColor = v_color;
-  } else {
-    diffuseColor = vec3(1.0, 1.0, 1.0);
-  }
-
-  // diffuseColorTexture
-  vec4 textureColor = ${_texture}(u_diffuseColorTexture, v_texcoord);
-  if (textureColor.r > 0.05) {
-    diffuseColor *= textureColor.rgb;
-  }
-
-  // Lighting
-  vec3 shadingColor = vec3(0.0, 0.0, 0.0);
-#ifdef RN_IS_LIGHTING
-  int shadingModel = get_shadingModel(materialSID, 0);
-  if (shadingModel > 0) {
-
-    vec3 diffuse = vec3(0.0, 0.0, 0.0);
-    vec3 specular = vec3(0.0, 0.0, 0.0);
-    for (int i = 0; i < ${Config.maxLightNumberInShader}; i++) {
-      if (i >= lightNumber) {
-        break;
-      }
-
-      vec4 gotLightDirection = get_lightDirection(0.0, i);
-      vec4 gotLightPosition = get_lightPosition(0.0, i);
-      vec4 gotLightIntensity = get_lightIntensity(0.0, i);
-      vec3 lightDirection = gotLightDirection.xyz;
-      vec3 lightIntensity = gotLightIntensity.xyz;
-      vec3 lightPosition = gotLightPosition.xyz;
-      float lightType = gotLightPosition.w;
-      float spotCosCutoff = gotLightDirection.w;
-      float spotExponent = gotLightIntensity.w;
-
-      if (0.75 < lightType) { // is pointlight or spotlight
-        lightDirection = normalize(lightPosition - v_position_inWorld.xyz);
-      }
-      float spotEffect = 1.0;
-      if (lightType > 1.75) { // is spotlight
-        spotEffect = dot(gotLightDirection.xyz, lightDirection);
-        if (spotEffect > spotCosCutoff) {
-          spotEffect = pow(spotEffect, spotExponent);
-        } else {
-          spotEffect = 0.0;
-        }
-      }
-
-      vec3 incidentLight = spotEffect * lightIntensity;
-
-
-
-      diffuse += diffuseColor * max(0.0, dot(normal_inWorld, lightDirection)) * incidentLight;
-
-      float shininess = get_shininess(materialSID, 0);
-      int shadingModel = get_shadingModel(materialSID, 0);
-
-      float cameraSID = u_currentComponentSIDs[${WellKnownComponentTIDs.CameraComponentTID}];
-      vec3 viewPosition = get_viewPosition(cameraSID, 0);
-
-      if (shadingModel == 2) {// BLINN
-        // ViewDirection
-        vec3 viewDirection = normalize(viewPosition - v_position_inWorld.xyz);
-        vec3 halfVector = normalize(lightDirection + viewDirection);
-        specular += pow(max(0.0, dot(halfVector, normal_inWorld)), shininess);
-      } else if (shadingModel == 3) { // PHONG
-        vec3 viewDirection = normalize(viewPosition - v_position_inWorld.xyz);
-        vec3 R = reflect(lightDirection, normal_inWorld);
-        specular += pow(max(0.0, dot(R, viewDirection)), shininess);
-      }
-
-    }
-
-    shadingColor = diffuse + specular;
-  } else {
-    shadingColor = diffuseColor;
-  }
-#else
-  shadingColor = diffuseColor;
-#endif
-
-  rt0 = vec4(shadingColor * alpha, alpha);
-  // rt0 = vec4(u_lightNumber, 0.0, 0.0, 1.0);
-  // rt0 = vec4(1.0, 0.0, 0.0, 1.0);
-  // rt0 = vec4(normal_inWorld*0.5+0.5, 1.0);
-
-
-  ${_def_fragColor}
-}
-`;
+    return code;
   }
 
   attributeNames: AttributeNames = [
