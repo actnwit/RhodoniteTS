43a7b1d9072e34435e4330234a5a6b36ad86b781
emadurandal <emadurandal@gmail.com>
2019-11-29 00:09:45 +0900

    Rename webvr.ts -> WebVRSystem.ts

---
+++ src/rhodonite-xr.ts
@@ -0,0 +1,16 @@
+import WebVRSystem from "./xr/WebVRSystem";
+
+const RnXR = Object.freeze({
+  WebVRSystem
+});
+export default RnXR;
+
+if (typeof exports !== 'undefined') {
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = RnXR;
+}
+
+export type RnXR = typeof RnXR;
+(0, eval)('this').RnXR = RnXR;
