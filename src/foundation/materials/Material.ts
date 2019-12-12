70260a60cd53318612b750b5c8fabbce21941ae2
emadurandal <emadurandal@gmail.com>
2019-12-12 12:09:13 +0900

    Fix the unifrom setting process

--- src/foundation/materials/Material.ts
+++ src/foundation/materials/Material.ts
@@ -379,11 +379,13 @@ export default class Material extends RnObject {
     const webglResourceRepository = CGAPIResourceRepository.getWebGLResourceRepository();
     const shaderProgram = webglResourceRepository.getWebGLResource(shaderProgramUid) as any;
 
-    this.__fieldsInfo.forEach((info, key) => {
+    this.__fields.forEach((value, key) => {
+      const info = this.__fieldsInfo.get(key)!;
       if (info.compositionType === CompositionType.Texture2D || info.compositionType === CompositionType.TextureCube) {
-      const value = this.__fields.get(key)!;
-        const shaderSemantic = ShaderSemanticsClass.getShaderSemanticByIndex(key);
-        webglResourceRepository.setUniformValue(shaderProgram, shaderSemantic.str, firstTime, value);
+        this.__fields.forEach((value, key) => {
+          const shaderSemantic = ShaderSemanticsClass.getShaderSemanticByIndex(key);
+          webglResourceRepository.setUniformValue(shaderProgram, shaderSemantic.str, firstTime, value);
+        });
       }
     });
   }
@@ -396,10 +398,10 @@ export default class Material extends RnObject {
     });
 
     const webglResourceRepository = CGAPIResourceRepository.getWebGLResourceRepository();
-    this.__fieldsInfo.forEach((info, key) => {
-      if (!info.isSystem) {
-        if (args.setUniform || info.compositionType === CompositionType.Texture2D || info.compositionType === CompositionType.TextureCube) {
-          const value = this.__fields.get(key)!;
+    this.__fields.forEach((value, key) => {
+      const info = this.__fieldsInfo.get(key)!;
+      if (args.setUniform || info.compositionType === CompositionType.Texture2D || info.compositionType === CompositionType.TextureCube) {
+        if (!info.isSystem) {
           webglResourceRepository.setUniformValue(shaderProgram, info.semantic.str, firstTime, value, info.index);
         }
       }
