24273f763ef8f0dcad159b15aadacde6213b7883
emadurandal <emadurandal@gmail.com>
2019-12-30 01:05:36 +0900

    Remove comments

--- jest-puppeteer-config.js
+++ jest-puppeteer-config.js
@@ -6,14 +6,11 @@ module.exports = {
     launchTimeout: 5000, // Wait 5 secs max before timing out
   },
   launch: {
-    // デバッグ時は以下をuncommentします
     headless: false,
     devtools: false,
 
-    // デフォルトのChromiumで動かない機能がある場合はChromeの実行ファイルパスを渡すこともできます
-    // executablePath: "/usr/bin/google-chrome-stable",
+    // executablePath: "/usr/bin/chromium-browser",
 
-    // 必要に応じて引数を渡します
     args: ["--start-maximized", "--no-sandbox"],
   },
 };
