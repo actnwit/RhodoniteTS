5b8c48e90357d812e3f4cb294747e74f5fa9b018
emadurandal <emadurandal@gmail.com>
2020-02-18 05:13:03 +0900

    Fix the code of Normalize.glsl

--- src/webgl/shaderity_shaders/nodes/Normalize.glsl
+++ src/webgl/shaderity_shaders/nodes/Normalize.glsl
@@ -1,12 +1,12 @@
 
 void normalize(in vec2 value, out vec2 outValue) {
-  outValue = normalize(value)
+  outValue = normalize(value);
 }
 
 void normalize(in vec3 value, out vec3 outValue) {
-  outValue = normalize(value)
+  outValue = normalize(value);
 }
 
 void normalize(in vec4 value, out vec4 outValue) {
-  outValue = normalize(value)
+  outValue = normalize(value);
 }
