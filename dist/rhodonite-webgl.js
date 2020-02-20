56074c3262cfe258c6611c97e6881eff996848be
emadurandal <emadurandal@gmail.com>
2020-02-21 05:11:38 +0900

    Update distribution files

--- dist/rhodonite-webgl.js
+++ dist/rhodonite-webgl.js
@@ -12,3 +12,5 @@
 (0,eval)('this').Rn.WEBGL_VERSION='version: 0.1.5-315-g6b02-mod branch: feat/node-editor';
 
 (0,eval)('this').Rn.WEBGL_VERSION='version: 0.1.5-315-g6b02-mod branch: feat/node-editor';
+
+(0,eval)('this').Rn.WEBGL_VERSION='version: 0.1.5-325-g0d3cc-mod branch: master';
