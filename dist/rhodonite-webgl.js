62de56497cf2e27958b1ed68c39c116f7338aa61
Yuki Shimada <emadurandal@gmail.com>
2020-03-10 12:21:13 +0900

    Update distribution files

--- dist/rhodonite-webgl.js
+++ dist/rhodonite-webgl.js
@@ -1,10 +1,2 @@
 
-(0,eval)('this').Rn.WEBGL_VERSION='version: 0.1.5-326-g5607-mod branch: master';
-
-(0,eval)('this').Rn.WEBGL_VERSION='version: 0.2.0-17-g0e700-mod branch: master';
-
-(0,eval)('this').Rn.WEBGL_VERSION='version: 0.2.0-18-g49dd-mod branch: master';
-
-(0,eval)('this').Rn.WEBGL_VERSION='version: 0.2.1-2-ga3c8b-mod branch: master';
-
-(0,eval)('this').Rn.WEBGL_VERSION='version: 0.2.2-6-g3c76-mod branch: master';
+(0,eval)('this').Rn.WEBGL_VERSION='version: 0.2.4-mod branch: master';
