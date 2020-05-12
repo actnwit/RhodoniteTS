b51cd65c58e9fb4f2e8667f77e3bfb5bc93eea9c
KaY <d.guitar.c@gmail.com>
2020-05-12 16:37:37 +0900

    chore: change order of methods

--- src/foundation/math/MutableQuaternion.ts
+++ src/foundation/math/MutableQuaternion.ts
@@ -12,112 +12,48 @@ export default class MutableQuaternion extends Quaternion implements IVector4 {
     super(x, y, z, w);
   }
 
-  static dummy() {
-    return new MutableQuaternion(null);
+  get x(): number {
+    return this.v[0];
   }
 
-  clone() {
-    return new MutableQuaternion(this.x, this.y, this.z, this.w);
+  set x(x: number) {
+    this.v[0] = x;
   }
 
-  axisAngle(axisVec3: Vector3, radian: number) {
-    var halfAngle = 0.5 * radian;
-    var sin = Math.sin(halfAngle);
-
-    var axis = Vector3.normalize(axisVec3);
-    this.w = Math.cos(halfAngle);
-    this.x = sin * axis.x;
-    this.y = sin * axis.y;
-    this.z = sin * axis.z;
-
-    return this;
+  get y(): number {
+    return this.v[1];
   }
 
-  add(q: Quaternion) {
-    this.x += q.x;
-    this.y += q.y;
-    this.z += q.z;
-    this.w += q.w;
-
-    return this;
+  set y(y: number) {
+    this.v[1] = y;
   }
 
-  multiply(q: Quaternion) {
-    let result = new Quaternion(0, 0, 0, 1);
-    result.v[0] = q.w * this.x + q.z * this.y + q.y * this.z - q.x * this.w;
-    result.v[1] = - q.z * this.x + q.w * this.y + q.x * this.z - q.y * this.w;
-    result.v[2] = q.y * this.x + q.x * this.y + q.w * this.z - q.z * this.w;
-    result.v[3] = - q.x * this.x - q.y * this.y - q.z * this.z - q.w * this.w;
-    this.x = result.x;
-    this.y = result.y;
-    this.z = result.z;
-    this.w = result.w;
-
-    return this;
+  get z(): number {
+    return this.v[2];
   }
 
-  fromMatrix(m: Matrix44) {
+  set z(z: number) {
+    this.v[2] = z;
+  }
 
-    let tr = m.m00 + m.m11 + m.m22;
+  get w(): number {
+    return this.v[3];
+  }
 
-    if (tr > 0) {
-      let S = 0.5 / Math.sqrt(tr + 1.0);
-      this.v[0] = (m.m21 - m.m12) * S;
-      this.v[1] = (m.m02 - m.m20) * S;
-      this.v[2] = (m.m10 - m.m01) * S;
-      this.v[3] = 0.25 / S;
-    } else if ((m.m00 > m.m11) && (m.m00 > m.m22)) {
-      let S = Math.sqrt(1.0 + m.m00 - m.m11 - m.m22) * 2;
-      this.v[0] = 0.25 * S;
-      this.v[1] = (m.m01 + m.m10) / S;
-      this.v[2] = (m.m02 + m.m20) / S;
-      this.v[3] = (m.m21 - m.m12) / S;
-    } else if (m.m11 > m.m22) {
-      let S = Math.sqrt(1.0 + m.m11 - m.m00 - m.m22) * 2;
-      this.v[0] = (m.m01 + m.m10) / S;
-      this.v[1] = 0.25 * S;
-      this.v[2] = (m.m12 + m.m21) / S;
-      this.v[3] = (m.m02 - m.m20) / S;
-    } else {
-      let S = Math.sqrt(1.0 + m.m22 - m.m00 - m.m11) * 2;
-      this.v[0] = (m.m02 + m.m20) / S;
-      this.v[1] = (m.m12 + m.m21) / S;
-      this.v[2] = 0.25 * S;
-      this.v[3] = (m.m10 - m.m01) / S;
-    }
+  set w(w: number) {
+    this.v[3] = w;
+  }
 
-    return this;
+  get raw() {
+    return this.v;
   }
 
   static get compositionType() {
     return CompositionType.Vec4;
   }
 
-  setAt(i: number, val: number) {
-    switch (i % 4) {
-      case 0: this.x = val; break;
-      case 1: this.y = val; break;
-      case 2: this.z = val; break;
-      case 3: this.w = val; break;
-    }
-    return this;
-  }
-
-  normalize() {
-    let norm = Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z + this.w * this.w);
-    this.x /= norm;
-    this.y /= norm;
-    this.z /= norm;
-    this.w /= norm;
-    return this;
-  }
-
-  identity() {
-    this.x = 0;
-    this.y = 0;
-    this.x = 0;
-    this.w = 1;
-    return this;
+  static dummy() {
+    return new MutableQuaternion(null);
   }
 
   static fromMatrixTo(m: Matrix44, q: MutableQuaternion) {
@@ -187,39 +123,103 @@ export default class MutableQuaternion extends Quaternion implements IVector4 {
     return q;
   }
 
-  get x(): number {
-    return this.v[0];
+  clone() {
+    return new MutableQuaternion(this.x, this.y, this.z, this.w);
   }
 
-  set x(x: number) {
-    this.v[0] = x;
+  identity() {
+    this.x = 0;
+    this.y = 0;
+    this.x = 0;
+    this.w = 1;
+    return this;
   }
 
-  get y(): number {
-    return this.v[1];
+  normalize() {
+    let norm = Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z + this.w * this.w);
+    this.x /= norm;
+    this.y /= norm;
+    this.z /= norm;
+    this.w /= norm;
+    return this;
   }
 
-  set y(y: number) {
-    this.v[1] = y;
+  setAt(i: number, val: number) {
+    switch (i % 4) {
+      case 0: this.x = val; break;
+      case 1: this.y = val; break;
+      case 2: this.z = val; break;
+      case 3: this.w = val; break;
+    }
+    return this;
   }
 
-  get z(): number {
-    return this.v[2];
+  fromMatrix(m: Matrix44) {
+
+    let tr = m.m00 + m.m11 + m.m22;
+
+    if (tr > 0) {
+      let S = 0.5 / Math.sqrt(tr + 1.0);
+      this.v[0] = (m.m21 - m.m12) * S;
+      this.v[1] = (m.m02 - m.m20) * S;
+      this.v[2] = (m.m10 - m.m01) * S;
+      this.v[3] = 0.25 / S;
+    } else if ((m.m00 > m.m11) && (m.m00 > m.m22)) {
+      let S = Math.sqrt(1.0 + m.m00 - m.m11 - m.m22) * 2;
+      this.v[0] = 0.25 * S;
+      this.v[1] = (m.m01 + m.m10) / S;
+      this.v[2] = (m.m02 + m.m20) / S;
+      this.v[3] = (m.m21 - m.m12) / S;
+    } else if (m.m11 > m.m22) {
+      let S = Math.sqrt(1.0 + m.m11 - m.m00 - m.m22) * 2;
+      this.v[0] = (m.m01 + m.m10) / S;
+      this.v[1] = 0.25 * S;
+      this.v[2] = (m.m12 + m.m21) / S;
+      this.v[3] = (m.m02 - m.m20) / S;
+    } else {
+      let S = Math.sqrt(1.0 + m.m22 - m.m00 - m.m11) * 2;
+      this.v[0] = (m.m02 + m.m20) / S;
+      this.v[1] = (m.m12 + m.m21) / S;
+      this.v[2] = 0.25 * S;
+      this.v[3] = (m.m10 - m.m01) / S;
+    }
+
+    return this;
   }
 
-  set z(z: number) {
-    this.v[2] = z;
+  axisAngle(axisVec3: Vector3, radian: number) {
+    var halfAngle = 0.5 * radian;
+    var sin = Math.sin(halfAngle);
+
+    var axis = Vector3.normalize(axisVec3);
+    this.w = Math.cos(halfAngle);
+    this.x = sin * axis.x;
+    this.y = sin * axis.y;
+    this.z = sin * axis.z;
+
+    return this;
   }
 
-  get w(): number {
-    return this.v[3];
+  add(q: Quaternion) {
+    this.x += q.x;
+    this.y += q.y;
+    this.z += q.z;
+    this.w += q.w;
+
+    return this;
   }
 
-  set w(w: number) {
-    this.v[3] = w;
-  }
+  multiply(q: Quaternion) {
+    let result = new Quaternion(0, 0, 0, 1);
+    result.v[0] = q.w * this.x + q.z * this.y + q.y * this.z - q.x * this.w;
+    result.v[1] = - q.z * this.x + q.w * this.y + q.x * this.z - q.y * this.w;
+    result.v[2] = q.y * this.x + q.x * this.y + q.w * this.z - q.z * this.w;
+    result.v[3] = - q.x * this.x - q.y * this.y - q.z * this.z - q.w * this.w;
+    this.x = result.x;
+    this.y = result.y;
+    this.z = result.z;
+    this.w = result.w;
 
-  get raw() {
-    return this.v;
+    return this;
   }
 }
