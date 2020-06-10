161a4e0b9b3589c79543429c3f4bc122f36578a4
KaY <d.guitar.c@gmail.com>
2020-06-10 17:03:54 +0900

    fix: remove 'rhodonite-webgl(.min).js' part

--- versioning.sh
+++ versioning.sh
@@ -6,16 +6,8 @@ echo "(0,eval)('this').Rn.VERSION='"$(cat ./VERSION-FILE)" branch: "$(git symbol
 echo "" >> ./dist/rhodonite.min.js
 echo "(0,eval)('this').Rn.VERSION='"$(cat ./VERSION-FILE)" branch: "$(git symbolic-ref --short HEAD)"';" >> ./dist/rhodonite.min.js
 
-echo "" >> ./dist/rhodonite-webgl.js
-echo "(0,eval)('this').Rn.WEBGL_VERSION='"$(cat ./VERSION-FILE)" branch: "$(git symbolic-ref --short HEAD)"';" >> ./dist/rhodonite-webgl.js
-
-echo "" >> ./dist/rhodonite-webgl.min.js
-echo "(0,eval)('this').Rn.WEBGL_VERSION='"$(cat ./VERSION-FILE)" branch: "$(git symbolic-ref --short HEAD)"';" >> ./dist/rhodonite-webgl.min.js
-
 echo "built branch: "$(git symbolic-ref --short HEAD)
 
 echo "branch: "$(git symbolic-ref --short HEAD) >> VERSION-FILE
 echo $(shasum -a 256 ./dist/rhodonite.js) >> VERSION-FILE
 echo $(shasum -a 256 ./dist/rhodonite.min.js) >> VERSION-FILE
-echo $(shasum -a 256 ./dist/rhodonite-webgl.js) >> VERSION-FILE
-echo $(shasum -a 256 ./dist/rhodonite-webgl.min.js) >> VERSION-FILE
