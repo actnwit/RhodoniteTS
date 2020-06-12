30102a209cdb820d541fdf61d4c5e45b9dca121a
KaY <d.guitar.c@gmail.com>
2020-06-12 16:03:38 +0900

    feat: add useful static methods

--- src/foundation/math/Vector2.ts
+++ src/foundation/math/Vector2.ts
@@ -1,6 +1,7 @@
-import { IVector2, IVector3, IVector4, IVector } from "./IVector";
+import { IVector2, IVector3, IVector4, IVector, IMutableVector2 } from "./IVector";
 import { TypedArray, TypedArrayConstructor } from "../../commontypes/CommonTypes";
 import { MathUtil } from "./MathUtil";
+import { CompositionType } from "../definitions/CompositionType";
 
 export class Vector2_<T extends TypedArrayConstructor> implements IVector, IVector2 {
   v: TypedArray;
@@ -40,16 +41,192 @@ export class Vector2_<T extends TypedArrayConstructor> implements IVector, IVect
     return `ivec2(${Math.floor(this.x)}, ${Math.floor(this.y)})`;
   }
 
-  static add<T extends TypedArrayConstructor>(lvec: Vector2_<T>, rvec: Vector2_<T>) {
-    return new (lvec.constructor as any)(lvec.x + rvec.x, lvec.y + rvec.y);
+  static get compositionType() {
+    return CompositionType.Vec2;
   }
 
-  static subtract<T extends TypedArrayConstructor>(lvec: Vector2_<T>, rvec: Vector2_<T>) {
-    return new (lvec.constructor as any)(lvec.x - rvec.x, lvec.y - rvec.y);
+  /**
+    * to square length(static version)
+    */
+  static lengthSquared(vec: IVector2) {
+    return vec.lengthSquared();
   }
 
-  static multiply<T extends TypedArrayConstructor>(vec2: Vector2_<T>, val: number) {
-    return new (vec2.constructor as any)(vec2.x * val, vec2.y * val);
+  static lengthBtw(l_vec: IVector2, r_vec: IVector2) {
+    return l_vec.lengthTo(r_vec);
+  }
+
+  static angleOfVectors(l_vec: IVector2, r_vec: IVector2) {
+    const multipliedLength = l_vec.length() * r_vec.length();
+    if (multipliedLength === 0) {
+      console.error('length of a vector is 0!');
+    }
+    const cos_sita = l_vec.dot(r_vec) / multipliedLength;
+    const sita = Math.acos(cos_sita);
+    return sita;
+  }
+
+  static _zero(type: TypedArrayConstructor) {
+    return new this(0, 0, { type });
+  }
+
+  static _one(type: TypedArrayConstructor) {
+    return new this(1, 1, { type });
+  }
+
+  static _dummy(type: TypedArrayConstructor) {
+    return new this(null, 0, { type });
+  }
+
+  /**
+   * normalize(static version)
+   */
+  static _normalize(vec: IVector2, type: TypedArrayConstructor) {
+    const length = vec.length();
+    return this._divide(vec, length, type);
+  }
+
+  /**
+   * add value（static version）
+   */
+  static _add(l_vec: IVector2, r_vec: IVector2, type: TypedArrayConstructor) {
+    const x = l_vec.v[0] + r_vec.v[0];
+    const y = l_vec.v[1] + r_vec.v[1];
+    return new this(x, y, { type });
+  }
+
+  /**
+    * add value（static version）
+    */
+  static addTo(l_vec: IVector2, r_vec: IVector2, out: IMutableVector2) {
+    out.v[0] = l_vec.v[0] + r_vec.v[0];
+    out.v[1] = l_vec.v[1] + r_vec.v[1];
+    return out;
+  }
+
+  /**
+    * subtract value(static version)
+    */
+  static _subtract(l_vec: IVector2, r_vec: IVector2, type: TypedArrayConstructor) {
+    const x = l_vec.v[0] - r_vec.v[0];
+    const y = l_vec.v[1] - r_vec.v[1];
+    return new this(x, y, { type });
+  }
+
+  /**
+   * subtract value(static version)
+   */
+  static subtractTo(l_vec: IVector2, r_vec: IVector2, out: IMutableVector2) {
+    out.v[0] = l_vec.v[0] - r_vec.v[0];
+    out.v[1] = l_vec.v[1] - r_vec.v[1];
+    return out;
+  }
+
+  /**
+   * multiply value(static version)
+   */
+  static _multiply(vec: IVector2, value: number, type: TypedArrayConstructor) {
+    const x = vec.v[0] * value;
+    const y = vec.v[1] * value;
+    return new this(x, y, { type });
+  }
+
+  /**
+   * multiply value(static version)
+   */
+  static multiplyTo(vec: IVector2, value: number, out: IMutableVector2) {
+    out.v[0] = vec.v[0] * value;
+    out.v[1] = vec.v[1] * value;
+    return out;
+  }
+
+  /**
+    * multiply vector(static version)
+    */
+  static _multiplyVector(l_vec: IVector2, r_vec: IVector2, type: TypedArrayConstructor) {
+    const x = l_vec.v[0] * r_vec.v[0];
+    const y = l_vec.v[1] * r_vec.v[1];
+    return new this(x, y, { type });
+  }
+
+  /**
+    * multiply vector(static version)
+    */
+  static multiplyVectorTo(l_vec: IVector2, r_vec: IVector2, out: IMutableVector2) {
+    out.v[0] = l_vec.v[0] * r_vec.v[0];
+    out.v[1] = l_vec.v[1] * r_vec.v[1];
+    return out;
+  }
+
+  /**
+   * divide by value(static version)
+   */
+  static _divide(vec: IVector2, value: number, type: TypedArrayConstructor) {
+    let x;
+    let y;
+    if (value !== 0) {
+      x = vec.v[0] / value;
+      y = vec.v[1] / value;
+    } else {
+      console.error("0 division occurred!");
+      x = Infinity;
+      y = Infinity;
+    }
+    return new this(x, y, { type });
+  }
+
+  /**
+   * divide by value(static version)
+   */
+  static divideTo(vec: IVector2, value: number, out: IMutableVector2) {
+    if (value !== 0) {
+      out.v[0] = vec.v[0] / value;
+      out.v[1] = vec.v[1] / value;
+    } else {
+      console.error("0 division occurred!");
+      out.v[0] = Infinity;
+      out.v[1] = Infinity;
+    }
+    return out;
+  }
+
+  /**
+   * divide by vector(static version)
+   */
+  static _divideVector(l_vec: IVector2, r_vec: IVector2, type: TypedArrayConstructor) {
+    let x;
+    let y;
+    if (r_vec.v[0] !== 0 && r_vec.v[1] !== 0) {
+      x = l_vec.v[0] / r_vec.v[0];
+      y = l_vec.v[1] / r_vec.v[1];
+    } else {
+      console.error("0 division occurred!");
+      x = r_vec.v[0] === 0 ? Infinity : l_vec.v[0] / r_vec.v[0];
+      y = r_vec.v[1] === 0 ? Infinity : l_vec.v[1] / r_vec.v[1];
+    }
+    return new this(x, y, { type });
+  }
+
+  /**
+   * divide by vector(static version)
+   */
+  static divideVectorTo(l_vec: IVector2, r_vec: IVector2, out: IMutableVector2) {
+    if (r_vec.v[0] !== 0 && r_vec.v[1] !== 0) {
+      out.v[0] = l_vec.v[0] / r_vec.v[0];
+      out.v[1] = l_vec.v[1] / r_vec.v[1];
+    } else {
+      console.error("0 division occurred!");
+      out.v[0] = r_vec.v[0] === 0 ? Infinity : l_vec.v[0] / r_vec.v[0];
+      out.v[1] = r_vec.v[1] === 0 ? Infinity : l_vec.v[1] / r_vec.v[1];
+    }
+    return out;
+  }
+
+  /**
+   * dot product(static version)
+   */
+  static dot(l_vec: IVector2, r_vec: IVector2) {
+    return l_vec.dot(r_vec);
   }
 
   /**
@@ -128,15 +305,39 @@ export default class Vector2 extends Vector2_<Float32ArrayConstructor> {
   }
 
   static zero() {
-    return new Vector2(0, 0);
+    return super._zero(Float32Array) as Vector2;
   }
 
   static one() {
-    return new Vector2(1, 1);
+    return super._one(Float32Array) as Vector2;
   }
 
   static dummy() {
-    return new Vector2(null, 0);
+    return super._dummy(Float32Array) as Vector2;
+  }
+
+  static add(l_vec: IVector2, r_vec: IVector2) {
+    return super._add(l_vec, r_vec, Float32Array) as Vector2;
+  }
+
+  static subtract(l_vec: IVector2, r_vec: IVector2) {
+    return super._subtract(l_vec, r_vec, Float32Array) as Vector2;
+  }
+
+  static multiply(vec: IVector2, value: number) {
+    return super._multiply(vec, value, Float32Array) as Vector2;
+  }
+
+  static multiplyVector(l_vec: IVector2, r_vec: IVector2) {
+    return super._multiplyVector(l_vec, r_vec, Float32Array) as Vector2;
+  }
+
+  static divide(vec: IVector2, value: number) {
+    return super._divide(vec, value, Float32Array) as Vector2;
+  }
+
+  static divideVector(l_vec: IVector2, r_vec: IVector2) {
+    return super._divideVector(l_vec, r_vec, Float32Array) as Vector2;
   }
 
   clone() {
@@ -150,15 +351,39 @@ export class Vector2d extends Vector2_<Float64ArrayConstructor> {
   }
 
   static zero() {
-    return new Vector2d(0, 0);
+    return super._zero(Float64Array) as Vector2d;
   }
 
   static one() {
-    return new Vector2d(1, 1);
+    return super._one(Float64Array) as Vector2d;
   }
 
   static dummy() {
-    return new Vector2d(null, 0);
+    return super._dummy(Float64Array) as Vector2d;
+  }
+
+  static add(l_vec: IVector2, r_vec: IVector2) {
+    return super._add(l_vec, r_vec, Float64Array) as Vector2d;
+  }
+
+  static subtract(l_vec: IVector2, r_vec: IVector2) {
+    return super._subtract(l_vec, r_vec, Float64Array) as Vector2d;
+  }
+
+  static multiply(vec: IVector2, value: number) {
+    return super._multiply(vec, value, Float64Array) as Vector2d;
+  }
+
+  static multiplyVector(l_vec: IVector2, r_vec: IVector2) {
+    return super._multiplyVector(l_vec, r_vec, Float64Array) as Vector2d;
+  }
+
+  static divide(vec: IVector2, value: number) {
+    return super._divide(vec, value, Float64Array) as Vector2d;
+  }
+
+  static divideVector(l_vec: IVector2, r_vec: IVector2) {
+    return super._divideVector(l_vec, r_vec, Float64Array) as Vector2d;
   }
 
   clone() {
