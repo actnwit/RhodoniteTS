bf6344366c38eabbd2b3851cdd97af47058aff92
KaY <d.guitar.c@gmail.com>
2020-06-15 19:19:14 +0900

    feat: ColorRgb inherits Vector3

--- src/foundation/math/ColorRgb.ts
+++ src/foundation/math/ColorRgb.ts
@@ -1,42 +1,11 @@
-//import GLBoost from './../../globals';
-import Vector2 from './Vector2';
-import Vector4 from './Vector4';
-import {IVector3} from './IVector';
-import {IColorRgb} from './IColor';
+import Vector3 from './Vector3';
+import { IVector3, IVector4 } from './IVector';
+import { IColorRgb } from './IColor';
 import { TypedArray } from '../../commontypes/CommonTypes';
 
-export default class ColorRgb implements IVector3, IColorRgb {
-  v: TypedArray;
-
-  constructor(r?:number|TypedArray|IVector3|Vector4|Array<number>|ColorRgb|null, g?:number, b?:number) {
-    if (ArrayBuffer.isView(r)) {
-      this.v = ((r as any) as TypedArray);
-      return;
-    } else if (r == null) {
-      this.v = new Float32Array(0);
-      return;
-    } else {
-      this.v = new Float32Array(3);
-    }
-
-    if (r == null) {
-      this.v[0] = 1;
-      this.v[1] = 1;
-      this.v[2] = 1;
-    } else if (Array.isArray(r)) {
-      this.v[0] = r[0];
-      this.v[1] = r[1];
-      this.v[2] = r[2];
-    } else if (typeof (r as any).z !== 'undefined') {
-      this.v[0] = (r as any).x;
-      this.v[1] = (r as any).y;
-      this.v[2] = (r as any).z;
-    } else {
-      this.v[0] = ((r as any) as number);
-      this.v[1] = ((g as any) as number);
-      this.v[2] = ((b as any) as number);
-    }
-
+export default class ColorRgb extends Vector3 implements IVector3, IColorRgb {
+  constructor(r: number | TypedArray | IVector3 | IVector4 | Array<number> | null, g?: number, b?: number) {
+    super(r, g, b);
   }
 
   get x() {
