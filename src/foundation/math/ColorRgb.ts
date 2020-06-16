ebc0b49818dde9847e47b9653433a9ca680166fb
KaY <d.guitar.c@gmail.com>
2020-06-16 15:24:03 +0900

    feat: add and refactor static methods in (Mutable)ColorRgb class

--- src/foundation/math/ColorRgb.ts
+++ src/foundation/math/ColorRgb.ts
@@ -39,4 +39,52 @@ export default class ColorRgb extends Vector3 implements IVector3, IColorRgb {
   get a() {
     return 1;
   }
+
+  static zero() {
+    return super._zero(Float32Array) as ColorRgb;
+  }
+
+  static one() {
+    return super._one(Float32Array) as ColorRgb;
+  }
+
+  static dummy() {
+    return super._dummy(Float32Array) as ColorRgb;
+  }
+
+  static normalize(vec: IVector3) {
+    return super._normalize(vec, Float32Array) as ColorRgb;
+  }
+
+  static add(l_vec: IVector3, r_vec: IVector3) {
+    return super._add(l_vec, r_vec, Float32Array) as ColorRgb;
+  }
+
+  static subtract(l_vec: IVector3, r_vec: IVector3) {
+    return super._subtract(l_vec, r_vec, Float32Array) as ColorRgb;
+  }
+
+  static multiply(vec: IVector3, value: number) {
+    return super._multiply(vec, value, Float32Array) as ColorRgb;
+  }
+
+  static multiplyVector(l_vec: IVector3, r_vec: IVector3) {
+    return super._multiplyVector(l_vec, r_vec, Float32Array) as ColorRgb;
+  }
+
+  static divide(vec: IVector3, value: number) {
+    return super._divide(vec, value, Float32Array) as ColorRgb;
+  }
+
+  static divideVector(l_vec: IVector3, r_vec: IVector3) {
+    return super._divideVector(l_vec, r_vec, Float32Array) as ColorRgb;
+  }
+
+  static cross(l_vec: IVector3, r_vec: IVector3) {
+    return super._cross(l_vec, r_vec, Float32Array) as ColorRgb;
+  }
+
+  clone(): ColorRgb {
+    return super.clone() as ColorRgb;
+  }
 }
--- src/foundation/math/MutableColorRgb.ts
+++ src/foundation/math/MutableColorRgb.ts
@@ -64,4 +64,52 @@ export default class MutableColorRgb extends MutableVector3 implements IVector3,
   get a() {
     return 1;
   }
+
+  static zero() {
+    return super._zero(Float32Array) as MutableColorRgb;
+  }
+
+  static one() {
+    return super._one(Float32Array) as MutableColorRgb;
+  }
+
+  static dummy() {
+    return super._dummy(Float32Array) as MutableColorRgb;
+  }
+
+  static normalize(vec: IVector3) {
+    return super._normalize(vec, Float32Array) as MutableColorRgb;
+  }
+
+  static add(l_vec: IVector3, r_vec: IVector3) {
+    return super._add(l_vec, r_vec, Float32Array) as MutableColorRgb;
+  }
+
+  static subtract(l_vec: IVector3, r_vec: IVector3) {
+    return super._subtract(l_vec, r_vec, Float32Array) as MutableColorRgb;
+  }
+
+  static multiply(vec: IVector3, value: number) {
+    return super._multiply(vec, value, Float32Array) as MutableColorRgb;
+  }
+
+  static multiplyVector(l_vec: IVector3, r_vec: IVector3) {
+    return super._multiplyVector(l_vec, r_vec, Float32Array) as MutableColorRgb;
+  }
+
+  static divide(vec: IVector3, value: number) {
+    return super._divide(vec, value, Float32Array) as MutableColorRgb;
+  }
+
+  static divideVector(l_vec: IVector3, r_vec: IVector3) {
+    return super._divideVector(l_vec, r_vec, Float32Array) as MutableColorRgb;
+  }
+
+  static cross(l_vec: IVector3, r_vec: IVector3) {
+    return super._cross(l_vec, r_vec, Float32Array) as MutableColorRgb;
+  }
+
+  clone(): MutableColorRgb {
+    return super.clone() as MutableColorRgb;
+  }
 }
