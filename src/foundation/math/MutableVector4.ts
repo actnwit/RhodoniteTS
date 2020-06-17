0f6f4a9405d16ee7a55761847b86574984df9b0d
KaY <d.guitar.c@gmail.com>
2020-06-17 10:14:58 +0900

    improvement: remove a method that exists in an immutable class
    clone method move to bottom

--- src/foundation/math/MutableVector4.ts
+++ src/foundation/math/MutableVector4.ts
@@ -45,10 +45,6 @@ export class MutableVector4_<T extends TypedArrayConstructor> extends Vector4_<T
     return this.v;
   }
 
-  static get compositionType() {
-    return CompositionType.Vec4;
-  }
-
   copyComponents(vec: Vector4_<T>) {
     this.v[0] = vec.v[0];
     this.v[1] = vec.v[1];
