50d7f511e849f9d229f8dfdd23c6ec32d1add0df
KaY <d.guitar.c@gmail.com>
2020-06-23 10:22:57 +0900

    improvement: Simplify the test code

--- samples/test_e2e/GltfImporter-specify-noLight/main.js
+++ samples/test_e2e/GltfImporter-specify-noLight/main.js
@@ -7,14 +7,8 @@ const load = async function (time) {
   const system = Rn.System.getInstance();
   system.setProcessApproachAndCanvas(Rn.ProcessApproach.UniformWebGL1, document.getElementById('world'));
 
-  const entityRepository = Rn.EntityRepository.getInstance();
-  const gltfImporter = Rn.GltfImporter.getInstance();
-
-  // params
-
-  const displayResolution = 800;
-
   // camera
+  const entityRepository = Rn.EntityRepository.getInstance();
   const cameraEntity = entityRepository.createEntity([Rn.TransformComponent, Rn.SceneGraphComponent, Rn.CameraComponent, Rn.CameraControllerComponent]);
   const cameraComponent = cameraEntity.getComponent(Rn.CameraComponent);
   cameraComponent.zNear = 0.1;
@@ -22,57 +16,20 @@ const load = async function (time) {
   cameraComponent.setFovyAndChangeFocalLength(30.0);
   cameraComponent.aspect = 1.0;
 
-  // expresions
-  const expressions = [];
-
   // gltf
+  const gltfImporter = Rn.GltfImporter.getInstance();
   const expression = await gltfImporter.import('../../../assets/gltf/2.0/AnimatedTriangle/glTF-Embedded/AnimatedTriangle.gltf', {
     defaultMaterialHelperArgumentArray: [{
       isLighting: false
     }],
-    autoResizeTexture: true,
     cameraComponent: cameraComponent
   });
-  expressions.push(expression);
 
 
   const componentRepository = Rn.ComponentRepository.getInstance();
   const meshComponents = componentRepository.getComponentsWithType(Rn.MeshComponent);
   setParameterForMeshComponents(meshComponents, Rn.ShaderSemantics.BaseColorFactor, new Rn.Vector4(0.5, 0.5, 0.5, 1.0));
 
-  // post effects
-  const expressionPostEffect = new Rn.Expression();
-  expressions.push(expressionPostEffect);
-
-
-  // gamma correction
-  const gammaTargetFramebuffer = Rn.RenderableHelper.createTexturesForRenderTarget(displayResolution, displayResolution, 1, {});
-  for (let renderPass of expression.renderPasses) {
-    renderPass.setFramebuffer(gammaTargetFramebuffer);
-    renderPass.toClearColorBuffer = false;
-    renderPass.toClearDepthBuffer = false;
-  }
-  expression.renderPasses[0].toClearColorBuffer = true;
-  expression.renderPasses[0].toClearDepthBuffer = true;
-
-  const gammaRenderPass = createPostEffectRenderPass('createGammaCorrectionMaterial');
-  setTextureParameterForMeshComponents(gammaRenderPass.meshComponents, Rn.ShaderSemantics.BaseColorTexture, gammaTargetFramebuffer.colorAttachments[0]);
-
-  // fxaa
-  const fxaaTargetFramebuffer = Rn.RenderableHelper.createTexturesForRenderTarget(displayResolution, displayResolution, 1, {});
-  gammaRenderPass.setFramebuffer(fxaaTargetFramebuffer);
-
-  const fxaaRenderPass = createRenderPassSharingEntitiesAndCamera(gammaRenderPass);
-  const fxaaMaterial = Rn.MaterialHelper.createFXAA3QualityMaterial();
-  fxaaMaterial.setParameter(Rn.ShaderSemantics.ScreenInfo, new Rn.Vector2(displayResolution, displayResolution));
-  fxaaMaterial.setTextureParameter(Rn.ShaderSemantics.BaseColorTexture, fxaaTargetFramebuffer.colorAttachments[0]);
-  fxaaRenderPass.setMaterial(fxaaMaterial);
-
-  expressionPostEffect.addRenderPasses([gammaRenderPass, fxaaRenderPass]);
-
-
-  //set default camera
-  Rn.CameraComponent.main = 0;
 
   // cameraController
   const mainRenderPass = expression.renderPasses[0];
@@ -101,8 +58,7 @@ const load = async function (time) {
       }
     }
 
-
-    system.process(expressions);
+    system.process([expression]);
 
     count++;
 
@@ -110,54 +66,6 @@ const load = async function (time) {
   };
 
   draw();
-
-
-  function createPostEffectRenderPass(materialHelperFunctionStr, arrayOfHelperFunctionArgument = []) {
-    const boardPrimitive = new Rn.Plane();
-    boardPrimitive.generate({
-      width: 1, height: 1, uSpan: 1, vSpan: 1, isUVRepeat: false,
-      material: Rn.MaterialHelper[materialHelperFunctionStr](...arrayOfHelperFunctionArgument)
-    });
-
-    const boardEntity = generateEntity();
-    boardEntity.getTransform().rotate = new Rn.Vector3(-Math.PI / 2, 0.0, 0.0);
-    boardEntity.getTransform().translate = new Rn.Vector3(0.0, 0.0, -0.5);
-
-    const boardMesh = new Rn.Mesh();
-    boardMesh.addPrimitive(boardPrimitive);
-    const boardMeshComponent = boardEntity.getComponent(Rn.MeshComponent);
-    boardMeshComponent.setMesh(boardMesh);
-
-    if (createPostEffectRenderPass.cameraComponent == null) {
-      const entityRepository = Rn.EntityRepository.getInstance();
-      const cameraEntity = entityRepository.createEntity([Rn.TransformComponent, Rn.SceneGraphComponent, Rn.CameraComponent]);
-      const cameraComponent = cameraEntity.getComponent(Rn.CameraComponent);
-      cameraComponent.zFarInner = 1.0;
-      createPostEffectRenderPass.cameraComponent = cameraComponent;
-    }
-
-    const renderPass = new Rn.RenderPass();
-    renderPass.toClearColorBuffer = true;
-    renderPass.clearColor = new Rn.Vector4(0.0, 0.0, 0.0, 1.0);
-    renderPass.cameraComponent = createPostEffectRenderPass.cameraComponent;
-    renderPass.addEntities([boardEntity]);
-
-    return renderPass;
-  }
-
-  function createRenderPassSharingEntitiesAndCamera(originalRenderPass) {
-    const renderPass = new Rn.RenderPass();
-    renderPass.addEntities(originalRenderPass.entities);
-    renderPass.cameraComponent = originalRenderPass.cameraComponent;
-
-    return renderPass;
-  }
-
-  function generateEntity() {
-    const repo = Rn.EntityRepository.getInstance();
-    const entity = repo.createEntity([Rn.TransformComponent, Rn.SceneGraphComponent, Rn.MeshComponent, Rn.MeshRendererComponent]);
-    return entity;
-  }
 
   function setParameterForMeshComponents(meshComponents, shaderSemantic, value) {
     for (let i = 0; i < meshComponents.length; i++) {
@@ -171,22 +79,6 @@ const load = async function (time) {
       }
     }
   }
-
-  function setTextureParameterForMeshComponents(meshComponents, shaderSemantic, value) {
-    for (let i = 0; i < meshComponents.length; i++) {
-      const mesh = meshComponents[i].mesh;
-      if (!mesh) continue;
-
-      const primitiveNumber = mesh.getPrimitiveNumber();
-      for (let j = 0; j < primitiveNumber; j++) {
-        const primitive = mesh.getPrimitiveAt(j);
-        primitive.material.setTextureParameter(shaderSemantic, value);
-      }
-    }
-  }
 }
 
 document.body.onload = load;
-
-
-
