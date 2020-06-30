8ee080f806e1eb598b808431b8c253575741c50c
KaY <d.guitar.c@gmail.com>
2020-06-30 14:00:27 +0900

    feat: add test for shadow map

---
+++ samples/test_e2e/ShadowMap/test.js
@@ -0,0 +1,13 @@
+test('regression test ShadowMap', async () => {
+  jest.setTimeout(60000);
+  const page = await browser.newPage();
+  await page.goto('http://localhost:8082/samples/test_e2e/ShadowMap');
+  await page.setViewport({ width: 1000, height: 1000 });
+  await page.waitForSelector('p#rendered', { timeout: 60000 });
+  const canvasElement = await page.$('#world');
+  const image = await canvasElement.screenshot();
+  expect(image).toMatchImageSnapshot({
+    failureThreshold: 0.003,
+    failureThresholdType: 'percent'
+  });
+});
