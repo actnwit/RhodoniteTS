e305e5cdbcf2012631960a4a97106974bd8a5842
KaY <d.guitar.c@gmail.com>
2020-07-06 16:13:08 +0900

    feat: add test for ibl with a transparent material

---
+++ samples/test_e2e/GltfImporter-ibl-1/main.js
@@ -0,0 +1,147 @@
+const p = document.createElement('p');
+document.body.appendChild(p);
+
+const load = async function () {
+  await Rn.ModuleManager.getInstance().loadModule('webgl');
+  await Rn.ModuleManager.getInstance().loadModule('pbr');
+  const system = Rn.System.getInstance();
+  system.setProcessApproachAndCanvas(Rn.ProcessApproach.UniformWebGL1, document.getElementById('world'));
+
+  // expressions
+  const expressions = [];
+
+  // camera
+  const entityRepository = Rn.EntityRepository.getInstance();
+  const cameraEntity = entityRepository.createEntity([Rn.TransformComponent, Rn.SceneGraphComponent, Rn.CameraComponent, Rn.CameraControllerComponent]);
+  const cameraComponent = cameraEntity.getComponent(Rn.CameraComponent);
+  cameraComponent.zNear = 0.1;
+  cameraComponent.zFar = 1000.0;
+  cameraComponent.setFovyAndChangeFocalLength(30.0);
+  cameraComponent.aspect = 1.0;
+
+  // gltf
+  const gltfImporter = Rn.GltfImporter.getInstance();
+  const mainExpression = await gltfImporter.import('../../../assets/gltf/2.0/FlightHelmet/glTF/FlightHelmet.gltf', {
+    cameraComponent: cameraComponent
+  });
+  expressions.push(mainExpression);
+
+  // post effects
+  const expressionPostEffect = new Rn.Expression();
+  expressions.push(expressionPostEffect);
+
+  // gamma correction (and super sampling)
+  const mainRenderPass = mainExpression.renderPasses[0];
+  const gammaTargetFramebuffer = Rn.RenderableHelper.createTexturesForRenderTarget(600, 600, 1, {});
+  mainRenderPass.setFramebuffer(gammaTargetFramebuffer);
+  mainRenderPass.toClearColorBuffer = true;
+  mainRenderPass.toClearDepthBuffer = true;
+
+  const gammaRenderPass = createPostEffectRenderPass('createGammaCorrectionMaterial');
+  setTextureParameterForMeshComponents(gammaRenderPass.meshComponents, Rn.ShaderSemantics.BaseColorTexture, gammaTargetFramebuffer.colorAttachments[0]);
+
+  expressionPostEffect.addRenderPasses([gammaRenderPass]);
+
+  // cameraController
+  const mainCameraControllerComponent = cameraEntity.getComponent(Rn.CameraControllerComponent);
+  const controller = mainCameraControllerComponent.controller;
+  controller.dolly = 0.65;
+  controller.setTarget(mainRenderPass.sceneTopLevelGraphComponents[0].entity);
+
+  // lighting
+  setIBL('./../../../assets/ibl/papermill');
+
+  let count = 0;
+
+  const draw = function () {
+    if (count > 100) {
+      p.id = 'rendered';
+      p.innerText = 'Rendered.';
+    } else if (count === 1) {
+      p.id = 'started';
+      p.innerText = 'Started.';
+    }
+
+    system.process(expressions);
+
+    count++;
+
+    requestAnimationFrame(draw);
+  };
+
+  draw();
+
+  function setIBL(baseUri) {
+    const specularCubeTexture = new Rn.CubeTexture();
+    specularCubeTexture.baseUriToLoad = baseUri + '/specular/specular';
+    specularCubeTexture.isNamePosNeg = true;
+    specularCubeTexture.hdriFormat = Rn.HdriFormat.LDR_SRGB;
+    specularCubeTexture.mipmapLevelNumber = 10;
+
+    const diffuseCubeTexture = new Rn.CubeTexture();
+    diffuseCubeTexture.baseUriToLoad = baseUri + '/diffuse/diffuse';
+    diffuseCubeTexture.hdriFormat = Rn.HdriFormat.LDR_SRGB;
+    diffuseCubeTexture.mipmapLevelNumber = 1;
+    diffuseCubeTexture.isNamePosNeg = true;
+
+    const componentRepository = Rn.ComponentRepository.getInstance();
+    const meshRendererComponents = componentRepository.getComponentsWithType(Rn.MeshRendererComponent);
+    for (let i = 0; i < meshRendererComponents.length; i++) {
+      const meshRendererComponent = meshRendererComponents[i];
+      meshRendererComponent.specularCubeMap = specularCubeTexture;
+      meshRendererComponent.diffuseCubeMap = diffuseCubeTexture;
+    }
+  }
+
+  function createPostEffectRenderPass(materialHelperFunctionStr, arrayOfHelperFunctionArgument = []) {
+    const boardPrimitive = new Rn.Plane();
+    boardPrimitive.generate({
+      width: 1, height: 1, uSpan: 1, vSpan: 1, isUVRepeat: false,
+      material: Rn.MaterialHelper[materialHelperFunctionStr].apply(this, arrayOfHelperFunctionArgument)
+    });
+
+    const boardEntity = generateEntity();
+    boardEntity.getTransform().rotate = new Rn.Vector3(-Math.PI / 2, 0.0, 0.0);
+    boardEntity.getTransform().translate = new Rn.Vector3(0.0, 0.0, -0.5);
+
+    const boardMesh = new Rn.Mesh();
+    boardMesh.addPrimitive(boardPrimitive);
+    const boardMeshComponent = boardEntity.getComponent(Rn.MeshComponent);
+    boardMeshComponent.setMesh(boardMesh);
+
+    const entityRepository = Rn.EntityRepository.getInstance();
+    const cameraEntity = entityRepository.createEntity([Rn.TransformComponent, Rn.SceneGraphComponent, Rn.CameraComponent]);
+    const cameraComponent = cameraEntity.getComponent(Rn.CameraComponent);
+    cameraComponent.zFarInner = 1.0;
+
+    const renderPass = new Rn.RenderPass();
+    renderPass.cameraComponent = cameraComponent;
+    renderPass.addEntities([boardEntity]);
+
+    return renderPass;
+  }
+
+  function generateEntity() {
+    const entityRepository = Rn.EntityRepository.getInstance();
+    const entity = entityRepository.createEntity([Rn.TransformComponent, Rn.SceneGraphComponent, Rn.MeshComponent, Rn.MeshRendererComponent]);
+    return entity;
+  }
+
+  function setTextureParameterForMeshComponents(meshComponents, shaderSemantic, value) {
+    for (let i = 0; i < meshComponents.length; i++) {
+      const mesh = meshComponents[i].mesh;
+      if (!mesh) continue;
+
+      const primitiveNumber = mesh.getPrimitiveNumber();
+      for (let j = 0; j < primitiveNumber; j++) {
+        const primitive = mesh.getPrimitiveAt(j);
+        primitive.material.setTextureParameter(shaderSemantic, value);
+      }
+    }
+  }
+
+
+
+};
+
+document.body.onload = load;
