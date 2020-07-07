41a04b15e5dd8906d029e7e80a57914f15520afd
KaY <d.guitar.c@gmail.com>
2020-07-07 19:01:32 +0900

    feat: add test for vertex skinning of BoneDataType.Mat4x4

---
+++ samples/test_e2e/GltfImporter-skinning-mat44/main.js
@@ -0,0 +1,62 @@
+const p = document.createElement('p');
+document.body.appendChild(p);
+
+const load = async function () {
+  Rn.Config.boneDataType = Rn.BoneDataType.Mat4x4;
+
+  await Rn.ModuleManager.getInstance().loadModule('webgl');
+  await Rn.ModuleManager.getInstance().loadModule('pbr');
+  const system = Rn.System.getInstance();
+  system.setProcessApproachAndCanvas(Rn.ProcessApproach.UniformWebGL1, document.getElementById('world'));
+
+
+  // camera
+  const entityRepository = Rn.EntityRepository.getInstance();
+  const cameraEntity = entityRepository.createEntity([Rn.TransformComponent, Rn.SceneGraphComponent, Rn.CameraComponent]);
+  const cameraComponent = cameraEntity.getComponent(Rn.CameraComponent);
+  cameraComponent.zNear = 0.1;
+  cameraComponent.zFar = 1000.0;
+  cameraComponent.setFovyAndChangeFocalLength(30.0);
+  cameraComponent.aspect = 1.0;
+
+  const cameraTransform = cameraEntity.getTransform();
+  cameraTransform.translate = new Rn.Vector3(0, 1, 5);
+
+  // gltf
+  const gltfImporter = Rn.GltfImporter.getInstance();
+  const expression = await gltfImporter.import('../../../assets/gltf/2.0/SimpleSkin/glTF-Embedded/SimpleSkin.gltf', {
+    cameraComponent: cameraComponent,
+  });
+
+  let count = 0;
+  let startTime = Date.now();
+
+  Rn.AnimationComponent.globalTime = 0.03;
+
+  const draw = function () {
+    if (count > 0) {
+      p.id = 'rendered';
+      p.innerText = 'Rendered.';
+    }
+
+    if (window.isAnimating) {
+      const date = new Date();
+      const time = (date.getTime() - startTime) / 1000;
+      Rn.AnimationComponent.globalTime = time;
+      if (time > Rn.AnimationComponent.endInputValue) {
+        startTime = date.getTime();
+      }
+    }
+
+    system.process([expression]);
+
+    count++;
+
+    requestAnimationFrame(draw);
+  };
+
+  draw();
+
+}
+
+document.body.onload = load;
