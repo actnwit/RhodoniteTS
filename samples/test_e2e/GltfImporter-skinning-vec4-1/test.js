b143379f82ac3a576e80094cc3f0e1e6a832e14d
KaY <d.guitar.c@gmail.com>
2020-07-08 17:58:44 +0900

    feat: add test for vertex skinning of BoneDataType.Vec4x1

---
+++ samples/test_e2e/GltfImporter-skinning-vec4-1/test.js
@@ -0,0 +1,13 @@
+test('regression test GltfImporter-skinning-vec4-1', async () => {
+  jest.setTimeout(600000);
+  const page = await browser.newPage();
+  await page.goto('http://localhost:8082/samples/test_e2e/GltfImporter-skinning-vec4-1');
+  await page.setViewport({ width: 1000, height: 1000 });
+  await page.waitForSelector('p#rendered', { timeout: 600000 });
+  const canvasElement = await page.$('#world');
+  const image = await canvasElement.screenshot();
+  expect(image).toMatchImageSnapshot({
+    failureThreshold: 0.03,
+    failureThresholdType: 'percent'
+  });
+});
