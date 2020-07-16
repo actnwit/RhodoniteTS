68f464f127fa929b507844ccac5968d8404bc143
KaY <d.guitar.c@gmail.com>
2020-07-16 10:57:17 +0900

    refactor: remove unused import statement

--- src/foundation/components/BlendShapeComponent.ts
+++ src/foundation/components/BlendShapeComponent.ts
@@ -1,23 +1,9 @@
-import Vector3 from '../math/Vector3';
-import Quaternion from '../math/Quaternion';
-import Matrix33 from '../math/Matrix33';
-import Matrix44 from '../math/Matrix44';
 import Component from '../core/Component';
 import ComponentRepository from '../core/ComponentRepository';
-import { ComponentType } from '../definitions/ComponentType';
 import EntityRepository from '../core/EntityRepository';
 import { WellKnownComponentTIDs } from './WellKnownComponentTIDs';
-import { BufferUse, BufferUseEnum } from '../definitions/BufferUse';
-import SceneGraphComponent from './SceneGraphComponent';
-import MutableMatrix44 from '../math/MutableMatrix44';
-import MutableQuaternion from '../math/MutableQuaternion';
 import { ProcessStage } from '../definitions/ProcessStage';
-import MutableMatrix33 from '../math/MutableMatrix33';
-import MutableVector3 from '../math/MutableVector3';
 import { ComponentTID, ComponentSID, EntityUID } from '../../commontypes/CommonTypes';
-import MeshComponent from './MeshComponent';
-
-// import AnimationComponent from './AnimationComponent';
 
 export default class BlendShapeComponent extends Component {
   private __weights: number[] = [];
