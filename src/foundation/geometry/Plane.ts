f5e8bf1a02dfeb9e77e22f52bcecbe422797ff34
KaY <d.guitar.c@gmail.com>
2020-07-20 17:23:14 +0900

    doc: add document

--- src/foundation/geometry/Plane.ts
+++ src/foundation/geometry/Plane.ts
@@ -14,6 +14,16 @@ export default class Plane extends Primitive {
     super();
   }
 
+  /**
+   * Generates a plane object
+   * @param width the length of U(X)-axis direction
+   * @param height the length of V(Y)-axis direction
+   * @param uSpan number of spans in U(X)-axis direction
+   * @param vSpan number of spans in V(Y)-axis direction
+   * @param isUVRepeat draw uSpan times vSpan number of textures
+   * @param flipTextureCoordinateY draw textures by flipping on the V(Y)-axis
+   * @param material attach a rhodonite material to this plane(the default material is the classicUberMaterial)
+   */
   generate({ width, height, uSpan, vSpan, isUVRepeat = false, flipTextureCoordinateY = false, material }:
     { width: Size, height: Size, uSpan: Size, vSpan: Size, isUVRepeat: boolean, flipTextureCoordinateY: boolean, material?: Material }) {
     var positions = [];
