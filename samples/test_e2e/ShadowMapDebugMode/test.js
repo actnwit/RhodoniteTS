d321bfc71e92590e1870b216eed3c63e67fb473f
KaY <d.guitar.c@gmail.com>
2020-08-06 18:11:03 +0900

    feat: add e2e test of the shadow map in debug mode

---
+++ samples/test_e2e/ShadowMapDebugMode/test.js
@@ -0,0 +1,13 @@
+test('regression test ShadowMapDebugMode', async () => {
+  jest.setTimeout(60000);
+  const page = await browser.newPage();
+  await page.goto('http://localhost:8082/samples/test_e2e/ShadowMapDebugMode');
+  await page.setViewport({ width: 1000, height: 1000 });
+  await page.waitForSelector('p#rendered', { timeout: 60000 });
+  const canvasElement = await page.$('#world');
+  const image = await canvasElement.screenshot();
+  expect(image).toMatchImageSnapshot({
+    failureThreshold: 0.01, // To counteract the error in depth encode shader
+    failureThresholdType: 'percent'
+  });
+});
