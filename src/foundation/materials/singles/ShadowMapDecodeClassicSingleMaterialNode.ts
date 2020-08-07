1c70fc3f888b59753ccd324c297e6df0379eb84b
KaY <d.guitar.c@gmail.com>
2020-08-07 17:01:36 +0900

    doc: add comments

--- src/foundation/materials/singles/ShadowMapDecodeClassicSingleMaterialNode.ts
+++ src/foundation/materials/singles/ShadowMapDecodeClassicSingleMaterialNode.ts
@@ -37,6 +37,15 @@ export default class ShadowMapDecodeClassicSingleMaterialNode extends AbstractMa
 
   private __encodedDepthRenderPass: RenderPass;
 
+  /**
+   * The constructor of the ShadowMapDecodeClassicSingleMaterialNode
+   * @param isMorphing True if the morphing is to be applied
+   * @param isSkinning True if the skeleton is to be applied
+   * @param isLighting True if the lighting is to be applied. When isLighting is false, the Shader draws the original color of the material, except for the shadow area.
+   * @param isDebugging True if the shader displays the DebugColorFactor color in areas outside of the depth map.
+   * @param colorAttachmentsNumber The index of colorAttachment in a framebuffer. The colorAttachment must have depth information drawn by the DepthEncodeSingleMaterialNode.
+   * @param encodedDepthRenderPass The render pass where the depth information from the DepthEncodeSingleMaterialNode is drawn to the frame buffer
+   */
   constructor(
     { isMorphing, isSkinning, isLighting, isDebugging, colorAttachmentsNumber }: { isMorphing: boolean, isSkinning: boolean, isLighting: boolean, isDebugging: boolean, colorAttachmentsNumber: Count },
     encodedDepthRenderPass: RenderPass
