9fdce4ad4b2ca7edb69cd979b134ece59cb52c7c
KaY <d.guitar.c@gmail.com>
2020-08-25 11:58:13 +0900

    feat: add four parameter definitions

--- src/foundation/definitions/TextureParameter.ts
+++ src/foundation/definitions/TextureParameter.ts
@@ -14,6 +14,9 @@ class TextureParameterClass extends EnumClass implements TextureParameterEnum {
 
 const Nearest: TextureParameterEnum = new TextureParameterClass({ index: 0x2600, str: 'NEAREST' });
 const Linear: TextureParameterEnum = new TextureParameterClass({ index: 0x2601, str: 'LINEAR' });
+const NearestMipmapNearest: TextureParameterEnum = new TextureParameterClass({ index: 0x2700, str: 'NEAREST_MIPMAP_NEAREST' });
+const LinearMipmapNearest: TextureParameterEnum = new TextureParameterClass({ index: 0x2701, str: 'LINEAR_MIPMAP_NEAREST' });
+const NearestMipmapLinear: TextureParameterEnum = new TextureParameterClass({ index: 0x2702, str: 'NEAREST_MIPMAP_LINEAR' });
 const LinearMipmapLinear: TextureParameterEnum = new TextureParameterClass({ index: 0x2703, str: 'LINEAR_MIPMAP_LINEAR' });
 const TextureMagFilter: TextureParameterEnum = new TextureParameterClass({ index: 0x2800, str: 'TEXTURE_MAG_FILTER' });
 const TextureMinFilter: TextureParameterEnum = new TextureParameterClass({ index: 0x2801, str: 'TEXTURE_MIN_FILTER' });
@@ -26,6 +29,7 @@ const Texture1: TextureParameterEnum = new TextureParameterClass({ index: 0x84C1
 const ActiveTexture: TextureParameterEnum = new TextureParameterClass({ index: 0x84E0, str: 'ACTIVE_TEXTURE' });
 const Repeat: TextureParameterEnum = new TextureParameterClass({ index: 0x2901, str: 'REPEAT' });
 const ClampToEdge: TextureParameterEnum = new TextureParameterClass({ index: 0x812F, str: 'CLAMP_TO_EDGE' });
+const MirroredRepeat: TextureParameterEnum = new TextureParameterClass({ index: 0x8370, str: 'MIRRORED_REPEAT' });
 const RGB8: TextureParameterEnum = new TextureParameterClass({ index: 0x8051, str: 'RGB8' });
 const RGBA8: TextureParameterEnum = new TextureParameterClass({ index: 0x8058, str: 'RGBA8' });
 const RGB10_A2: TextureParameterEnum = new TextureParameterClass({ index: 0x8059, str: 'RGB10_A2' });
@@ -39,14 +43,14 @@ const Depth32F: TextureParameterEnum = new TextureParameterClass({ index: 0x8CAC
 const Depth24Stencil8: TextureParameterEnum = new TextureParameterClass({ index: 0x88F0, str: 'DEPTH24_STENCIL8' });
 const Depth32FStencil8: TextureParameterEnum = new TextureParameterClass({ index: 0x8CAD, str: 'DEPTH32F_STENCIL8' });
 
-const typeList = [Nearest, Linear, LinearMipmapLinear, TextureMagFilter, TextureMinFilter, TextureWrapS, TextureWrapT, Texture2D, Texture, Texture0, Texture1, ActiveTexture,
-  Repeat, ClampToEdge, RGB8, RGBA8, RGB10_A2, RGB16F, RGB32F, RGBA16F, RGBA32F, Depth16, Depth24, Depth32F, Depth24Stencil8, Depth32FStencil8];
+const typeList = [Nearest, Linear, NearestMipmapNearest, LinearMipmapNearest, NearestMipmapLinear, LinearMipmapLinear, TextureMagFilter, TextureMinFilter, TextureWrapS, TextureWrapT, Texture2D, Texture, Texture0, Texture1, ActiveTexture,
+  Repeat, ClampToEdge, MirroredRepeat, RGB8, RGBA8, RGB10_A2, RGB16F, RGB32F, RGBA16F, RGBA32F, Depth16, Depth24, Depth32F, Depth24Stencil8, Depth32FStencil8];
 
 function from(index: number): TextureParameterEnum {
   return _from({ typeList, index }) as TextureParameterEnum;
 }
 
 export const TextureParameter = Object.freeze({
-  Nearest, Linear, LinearMipmapLinear, TextureMagFilter, TextureMinFilter, TextureWrapS, TextureWrapT, Texture2D, Texture,
-  Texture0, Texture1, ActiveTexture, Repeat, ClampToEdge, RGB8, RGBA8, RGB10_A2, RGB16F, RGB32F, RGBA16F, RGBA32F, Depth16, Depth24, Depth32F, Depth24Stencil8, Depth32FStencil8, from
+  Nearest, Linear, NearestMipmapNearest, LinearMipmapNearest, NearestMipmapLinear, LinearMipmapLinear, TextureMagFilter, TextureMinFilter, TextureWrapS, TextureWrapT, Texture2D, Texture,
+  Texture0, Texture1, ActiveTexture, Repeat, ClampToEdge, MirroredRepeat, RGB8, RGBA8, RGB10_A2, RGB16F, RGB32F, RGBA16F, RGBA32F, Depth16, Depth24, Depth32F, Depth24Stencil8, Depth32FStencil8, from
 });
