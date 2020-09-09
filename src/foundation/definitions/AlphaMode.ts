e9738c4c3bb03e080a2f0d35f26c93207825cbe1
KaY <d.guitar.c@gmail.com>
2020-09-09 11:47:31 +0900

    chore: code cleaning

--- src/foundation/definitions/AlphaMode.ts
+++ src/foundation/definitions/AlphaMode.ts
@@ -5,23 +5,23 @@ export interface AlphaModeEnum extends EnumIO {
 }
 
 class AlphaModeClass extends EnumClass implements AlphaModeEnum {
-  constructor({index, str} : {index: number, str: string}) {
-    super({index, str});
+  constructor({ index, str }: { index: number, str: string }) {
+    super({ index, str });
   }
 }
 
-const Opaque: AlphaModeEnum = new AlphaModeClass({index:0, str:'OPAQUE'});
-const Mask: AlphaModeEnum = new AlphaModeClass({index:1, str:'MASK'});
-const Blend: AlphaModeEnum = new AlphaModeClass({index:2, str:'BLEND'});
+const Opaque: AlphaModeEnum = new AlphaModeClass({ index: 0, str: 'OPAQUE' });
+const Mask: AlphaModeEnum = new AlphaModeClass({ index: 1, str: 'MASK' });
+const Blend: AlphaModeEnum = new AlphaModeClass({ index: 2, str: 'BLEND' });
 
 const typeList = [Opaque, Mask, Blend];
 
-function from( index : number): AlphaModeEnum|undefined {
-  return _from({typeList, index});
+function from(index: number): AlphaModeEnum | undefined {
+  return _from({ typeList, index });
 }
 
-function fromString( str: string ): AlphaModeEnum|undefined {
-  return _fromString({typeList, str}) as AlphaModeEnum;
+function fromString(str: string): AlphaModeEnum | undefined {
+  return _fromString({ typeList, str }) as AlphaModeEnum;
 }
 
 export const AlphaMode = Object.freeze({ Opaque, Mask, Blend, from, fromString });
