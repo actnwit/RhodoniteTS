8a465fd25637455c9152d909157568e3929bbc7a
Yuki Shimada <emadurandal@gmail.com>
2020-09-21 22:28:51 +0900

    Remove distribution files

--- dist/foundation/materials/nodes/UniformDataShaderNode.d.ts
+++
@@ -1,8 +0,0 @@
-import { CompositionTypeEnum } from "../../definitions/CompositionType";
-import { ComponentTypeEnum } from "../../definitions/ComponentType";
-import AbstractShaderNode from "../core/AbstractShaderNode";
-export default class UniformDataShaderNode extends AbstractShaderNode {
-    constructor(compositionType: CompositionTypeEnum, componentType: ComponentTypeEnum);
-    setDefaultInputValue(inputName: string, value: any): void;
-    setUniformDataName(value: any): void;
-}
