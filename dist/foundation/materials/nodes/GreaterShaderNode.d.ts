be7fad113c21f61038b07a1f905928b4a1754dc4
Yuki Shimada <emadurandal@gmail.com>
2020-09-27 01:57:55 +0900

    Remove *.d.ts files

--- dist/foundation/materials/nodes/GreaterShaderNode.d.ts
+++
@@ -1,6 +0,0 @@
-import AbstractShaderNode from "../core/AbstractShaderNode";
-import { CompositionTypeEnum } from "../../definitions/CompositionType";
-import { ComponentTypeEnum } from "../../definitions/ComponentType";
-export default class GreaterShaderNode extends AbstractShaderNode {
-    constructor(compositionType: CompositionTypeEnum, componentType: ComponentTypeEnum);
-}
