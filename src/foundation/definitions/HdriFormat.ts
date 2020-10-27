8356a4dd9f9393c95ef01a7d02f377dbb2b5728d
Katsuya Furusho <d.guitar.c@gmail.com>
2020-10-27 19:08:46 +0900

    chore: code cleaning

--- src/foundation/definitions/HdriFormat.ts
+++ src/foundation/definitions/HdriFormat.ts
@@ -5,24 +5,24 @@ export interface HdriFormatEnum extends EnumIO {
 
 class HdriFormatClass extends EnumClass implements HdriFormatEnum {
 
-  constructor({index, str} : {index: number, str: string}) {
-    super({index, str});
+  constructor({ index, str }: { index: number, str: string }) {
+    super({ index, str });
 
   }
 
 }
 
-const LDR_SRGB: HdriFormatEnum = new HdriFormatClass({index:0, str:'.ldr_srgb'});
-const LDR_LINEAR: HdriFormatEnum = new HdriFormatClass({index:1, str:'.ldr_linear'});
-const HDR: HdriFormatEnum = new HdriFormatClass({index:2, str:'.hdr'});
-const RGBE_PNG: HdriFormatEnum = new HdriFormatClass({index:3, str:'.rgbe.png'});
-const RGB9_E5_PNG: HdriFormatEnum = new HdriFormatClass({index:4, str:'.rgb9_e5.png'});
-const OpenEXR: HdriFormatEnum = new HdriFormatClass({index:5, str:'.exr'});
+const LDR_SRGB: HdriFormatEnum = new HdriFormatClass({ index: 0, str: '.ldr_srgb' });
+const LDR_LINEAR: HdriFormatEnum = new HdriFormatClass({ index: 1, str: '.ldr_linear' });
+const HDR: HdriFormatEnum = new HdriFormatClass({ index: 2, str: '.hdr' });
+const RGBE_PNG: HdriFormatEnum = new HdriFormatClass({ index: 3, str: '.rgbe.png' });
+const RGB9_E5_PNG: HdriFormatEnum = new HdriFormatClass({ index: 4, str: '.rgb9_e5.png' });
+const OpenEXR: HdriFormatEnum = new HdriFormatClass({ index: 5, str: '.exr' });
 
-const typeList = [ LDR_SRGB, LDR_LINEAR, HDR, RGBE_PNG, RGB9_E5_PNG, OpenEXR ];
+const typeList = [LDR_SRGB, LDR_LINEAR, HDR, RGBE_PNG, RGB9_E5_PNG, OpenEXR];
 
-function from( index : number ): HdriFormatEnum {
-  return _from({typeList, index}) as HdriFormatEnum;
+function from(index: number): HdriFormatEnum {
+  return _from({ typeList, index }) as HdriFormatEnum;
 }
 
 export const HdriFormat = Object.freeze({ LDR_SRGB, LDR_LINEAR, HDR, RGBE_PNG, RGB9_E5_PNG, OpenEXR });
