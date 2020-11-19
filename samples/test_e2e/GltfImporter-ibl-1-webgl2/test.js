785fb05a2ac7492ea9b1873b8979fc7b4155bac4
Katsuya Furusho <d.guitar.c@gmail.com>
2020-11-19 12:44:25 +0900

    feat: add e2e-test for ProcessApproach.UniformWebGL2

---
+++ samples/test_e2e/GltfImporter-ibl-1-webgl2/test.js
@@ -0,0 +1,24 @@
+test('regression test GltfImporter-ibl-1--webgl2', async () => {
+  jest.setTimeout(600000);
+  const page = await browser.newPage();
+  await page.goto('http://localhost:8082/samples/test_e2e/GltfImporter-ibl-1-webgl2');
+  await page.setViewport({ width: 1000, height: 1000 });
+  await page.waitForSelector('p#started', { timeout: 600000 });
+
+  await page.mouse.down();
+  await page.mouse.move(0, 0);
+  await page.mouse.move(0, 100);
+  await page.mouse.move(50, 100);
+  await page.mouse.up();
+
+  await page.waitForSelector('p#rendered', { timeout: 600000 });
+
+  const canvasElement = await page.$('#world');
+  const image = await canvasElement.screenshot();
+  expect(image).toMatchImageSnapshot({
+    failureThreshold: 0.03,
+    failureThresholdType: 'percent'
+  });
+  await page.goto('about:blank');
+  await page.close();
+});
