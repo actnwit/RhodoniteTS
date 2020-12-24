dedc9a8f5d2d35f97b36264f076a6ce8f667bfab
Yuki Shimada <emadurandal@gmail.com>
2020-12-24 20:02:07 +0900

    Refactoring prerequisites.glsl

--- src/webgl/shaderity_shaders/common/prerequisites.glsl
+++ src/webgl/shaderity_shaders/common/prerequisites.glsl
@@ -9,61 +9,54 @@ uniform sampler2D u_dataTexture; // skipProcess=true
 #endif
 
 
-  /*
-  * This idea from https://qiita.com/YVT/items/c695ab4b3cf7faa93885
-  * arg = vec2(1. / size.x, 1. / size.x / size.y);
-  */
-  // highp vec4 fetchElement(highp sampler2D tex, highp float index, highp vec2 arg)
-  // {
-  //   return ${this.glsl_texture}( tex, arg * (index + 0.5) );
-  // }
-
-highp vec4 fetchElement(highp sampler2D tex, int index, int texWidth, int texHeight) {
+highp vec4 fetchElement(highp sampler2D tex, int vec4_idx, int texWidth, int texHeight) {
 #if defined(GLSL_ES3) && defined(RN_IS_FASTEST_MODE)
-  if (index < dataUBOVec4Size) {
-    return fetchVec4FromVec4Block(index);
+  if (vec4_idx < dataUBOVec4Size) {
+    return fetchVec4FromVec4Block(vec4_idx);
   } else {
-    int idxOnDataTex = index - dataUBOVec4Size;
+    int idxOnDataTex = vec4_idx - dataUBOVec4Size;
     highp ivec2 uv = ivec2(idxOnDataTex % texWidth, idxOnDataTex / texWidth);
     return texelFetch( tex, uv, 0 );
   }
 #elif defined(GLSL_ES3)
-  highp ivec2 uv = ivec2(index % texWidth, index / texWidth);
+  highp ivec2 uv = ivec2(vec4_idx % texWidth, vec4_idx / texWidth);
   return texelFetch( tex, uv, 0 );
 #else
+  // This idea from https://qiita.com/YVT/items/c695ab4b3cf7faa93885
   highp vec2 invSize = vec2(1.0/float(texWidth), 1.0/float(texHeight));
-  highp float t = (float(index) + 0.5) * invSize.x;
+  highp float t = (float(vec4_idx) + 0.5) * invSize.x;
   highp float x = fract(t);
   highp float y = (floor(t) + 0.5) * invSize.y;
-#ifdef GLSL_ES3  
+  #ifdef GLSL_ES3
   return texture( tex, vec2(x, y));
-#else
+  #else
   return texture2D( tex, vec2(x, y));
-#endif
+  #endif
 #endif
 }
 
-vec4 fetchVec4(highp sampler2D tex, int idx, int texWidth, int texHeight) {
-  vec4 val = fetchElement(u_dataTexture, idx, texWidth, texHeight);
+
+vec4 fetchVec4(highp sampler2D tex, int vec4_idx, int texWidth, int texHeight) {
+  vec4 val = fetchElement(u_dataTexture, vec4_idx, texWidth, texHeight);
 
   return val;
 }
 
-mat2 fetchMat2(highp sampler2D tex, int idx, int texWidth, int texHeight) {
-  vec4 col0 = fetchElement(u_dataTexture, idx, texWidth, texHeight);
+mat2 fetchMat2(highp sampler2D tex, int vec4_idx, int texWidth, int texHeight) {
+  vec4 col0 = fetchElement(u_dataTexture, vec4_idx, texWidth, texHeight);
 
   mat2 val = mat2(
     col0.x, col0.y,
     col0.z, col0.w
     );
-  
+
   return val;
 }
 
-mat3 fetchMat3(highp sampler2D tex, int idx, int texWidth, int texHeight) {
-  vec4 col0 = fetchElement(u_dataTexture, idx, texWidth, texHeight);
-  vec4 col1 = fetchElement(u_dataTexture, idx + 1, texWidth, texHeight);
-  vec4 col2 = fetchElement(u_dataTexture, idx + 2, texWidth, texHeight);
+mat3 fetchMat3(highp sampler2D tex, int vec4_idx, int texWidth, int texHeight) {
+  vec4 col0 = fetchElement(u_dataTexture, vec4_idx, texWidth, texHeight);
+  vec4 col1 = fetchElement(u_dataTexture, vec4_idx + 1, texWidth, texHeight);
+  vec4 col2 = fetchElement(u_dataTexture, vec4_idx + 2, texWidth, texHeight);
 
   mat3 val = mat3(
     col0.x, col0.y, col0.z,
@@ -74,11 +67,11 @@ mat3 fetchMat3(highp sampler2D tex, int idx, int texWidth, int texHeight) {
   return val;
 }
 
-mat4 fetchMat4(highp sampler2D tex, int idx, int texWidth, int texHeight) {
-  vec4 col0 = fetchElement(u_dataTexture, idx, texWidth, texHeight);
-  vec4 col1 = fetchElement(u_dataTexture, idx + 1, texWidth, texHeight);
-  vec4 col2 = fetchElement(u_dataTexture, idx + 2, texWidth, texHeight);
-  vec4 col3 = fetchElement(u_dataTexture, idx + 3, texWidth, texHeight);
+mat4 fetchMat4(highp sampler2D tex, int vec4_idx, int texWidth, int texHeight) {
+  vec4 col0 = fetchElement(u_dataTexture, vec4_idx, texWidth, texHeight);
+  vec4 col1 = fetchElement(u_dataTexture, vec4_idx + 1, texWidth, texHeight);
+  vec4 col2 = fetchElement(u_dataTexture, vec4_idx + 2, texWidth, texHeight);
+  vec4 col3 = fetchElement(u_dataTexture, vec4_idx + 3, texWidth, texHeight);
 
   mat4 val = mat4(
     col0.x, col0.y, col0.z, col0.w,
