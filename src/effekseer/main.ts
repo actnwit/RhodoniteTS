73d82dbb2ddf034fda724b5c53353fa149944a55
Yuki Shimada <emadurandal@gmail.com>
2021-02-18 20:52:05 +0900

    Fix the code to the gts style

--- src/effekseer/main.ts
+++ src/effekseer/main.ts
@@ -1,18 +1,21 @@
-
 import EffekseerComponent from './EffekseerComponent';
 import EntityRepository from '../foundation/core/EntityRepository';
 import TransformComponent from '../foundation/components/TransformComponent';
 import SceneGraphComponent from '../foundation/components/SceneGraphComponent';
 
-const createEffekseerEntity = function() {
+const createEffekseerEntity = function () {
   const entityRepository = EntityRepository.getInstance();
-  const entity = entityRepository.createEntity([TransformComponent, SceneGraphComponent, EffekseerComponent])
+  const entity = entityRepository.createEntity([
+    TransformComponent,
+    SceneGraphComponent,
+    EffekseerComponent,
+  ]);
 
   return entity;
-}
+};
 
 const Effekseer = Object.freeze({
   EffekseerComponent,
-  createEffekseerEntity
+  createEffekseerEntity,
 });
 export default Effekseer;
