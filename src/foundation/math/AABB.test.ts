dd6159e482e64d6e44282cbb5f83e25957dda339
Yuki Shimada <emadurandal@gmail.com>
2021-03-13 04:17:19 +0900

    Add the AABB class test

---
+++ src/foundation/math/AABB.test.ts
@@ -0,0 +1,16 @@
+import AABB from './AABB';
+import Vector3 from './Vector3';
+
+test('AABB is vanilla first', () => {
+  const aabb = new AABB();
+  expect(aabb.isVanilla()).toBe(true);
+
+  aabb.addPosition(new Vector3(0, 0, 0));
+
+  expect(aabb.isVanilla()).toBe(false);
+});
+
+test('AABB clone method works properly', () => {
+  const aabb = new AABB();
+  expect(aabb.isVanilla()).toBe(true);
+});
