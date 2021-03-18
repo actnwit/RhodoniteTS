121fa940ebde27e042c09dffbcafe5d31c6411a4
Katsuya Furusho <d.guitar.c@gmail.com>
2021-03-18 18:54:24 +0900

    refactor: organize jest configuration files into a single folder

--- config/test/jest.config.js
+++ config/test/jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
-    '\\.(vert|frag|glsl)$': '<rootDir>/jest-webpack-shaderity.js',
-    '\\VERSION-FILE$': '<rootDir>/jest-webpack-shaderity.js',
+    '\\.(vert|frag|glsl)$': '<rootDir>/config/test/jest-webpack-shaderity.js',
+    '\\VERSION-FILE$': '<rootDir>/config/test/jest-webpack-shaderity.js',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
