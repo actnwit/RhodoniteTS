e887f8698902a390d35c2322f382fc4b52732c82
Yuki Shimada <emadurandal@gmail.com>
2021-03-21 02:28:57 +0900

    Fix the isEqual of IdentityMatrix44

--- src/foundation/math/IdentityMatrix44.ts
+++ src/foundation/math/IdentityMatrix44.ts
@@ -53,10 +53,10 @@ export default class IdentityMatrix44 implements IMatrix, IMatrix44 {
   isStrictEqual(mat: IMatrix): boolean {
     const v = (mat as Matrix44).v;
     if (
-      v[0] === 0 && v[1] === 0 && v[2] === 0 && v[3] === 0 &&
-      v[4] === 0 && v[5] === 0 && v[6] === 0 && v[7] === 0 &&
-      v[8] === 0 && v[9] === 0 && v[10] === 0 && v[11] === 0 &&
-      v[12] === 0 && v[13] === 0 && v[14] === 0 && v[15] === 0
+      v[0] === 1 && v[1] === 0 && v[2] === 0 && v[3] === 0 &&
+      v[4] === 0 && v[5] === 1 && v[6] === 0 && v[7] === 0 &&
+      v[8] === 0 && v[9] === 0 && v[10] === 1 && v[11] === 0 &&
+      v[12] === 0 && v[13] === 0 && v[14] === 0 && v[15] === 1
     ) {
       return true;
     } else {
