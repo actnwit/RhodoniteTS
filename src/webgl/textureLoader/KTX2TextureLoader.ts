d72b23caa5dac9eae491795979bc8486435fd0c5
Katsuya Furusho <d.guitar.c@gmail.com>
2021-05-21 14:38:10 +0900

    feat: load msc_basis_transcoder for importing ktx2 file

---
+++ src/webgl/textureLoader/KTX2TextureLoader.ts
@@ -0,0 +1,45 @@
+declare const MSC_TRANSCODER: any;
+
+export default class KTX2TextureLoader {
+  private static __instance: KTX2TextureLoader;
+
+  // TODO: create type of __mscTranscoderModule
+  private static __mscTranscoderModule: any;
+
+  private __mscTranscoderPromise: Promise<void>;
+
+  constructor() {
+    if (typeof MSC_TRANSCODER === 'undefined') {
+      console.error(
+        'Failed to call MSC_TRANSCODER() function. Please check to import msc_basis_transcoder.js.'
+      );
+    }
+    this.__mscTranscoderPromise = this.__loadMSCTranscoder();
+  }
+
+  // ----- Public Methods -----------------------------------------------------
+
+  static getInstance() {
+    if (!this.__instance) {
+      this.__instance = new KTX2TextureLoader();
+    }
+    return this.__instance;
+  }
+
+  // ----- Private Methods ----------------------------------------------------
+
+  private __loadMSCTranscoder(): Promise<void> {
+    // load msc_basis_transcoder once
+    return new Promise(resolve => {
+      if (KTX2TextureLoader.__mscTranscoderModule) {
+        resolve();
+      }
+
+      MSC_TRANSCODER().then((transcoderModule: any) => {
+        transcoderModule.initTranscoders();
+        KTX2TextureLoader.__mscTranscoderModule = transcoderModule;
+        resolve();
+      });
+    });
+  }
+}
