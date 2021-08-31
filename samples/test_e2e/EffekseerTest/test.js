782e5c1595f9f894346c089e68f266428555a1d5
ishiiyuki <mizuki.yuuri@gmail.com>
2021-09-01 00:18:14 +0900

    add e2etest Effekseer

---
+++ samples/test_e2e/EffekseerTest/test.js
@@ -0,0 +1,15 @@
+test('regression test VRMImporter', async () => {
+  jest.setTimeout(180000);
+  const page = await browser.newPage();
+  await page.goto('http://localhost:8082/samples/test_e2e/Effekseer');
+  await page.setViewport({width: 1000, height: 1000});
+  await page.waitForSelector('p#rendered', {timeout: 180000});
+  const canvasElement = await page.$('#world');
+  const image = await canvasElement.screenshot();
+  expect(image).toMatchImageSnapshot({
+    failureThreshold: 0.03,
+    failureThresholdType: 'percent',
+  });
+
+  await page.close();
+});
