ff6c73a859a06d226e69829e5e59e8ff533b564f
ishiiyuki <mizuki.yuuri@gmail.com>
2021-09-07 21:28:36 +0900

    fix: timeout bug fix

--- samples/test_e2e/EffekseerTest/test.js
+++ samples/test_e2e/EffekseerTest/test.js
@@ -1,10 +1,9 @@
 test('regression test EffekseerTest', async () => {
   jest.setTimeout(180000);
   const page = await browser.newPage();
-  await page.setDefaultNavigationTimeout(180000);
   await page.goto('http://localhost:8082/samples/test_e2e/EffekseerTest');
   await page.setViewport({width: 1000, height: 1000});
-  await page.waitForFunction(() => {return window._rendered}, {timeout: 180000});
+  await page.waitForSelector('p#rendered', {timeout: 180000});
   const canvasElement = await page.$('#world');
   const image = await canvasElement.screenshot();
   expect(image).toMatchImageSnapshot({
