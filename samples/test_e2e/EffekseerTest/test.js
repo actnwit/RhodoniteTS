8f562202322ca56292e7bc3181d785f373ea38c2
ishiiyuki <mizuki.yuuri@gmail.com>
2021-09-09 01:08:24 +0900

    fix timeout error
     timeout num change

--- samples/test_e2e/EffekseerTest/test.js
+++ samples/test_e2e/EffekseerTest/test.js
@@ -1,10 +1,10 @@
 test('regression test EffekseerTest', async () => {
   jest.setTimeout(180000);
   const page = await browser.newPage();
-  await page.setDefaultNavigationTimeout(130000);
+  await page.setDefaultNavigationTimeout(100000);
   await page.goto('http://localhost:8082/samples/test_e2e/EffekseerTest');
   await page.setViewport({width: 1000, height: 1000});
-  await page.waitForSelector('p#rendered', {timeout: 180000});
+  await page.waitForSelector('p#rendered', {timeout: 100000});
   const canvasElement = await page.$('#world');
   const image = await canvasElement.screenshot();
   expect(image).toMatchImageSnapshot({
