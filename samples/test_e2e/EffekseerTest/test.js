ec2fd33f65d22418d8ec39cf05dd0544b8d18ea8
Yuki Shimada <emadurandal@gmail.com>
2021-09-09 18:50:52 +0900

    Add the usefull puppeteer console bypass (browser -> jest test code) technique

--- samples/test_e2e/EffekseerTest/test.js
+++ samples/test_e2e/EffekseerTest/test.js
@@ -3,6 +3,11 @@ test('regression test EffekseerTest', async () => {
   const page = await browser.newPage();
   await page.setDefaultNavigationTimeout(180000);
   await page.goto('http://localhost:8082/samples/test_e2e/EffekseerTest');
+  page.on('console', msg => {
+    for (let i = 0; i < msg.args().length; i++) {
+      // console.log(msg.args()[i]); // comment out if you check the browser side console out
+    }
+  });
   await page.setViewport({width: 1000, height: 1000});
   // await page.waitForSelector('p#rendered', {timeout: 180000});
   await page.waitForFunction(() => {return window._rendered}, {timeout: 180000});
