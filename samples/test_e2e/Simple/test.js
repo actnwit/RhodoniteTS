18abe2f455908639f32f2ddc703a240fb91f29d9
Yuki Shimada <emadurandal@gmail.com>
2021-09-20 21:45:38 +0900

    test: Add Simple sample

---
+++ samples/test_e2e/Simple/test.js
@@ -0,0 +1,6 @@
+const testCheckWindowRendered = require('../common/testFunc').testCheckWindowRendered;
+const setURL = 'http://localhost:8082/samples/test_e2e/Simple';
+
+test('regression test Simple', async () => {
+  await testCheckWindowRendered(jest, browser, setURL, expect, 0.01);
+});
