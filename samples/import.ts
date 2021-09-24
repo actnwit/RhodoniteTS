5bf7afaccdf85455f8f035bf974975d284579f67
Yuki Shimada <emadurandal@gmail.com>
2021-09-24 13:14:15 +0900

    chore: fix a typo

--- samples/import.ts
+++ samples/import.ts
@@ -17,6 +17,6 @@ import RnForType from '../src/index';
 // Change the TypeScript type of Rhodonite,
 //   from `typeof Rn` (ESM built index.d.ts)
 //   to `typeof RnForType` (TypeScript source codes).
-// This is for code editing convinience.
+// This is for code editing convenience.
 // You can jump to the source code definition of Rhodonite Object Types easily in your source code editor.
 export default Rn as unknown as typeof RnForType; // type Rn = typeof RnForType;
