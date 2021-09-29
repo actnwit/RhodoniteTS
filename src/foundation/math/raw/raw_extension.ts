8dfa4ac068b57f28d53431710073b164b4036fd1
Yuki Shimada <emadurandal@gmail.com>
2021-09-29 17:01:23 +0900

    feat: some extension functions to raw_extensions.ts

--- src/foundation/math/raw/raw_extension.ts
+++ src/foundation/math/raw/raw_extension.ts
@@ -1,15 +1,42 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import type {ArrayType} from '../../../types/CommonTypes';
+import type {
+  Array1,
+  Array2,
+  Array3,
+  ArrayType,
+} from '../../../types/CommonTypes';
+export const get1_offsetAsComposition = Symbol();
+export const get2_offsetAsComposition = Symbol();
+export const get3_offsetAsComposition = Symbol();
+export const get4_offsetAsComposition = Symbol();
 export const add2 = Symbol();
 export const add2_offset = Symbol();
 export const add3 = Symbol();
 export const add3_offset = Symbol();
 export const add4 = Symbol();
 export const add4_offset = Symbol();
+export const qlerp_offsetAsComposition_to = Symbol();
+export const scalar_lerp_offsetAsComposition = Symbol();
 
 declare global {
   interface Extension {
+    [get1_offsetAsComposition](
+      this: ArrayType,
+      offsetAsComposition: number
+    ): ArrayType;
+    [get2_offsetAsComposition](
+      this: ArrayType,
+      offsetAsComposition: number
+    ): ArrayType;
+    [get3_offsetAsComposition](
+      this: ArrayType,
+      offsetAsComposition: number
+    ): ArrayType;
+    [get4_offsetAsComposition](
+      this: ArrayType,
+      offsetAsComposition: number
+    ): ArrayType;
     [add2](this: ArrayType, array: ArrayType): ArrayType;
     [add2_offset](
       this: ArrayType,
@@ -31,12 +58,64 @@ declare global {
       selfOffset: number,
       argOffset: number
     ): ArrayType;
+    [qlerp_offsetAsComposition_to](
+      this: ArrayType,
+      array: ArrayType,
+      ratio: number,
+      out: ArrayType,
+      selfOffset: number,
+      argOffset: number
+    ): ArrayType;
+    [scalar_lerp_offsetAsComposition](
+      this: ArrayType,
+      array: ArrayType,
+      ratio: number,
+      selfOffset: number,
+      argOffset: number
+    ): number;
   }
 
   interface Array<T> extends Extension {}
   interface ReadonlyArray<T> extends Extension {}
   interface Float32Array extends Extension {}
 }
+
+const get1_offsetAsComposition_fn = function (
+  this: ArrayType,
+  offsetAsComposition: number
+): Array1<number> {
+  return [this[offsetAsComposition]];
+};
+
+const get2_offsetAsComposition_fn = function (
+  this: ArrayType,
+  offsetAsComposition: number
+): Array2<number> {
+  return [this[offsetAsComposition * 2], this[offsetAsComposition * 2 + 1]];
+};
+
+const get3_offsetAsComposition_fn = function (
+  this: ArrayType,
+  offsetAsComposition: number
+): Array3<number> {
+  return [
+    this[offsetAsComposition * 3],
+    this[offsetAsComposition * 3 + 1],
+    this[offsetAsComposition * 3 + 2],
+  ];
+};
+
+const get4_offsetAsComposition_fn = function (
+  this: ArrayType,
+  offsetAsComposition: number
+) {
+  return [
+    this[offsetAsComposition * 4],
+    this[offsetAsComposition * 4 + 1],
+    this[offsetAsComposition * 4 + 2],
+    this[offsetAsComposition * 4 + 3],
+  ];
+};
 
 const add2_fn = function (this: ArrayType, array: ArrayType) {
   this[0] += array[0];
@@ -101,15 +180,111 @@ const add4_offset_fn = function (
   return this;
 };
 
-const arrayTypes = [Array, Float32Array, Float64Array];
-const operators = [add2, add2_offset, add3, add3_offset, add4, add4_offset];
+const qlerp_offsetAsComposition_to_fn = function (
+  this: ArrayType,
+  array: ArrayType,
+  ratio: number,
+  out: ArrayType,
+  selfOffsetAsComposition: number,
+  argOffsetAsComposition: number
+) {
+  let dotProduct =
+    this[0 + selfOffsetAsComposition * 4] *
+      array[0 + argOffsetAsComposition * 4] +
+    this[1 + selfOffsetAsComposition * 4] *
+      array[1 + argOffsetAsComposition * 4] +
+    this[2 + selfOffsetAsComposition * 4] *
+      array[2 + argOffsetAsComposition * 4] +
+    this[3 + selfOffsetAsComposition * 4] *
+      array[3 + argOffsetAsComposition * 4];
+  const ss = 1.0 - dotProduct * dotProduct;
+
+  if (ss === 0.0) {
+    out[0] = this[0 + selfOffsetAsComposition * 4];
+    out[1] = this[1 + selfOffsetAsComposition * 4];
+    out[2] = this[2 + selfOffsetAsComposition * 4];
+    out[3] = this[3 + selfOffsetAsComposition * 4];
+  } else {
+    if (dotProduct > 1) {
+      dotProduct = 0.999;
+    } else if (dotProduct < -1) {
+      dotProduct = -0.999;
+    }
+
+    let theta = Math.acos(dotProduct);
+    const sinTheta = Math.sin(theta);
+
+    let s2;
+    if (dotProduct < 0.0) {
+      dotProduct *= -1;
+      theta = Math.acos(dotProduct);
+      s2 = (-1 * Math.sin(theta * ratio)) / sinTheta;
+    } else {
+      s2 = Math.sin(theta * ratio) / sinTheta;
+    }
+    const s1 = Math.sin(theta * (1.0 - ratio)) / sinTheta;
+
+    out[0] =
+      this[0 + selfOffsetAsComposition * 4] * s1 +
+      array[0 + selfOffsetAsComposition * 4] * s2;
+    out[1] =
+      this[1 + selfOffsetAsComposition * 4] * s1 +
+      array[1 + selfOffsetAsComposition * 4] * s2;
+    out[2] =
+      this[2 + selfOffsetAsComposition * 4] * s1 +
+      array[2 + selfOffsetAsComposition * 4] * s2;
+    out[3] =
+      this[3 + selfOffsetAsComposition * 4] * s1 +
+      array[3 + selfOffsetAsComposition * 4] * s2;
+  }
+
+  return this;
+};
+
+const scalar_lerp_offsetAsComposition_fn = function (
+  this: ArrayType,
+  array: ArrayType,
+  ratio: number,
+  selfOffset: number,
+  argOffset: number
+) {
+  return this[selfOffset] * (1 - ratio) - array[argOffset + 1] * ratio;
+};
+
+const arrayTypes = [
+  Array,
+  Float32Array,
+  Float64Array,
+  Int8Array,
+  Int16Array,
+  Int32Array,
+  Uint8Array,
+  Uint16Array,
+  Uint32Array,
+];
+const operators = [
+  get3_offsetAsComposition,
+  get4_offsetAsComposition,
+  add2,
+  add2_offset,
+  add3,
+  add3_offset,
+  add4,
+  add4_offset,
+  qlerp_offsetAsComposition_to,
+  scalar_lerp_offsetAsComposition,
+];
 const functions = [
+  get3_offsetAsComposition_fn,
+  get4_offsetAsComposition_fn,
   add2_fn,
   add2_offset_fn,
   add3_fn,
   add3_offset_fn,
   add4_fn,
   add4_offset_fn,
+  qlerp_offsetAsComposition_to_fn,
+  scalar_lerp_offsetAsComposition_fn,
 ];
 
 for (let i = 0; i < arrayTypes.length; i++) {
