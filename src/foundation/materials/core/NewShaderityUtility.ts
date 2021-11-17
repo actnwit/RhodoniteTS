146b7d2dbfe968e65294c2cbaa195867498c1cbf
KaYaF <d.guitar.c@gmail.com>
2021-11-17 11:16:43 +0900

    feat: introduce NewShaderityUtility file to update shaderity version to v0.2.2 (will replace to ShaderityUtility)

---
+++ src/foundation/materials/core/NewShaderityUtility.ts
@@ -0,0 +1,6 @@
+export default class NewShaderityUtility {
+  public static fillTemplate() {}
+  public static transformWebGLVersionTo() {}
+  public static getReflection() {}
+  public static getShaderDataRefection() {}
+}
