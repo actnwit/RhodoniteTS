bc2adf94243a2c0467e8b24a9aa3f798ebe09d0b
KaYaF <d.guitar.c@gmail.com>
2021-11-17 12:17:18 +0900

    feat: write fillTemplate method
    Since we have not updated the Shaderity package, the method implementation is commented out.

--- src/foundation/materials/core/NewShaderityUtility.ts
+++ src/foundation/materials/core/NewShaderityUtility.ts
@@ -1,5 +1,33 @@
+// import Shaderity, {ShaderityObject, TemplateObject} from 'shaderity';
+import MemoryManager from '../../core/MemoryManager';
+import {WellKnownComponentTIDs} from '../../components/WellKnownComponentTIDs';
+import Config from '../../core/Config';
+
+export type FillArgsObject = {
+  [key: string]: string;
+};
+
 export default class NewShaderityUtility {
+  private static __defaultTemplate = {
+    WellKnownComponentTIDs,
+    widthOfDataTexture: `const int widthOfDataTexture = ${MemoryManager.bufferWidthLength};`,
+    heightOfDataTexture: `const int heightOfDataTexture = ${MemoryManager.bufferHeightLength};`,
+    Config,
+  };
+
   public static fillTemplate() {}
+  // public static fillTemplate(
+  //   shaderityObject: ShaderityObject,
+  //   args: FillArgsObject
+  // ): ShaderityObject {
+  //   const templateObject = Object.assign(
+  //     args,
+  //     this.__defaultTemplate
+  //   ) as TemplateObject;
+
+  //   return Shaderity.fillTemplate(shaderityObject, templateObject);
+  // }
+
   public static transformWebGLVersionTo() {}
   public static getReflection() {}
   public static getShaderDataRefection() {}
