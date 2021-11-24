270f3c0ddb3fb13c47cffa4b104ec4aa4da2408a
KaYaF <d.guitar.c@gmail.com>
2021-11-24 12:58:50 +0900

    feat: create shader semantics info array of ShaderitySingleMaterialNode

--- src/foundation/materials/singles/ShaderitySingleMaterialNode.ts
+++ src/foundation/materials/singles/ShaderitySingleMaterialNode.ts
@@ -2,6 +2,8 @@ import {ShaderSemanticsInfo} from '../../definitions/ShaderSemantics';
 import AbstractMaterialNode from '../core/AbstractMaterialNode';
 import Material from '../core/Material';
 import {ShaderityObject} from 'shaderity';
+import ShaderityUtility from '../core/ShaderityUtility';
+import {ShaderType} from '../../definitions/ShaderType';
 
 export default class ShaderitySingleMaterialNode extends AbstractMaterialNode {
   constructor({
@@ -15,10 +17,33 @@ export default class ShaderitySingleMaterialNode extends AbstractMaterialNode {
   }) {
     super(null, name, {});
 
+    const vertexShaderData = ShaderityUtility.getShaderDataRefection(
+      vertexShaderityObj,
+      AbstractMaterialNode.__semanticsMap.get(this.shaderFunctionName)
+    );
+    const pixelShaderData = ShaderityUtility.getShaderDataRefection(
+      pixelShaderityObj,
+      AbstractMaterialNode.__semanticsMap.get(this.shaderFunctionName)
+    );
     this.__vertexShaderityObject = vertexShaderityObj;
     this.__pixelShaderityObject = pixelShaderityObj;
 
-    const shaderSemanticsInfoArray: ShaderSemanticsInfo[] = [];
+    const shaderSemanticsInfoArray: ShaderSemanticsInfo[] =
+      vertexShaderData.shaderSemanticsInfoArray.concat();
+
+    for (const pixelShaderSemanticsInfo of pixelShaderData.shaderSemanticsInfoArray) {
+      const foundShaderSemanticsInfo = shaderSemanticsInfoArray.find(
+        (info: ShaderSemanticsInfo) => {
+          return info.semantic.str === pixelShaderSemanticsInfo.semantic.str;
+        }
+      );
+      if (foundShaderSemanticsInfo) {
+        foundShaderSemanticsInfo.stage = ShaderType.VertexAndPixelShader;
+      } else {
+        shaderSemanticsInfoArray.push(pixelShaderSemanticsInfo);
+      }
+    }
+
     this.setShaderSemanticsInfoArray(shaderSemanticsInfoArray);
   }
 
