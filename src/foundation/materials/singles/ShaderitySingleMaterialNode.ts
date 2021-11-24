5ccd656168e0642c54975ccf30b183764f1616b8
KaYaF <d.guitar.c@gmail.com>
2021-11-24 19:03:16 +0900

    fix: uses the default setting for some shader semantics

--- src/foundation/materials/singles/ShaderitySingleMaterialNode.ts
+++ src/foundation/materials/singles/ShaderitySingleMaterialNode.ts
@@ -1,9 +1,13 @@
-import {ShaderSemanticsInfo} from '../../definitions/ShaderSemantics';
+import {
+  ShaderSemantics,
+  ShaderSemanticsInfo,
+} from '../../definitions/ShaderSemantics';
 import AbstractMaterialNode from '../core/AbstractMaterialNode';
 import Material from '../core/Material';
 import {ShaderityObject} from 'shaderity';
 import ShaderityUtility from '../core/ShaderityUtility';
 import {ShaderType} from '../../definitions/ShaderType';
+import GlobalDataRepository from '../../core/GlobalDataRepository';
 
 export default class ShaderitySingleMaterialNode extends AbstractMaterialNode {
   constructor({
@@ -44,22 +48,9 @@ export default class ShaderitySingleMaterialNode extends AbstractMaterialNode {
       }
     }
 
-    const globalPropertyStructs =
-      GlobalDataRepository.getInstance().getGlobalProperties();
-    for (const globalPropertyStruct of globalPropertyStructs) {
-      const globalShaderSemanticsInfo =
-        globalPropertyStruct.shaderSemanticsInfo;
-
-      const duplicateElemId = shaderSemanticsInfoArray.findIndex(
-        shaderSemanticsInfo =>
-          shaderSemanticsInfo.semantic.str ===
-          globalShaderSemanticsInfo.semantic.str
-      );
-
-      if (duplicateElemId !== -1) {
-        shaderSemanticsInfoArray.splice(duplicateElemId, 1);
-      }
-    }
+    ShaderitySingleMaterialNode.__removeUselessShaderSemantics(
+      shaderSemanticsInfoArray
+    );
 
     this.setShaderSemanticsInfoArray(shaderSemanticsInfoArray);
   }
@@ -75,4 +66,45 @@ export default class ShaderitySingleMaterialNode extends AbstractMaterialNode {
     firstTime: boolean;
     args?: any;
   }) {}
+
+  private static __removeUselessShaderSemantics(
+    shaderSemanticsInfoArray: ShaderSemanticsInfo[]
+  ) {
+    const globalPropertyStructs =
+      GlobalDataRepository.getInstance().getGlobalProperties();
+
+    for (const globalPropertyStruct of globalPropertyStructs) {
+      const globalShaderSemanticsInfo =
+        globalPropertyStruct.shaderSemanticsInfo;
+
+      const duplicateElemId = shaderSemanticsInfoArray.findIndex(
+        shaderSemanticsInfo =>
+          shaderSemanticsInfo.semantic.str ===
+          globalShaderSemanticsInfo.semantic.str
+      );
+
+      if (duplicateElemId !== -1) {
+        shaderSemanticsInfoArray.splice(duplicateElemId, 1);
+      }
+    }
+
+    const defaultShaderSemantics = [
+      ShaderSemantics.VertexAttributesExistenceArray,
+      ShaderSemantics.WorldMatrix,
+      ShaderSemantics.NormalMatrix,
+      ShaderSemantics.PointSize,
+      ShaderSemantics.PointDistanceAttenuation,
+    ];
+
+    for (const shaderSemantic of defaultShaderSemantics) {
+      const duplicateElemId = shaderSemanticsInfoArray.findIndex(
+        shaderSemanticsInfo =>
+          shaderSemanticsInfo.semantic.str === shaderSemantic.str
+      );
+
+      if (duplicateElemId !== -1) {
+        shaderSemanticsInfoArray.splice(duplicateElemId, 1);
+      }
+    }
+  }
 }
