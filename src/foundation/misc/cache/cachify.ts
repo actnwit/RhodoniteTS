098d4515fc449f4bf9e7789a6c9c22681ec67ae5
Yuki Shimada <emadurandal@gmail.com>
2021-12-19 09:06:10 +0900

    feat: add cachify funtions

---
+++ src/foundation/misc/cache/cachify.ts
@@ -0,0 +1,46 @@
+/**
+ * This is from : https://github.com/threepointone/glamor/blob/667b480d31b3721a905021b26e1290ce92ca2879/docs/weakmaps.md#so-what-does-a-cache-built-with-a-weakmap-look-like
+ * Thank you!
+ */
+
+import {primitives} from '../../../types/CommonTypes';
+
+type ObjectFnType = (arg: object) => unknown;
+
+/**
+ * Return the caching wrapper function
+ * @param fn the target function for caching result
+ * @returns
+ */
+export const objectCachify = (fn: ObjectFnType): ObjectFnType => {
+  const cache = new WeakMap();
+  return (arg: object): unknown => {
+    if (cache.has(arg)) {
+      return cache.get(arg);
+    }
+
+    const value = fn(arg);
+    cache.set(arg, value);
+    return value;
+  };
+};
+
+/**
+ * Return the caching wrapper function
+ * @param fn the target function for caching result
+ * @returns
+ */
+export const primitiveCachify1 = <P extends primitives>(
+  fn: (arg: P) => unknown
+): ((arg: P) => unknown) => {
+  const cache = new Map();
+  return (arg: P): unknown => {
+    if (cache.has(arg)) {
+      return cache.get(arg);
+    }
+
+    const value = fn(arg);
+    cache.set(arg, value);
+    return value;
+  };
+};
