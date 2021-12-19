c2e16e17b5665775fdfaa3266277805015d3115f
Yuki Shimada <emadurandal@gmail.com>
2021-12-19 09:05:54 +0900

    refactor: move ManualCache to cache/SymbolWeakMap

---
+++ src/foundation/misc/cache/Cache.ts
@@ -0,0 +1,13 @@
+import {SymbolWeakMap} from './SymbolWeakMap';
+
+export class Cache<T> {
+  private __symbolWeakMap = new SymbolWeakMap();
+  constructor() {}
+  public register(value: T) {
+    this.__symbolWeakMap.set(Symbol(), value);
+  }
+
+  public read(): T {
+
+  }
+}
