d9f9987e4c461b22ac116ec98f9ae5ebf26fe479
Yuki Shimada <emadurandal@gmail.com>
2022-01-08 18:18:00 +0900

    feat: improve Result.ts

--- src/foundation/misc/Result.test.ts
+++ src/foundation/misc/Result.test.ts
@@ -1,7 +1,7 @@
-import { IResult, Ok, Err } from "./Result";
+import {IResult, Ok, Err} from './Result';
 
 test('Basic usage of Result', () => {
-  const succeedIfEven = (val: number): IResult<number, string> => {
+  const succeedIfValueEven = (val: number): IResult<number, string> => {
     if (val % 2 === 0) {
       return new Ok(val);
     } else {
@@ -9,13 +9,35 @@ test('Basic usage of Result', () => {
     }
   };
 
-  const result0 = succeedIfEven(0);
-  const result1 = succeedIfEven(1);
+  const result0 = succeedIfValueEven(0);
+  const result1 = succeedIfValueEven(1);
+  let result = '';
+  result0.match({
+    Ok: (val: number) => {
+      expect(val).toBe(0);
+      result = result0.name();
+    },
+    Err: (err: string) => {
+      expect(true).toBe(false); // If here come, this is wrong behavior.
+      result = result0.name();
+    },
+    Finally: () => {
+      expect(result).toBe('Ok');
+    },
+  });
 
-  expect(result0.match(
-    (val: number)=>{return new Ok(val)},
-    (val: string)=>{return val})
-    ).toBeInstanceOf(Ok);
-
+  result1.match({
+    Ok: (val: number) => {
+      expect(true).toBe(false); // If here come, this is wrong behavior.
+      result = result1.name();
+    },
+    Err: (err: string) => {
+      expect(err).toBe('Error');
+      result = result1.name();
+    },
+    Finally: () => {
+      expect(result).toBe('Err');
+    },
+  });
   expect(result1.getBoolean()).toBe(false);
 });
--- src/foundation/misc/Result.ts
+++ src/foundation/misc/Result.ts
@@ -1,39 +1,73 @@
+import {Is} from './Is';
+
 /**
  * An interface to handle the results in a unified manner,
  * regardless of whether they are successful or not.
  */
 export interface IResult<T, E> {
-  match<U, F>(thenFn: (value: T) => IResult<U, F>, catchFn: (value: E) => E): void;
-  then<U, F>(f: (value: T) => IResult<U, F>): IResult<U, F>;
-  catch(f: (value: E) => E): E;
+  match({
+    Ok,
+    Err,
+    Finally,
+  }: {
+    Ok: (value: T) => void;
+    Err: (value: E) => void;
+    Finally?: () => void;
+  }): void;
+  then(f: (value: T) => void): IResult<undefined, undefined>;
+  catch(f: (value: E) => void): IResult<undefined, undefined>;
+  finally(f: () => void): void;
   getBoolean(): boolean;
+  name(): string;
 }
 
 abstract class Result<T, E> {
-  constructor(protected val: T | E) { }
-  match<U, F>(thenFn: (value: T) => IResult<U, F>, catchFn: (value: E) => E): IResult<U, F> | E {
+  constructor(protected val: T | E) {}
+  match(obj: {
+    Ok: (value: T) => void;
+    Err: (value: E) => void;
+    Finally?: () => void;
+  }): void {
     if (this instanceof Ok) {
-      return thenFn(this.val);
-    } else {
-      return catchFn(this.val as E);
+      obj.Ok(this.val as T);
+    } else if (this instanceof Err) {
+      obj.Err(this.val as E);
     }
+    if (Is.exist(obj.Finally)) {
+      obj.Finally();
+    }
+  }
+  name(): string {
+    return this.constructor.name;
   }
 }
 
 /**
  * a class indicating that the result is Ok (Succeeded).
  */
-export class Ok<T> extends Result<T, never> implements IResult<T, never> {
+export class Ok<T, E> extends Result<T, E> implements IResult<T, E> {
+  constructor(val: T) {
+    super(val);
+  }
   /**
    * This method is essentially same to the Ok::and_then() in Rust language
    * @param f
    */
-  then<U, F>(f: (value: T) => IResult<U, F>): IResult<U, F> {
-    return f(this.val);
+  then(f: (value: T) => void): IResult<undefined, undefined> {
+    f(this.val as T);
+    return new Done(undefined);
   }
 
-  catch(f: (value: never) => never): never {
-    return undefined as never;
+  catch(f: (value: E) => void): IResult<undefined, undefined> {
+    throw new Error(
+      'Error due to calling the "catch" method from an Ok object!'
+    );
+  }
+
+  finally(f: () => void): void {
+    throw new Error(
+      'Error due to calling the "finally" method from an Ok object!'
+    );
   }
 
   true(): true {
@@ -45,21 +79,32 @@ export class Ok<T> extends Result<T, never> implements IResult<T, never> {
   }
 
   get(): T {
-    return this.val;
+    return this.val as T;
   }
 }
 
 /**
  * a class indicating that the result is Error (Failed).
  */
-export class Err<E> extends Result<never, E> implements IResult<never, E> {
-
-  then<U, F>(f: (value: never) => IResult<U, F>): IResult<U, F> {
-    throw new Error('Error due to calling the "then" method from an Err object!');
+export class Err<T, E> extends Result<T, E> implements IResult<T, E> {
+  constructor(val: E) {
+    super(val);
+  }
+  then(f: (value: never) => void): IResult<never, never> {
+    throw new Error(
+      'Error due to calling the "then" method from an Err object!'
+    );
   }
 
-  catch(f: (value: E) => E): E {
-    return this.val;
+  catch(f: (value: E) => void): IResult<undefined, undefined> {
+    f(this.val as E);
+    return new Done(undefined);
+  }
+
+  finally(f: () => void): void {
+    throw new Error(
+      'Error due to calling the "finally" method from an Err object!'
+    );
   }
 
   false(): false {
@@ -71,6 +116,41 @@ export class Err<E> extends Result<never, E> implements IResult<never, E> {
   }
 
   get(): E {
-    return this.val;
+    return this.val as E;
+  }
+}
+
+export class Done
+  extends Result<undefined, undefined>
+  implements IResult<undefined, undefined>
+{
+  then(f: (value: never) => void): IResult<undefined, undefined> {
+    throw new Error(
+      'Error due to calling the "then" method from an Err object!'
+    );
+  }
+
+  catch(f: (value: never) => void): IResult<undefined, undefined> {
+    throw new Error(
+      'Error due to calling the "catch" method from an Err object!'
+    );
+  }
+
+  finally(f: () => void): void {
+    f();
+  }
+
+  false(): false {
+    return false;
+  }
+
+  getBoolean(): false {
+    return false;
+  }
+
+  get(): undefined {
+    throw new Error(
+      'Error due to calling the "get" method from an Done object!'
+    );
   }
 }
