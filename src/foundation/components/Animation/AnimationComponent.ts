0f9b21b1764420ad3e6d953279303ecc21440174
Yuki Shimada <emadurandal@gmail.com>
2022-02-25 01:54:56 +0900

    chore: prettier

--- src/foundation/components/Animation/AnimationComponent.ts
+++ src/foundation/components/Animation/AnimationComponent.ts
@@ -1,6 +1,6 @@
 import Component from '../../core/Component';
 import ComponentRepository from '../../core/ComponentRepository';
-import EntityRepository, { applyMixins } from '../../core/EntityRepository';
+import EntityRepository, {applyMixins} from '../../core/EntityRepository';
 import {WellKnownComponentTIDs} from '../WellKnownComponentTIDs';
 import {
   AnimationInterpolationEnum,
@@ -18,6 +18,7 @@ import {
   Array4,
   Array3,
   VectorComponentN,
+  Array1,
 } from '../../../types/CommonTypes';
 import {
   AnimationPathName,
@@ -35,6 +36,7 @@ import Quaternion from '../../math/Quaternion';
 import {
   array3_lerp_offsetAsComposition,
   arrayN_lerp_offsetAsComposition,
+  get1_offset,
   get3_offset,
   get3_offsetAsComposition,
   get4_offset,
@@ -71,7 +73,7 @@ export default class AnimationComponent extends Component {
     IVector | IQuaternion | number[]
   > = new Map();
 
-  //
+  // The name of the current Active Track
   private __currentActiveAnimationTrackName?: AnimationTrackName;
 
   // Animation Data of each AnimationComponent
@@ -634,8 +636,15 @@ export default class AnimationComponent extends Component {
             channel.sampler.outputComponentN
         ) as Array3<number>;
         return value;
+      } else if (channel.sampler.outputComponentN === 1) {
+        // Effekseer (Animation Event)
+        const value = array[get1_offset](
+          channel.sampler.outputComponentN * 3 * keyFrameId +
+            channel.sampler.outputComponentN
+        ) as Array1<number>;
+        return value;
       } else {
-        // weights
+        // weights // outputComponentN === N
         const value = array[getN_offset](
           channel.sampler.outputComponentN * 3 * keyFrameId +
             channel.sampler.outputComponentN,
