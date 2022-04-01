207b0bdc7b860aa16560ac559a8de762a77c3c08
Yuki Shimada <emadurandal@gmail.com>
2022-04-01 14:44:17 +0900

    refactor: "Primitive" sample

--- samples/test_e2e/Primitives/main.ts
+++ samples/test_e2e/Primitives/main.ts
@@ -28,8 +28,6 @@ let p: HTMLParagraphElement | undefined;
 
 function createGroupOfShapes(): IMeshEntity {
   const group = Rn.EntityHelper.createGroupEntity();
-  const material = Rn.MaterialHelper.createClassicUberMaterial();
-  material.cullFace = false;
 
   const creators = [
     createPlane,
@@ -41,108 +39,62 @@ function createGroupOfShapes(): IMeshEntity {
     createSphere,
   ];
   for (const creator of creators) {
-    const shape = creator(material);
+    const shape = creator();
     group.getSceneGraph().addChild(shape.getSceneGraph());
   }
 
   return group as IMeshEntity;
 }
 
-function createGrid(material: Material) {
-  const primitive = new Rn.Grid();
-  primitive.generate({
-    length: 1,
-    division: 5,
-    isXZ: true,
-    isXY: true,
-    isYZ: true,
-    material,
-  });
-  const shape = Rn.MeshHelper.createShape(primitive);
+function createGrid() {
+  const shape = Rn.MeshHelper.createGrid();
   shape.rotate = Rn.Vector3.fromCopy3(90, 0, 0);
 
   return shape;
 }
 
-function createPlane(material: Material) {
-  const primitive = new Rn.Plane();
-  primitive.generate({
-    width: 1,
-    height: 1,
-    uSpan: 5,
-    vSpan: 5,
-    isUVRepeat: false,
-    material,
-  });
-  const shape = Rn.MeshHelper.createShape(primitive);
+function createPlane() {
+  const shape = Rn.MeshHelper.createPlane();
   shape.rotate = Rn.Vector3.fromCopy3(90, 0, 0);
   shape.translate = Rn.Vector3.fromCopy3(3, 0, 0);
 
   return shape;
 }
 
-function createCube(material: Material) {
-  const primitive = new Rn.Cube();
-  primitive.generate({
-    widthVector: Rn.Vector3.fromCopy3(1, 1, 1),
-    material,
-  });
-  const shape = Rn.MeshHelper.createShape(primitive);
+function createCube() {
+  const shape = Rn.MeshHelper.createCube();
   shape.rotate = Rn.Vector3.fromCopy3(90, 0, 0);
   shape.translate = Rn.Vector3.fromCopy3(0, 3, 0);
 
   return shape;
 }
 
-function createAxis(material: Material) {
-  const primitive = new Rn.Axis();
-  primitive.generate({
-    length: 1,
-    material,
-  });
-  const shape = Rn.MeshHelper.createShape(primitive);
+function createAxis() {
+  const shape = Rn.MeshHelper.createAxis();
   shape.rotate = Rn.Vector3.fromCopy3(90, 0, 0);
   shape.translate = Rn.Vector3.fromCopy3(0, 6, 0);
 
   return shape;
 }
 
-function createJoint(material: Material) {
-  const primitive = new Rn.Joint();
-  primitive.generate({
-    material,
-  });
-  const shape = Rn.MeshHelper.createShape(primitive);
+function createJoint() {
+  const shape = Rn.MeshHelper.createJoint();
   shape.rotate = Rn.Vector3.fromCopy3(90, 0, 0);
   shape.translate = Rn.Vector3.fromCopy3(3, 3, 0);
 
   return shape;
 }
 
-function createLine(material: Material) {
-  const primitive = new Rn.Line();
-  primitive.generate({
-    startPos: Rn.Vector3.fromCopy3(0, 0, 0),
-    endPos: Rn.Vector3.fromCopy3(1, 0, 0),
-    hasTerminalMark: true,
-    material,
-  });
-  const shape = Rn.MeshHelper.createShape(primitive);
+function createLine() {
+  const shape = Rn.MeshHelper.createLine();
   shape.rotate = Rn.Vector3.fromCopy3(90, 0, 0);
   shape.translate = Rn.Vector3.fromCopy3(6, 3, 0);
 
   return shape;
 }
 
-function createSphere(material: Material) {
-  const primitive = new Rn.Sphere();
-  primitive.generate({
-    radius: 1,
-    widthSegments: 10,
-    heightSegments: 10,
-    material,
-  });
-  const shape = Rn.MeshHelper.createShape(primitive);
+function createSphere() {
+  const shape = Rn.MeshHelper.createSphere();
   shape.rotate = Rn.Vector3.fromCopy3(90, 0, 0);
   shape.translate = Rn.Vector3.fromCopy3(6, 6, 0);
 
