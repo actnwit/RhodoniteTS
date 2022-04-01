6907818249e2793b42a75790078660fe9c2398c9
Yuki Shimada <emadurandal@gmail.com>
2022-04-01 14:37:10 +0900

    feat: add helper functions

--- src/foundation/helpers/MeshHelper.ts
+++ src/foundation/helpers/MeshHelper.ts
@@ -1,14 +1,15 @@
-import TransformComponent from '../components/Transform/TransformComponent';
-import SceneGraphComponent from '../components/SceneGraph/SceneGraphComponent';
-import MeshComponent from '../components/Mesh/MeshComponent';
-import MeshRendererComponent from '../components/MeshRenderer/MeshRendererComponent';
-import EntityRepository from '../core/EntityRepository';
 import {Plane, PlaneDescriptor} from '../geometry/shapes/Plane';
 import Mesh from '../geometry/Mesh';
 import {AxisDescriptor} from '../geometry/shapes/Axis';
 import {Axis} from '../geometry/shapes/Axis';
 import {IShape} from '../geometry/shapes/IShape';
 import EntityHelper from './EntityHelper';
+import {Line, LineDescriptor} from '../geometry/shapes/Line';
+import Vector3 from '../math/Vector3';
+import {Grid, GridDescriptor} from '../geometry/shapes/Grid';
+import { Cube, CubeDescriptor } from '../geometry/shapes/Cube';
+import { Sphere, SphereDescriptor } from '../geometry/shapes/Sphere';
+import { Joint, JointDescriptor } from '../geometry/shapes/Joint';
 
 const createPlane = (
   desc: PlaneDescriptor = {
@@ -20,6 +21,65 @@ const createPlane = (
   }
 ) => {
   const primitive = new Plane();
+  primitive.generate(desc);
+  const entity = createShape(primitive);
+  return entity;
+};
+
+const createLine = (
+  desc: LineDescriptor = {
+    startPos: Vector3.fromCopy3(0, 0, 0),
+    endPos: Vector3.fromCopy3(1, 0, 0),
+    hasTerminalMark: true,
+  }
+) => {
+  const primitive = new Line();
+  primitive.generate(desc);
+  const entity = createShape(primitive);
+  return entity;
+};
+
+const createGrid = (
+  desc: GridDescriptor = {
+    length: 1,
+    division: 10,
+    isXY: true,
+    isXZ: true,
+    isYZ: true,
+  }
+) => {
+  const primitive = new Grid();
+  primitive.generate(desc);
+  const entity = createShape(primitive);
+  return entity;
+};
+
+const createCube = (
+  desc: CubeDescriptor = {
+    widthVector: Vector3.fromCopy3(1, 1, 1),
+  }
+) => {
+  const primitive = new Cube();
+  primitive.generate(desc);
+  const entity = createShape(primitive);
+  return entity;
+};
+
+const createSphere = (
+  desc: SphereDescriptor = {
+    radius: 1,
+    widthSegments: 10,
+    heightSegments: 10,
+  }
+) => {
+  const primitive = new Sphere();
+  primitive.generate(desc);
+  const entity = createShape(primitive);
+  return entity;
+};
+
+const createJoint = (desc: JointDescriptor = {}) => {
+  const primitive = new Joint();
   primitive.generate(desc);
   const entity = createShape(primitive);
   return entity;
@@ -46,7 +106,12 @@ function createShape(primitive: IShape) {
 }
 
 export const MeshHelper = Object.freeze({
-  createShape,
   createPlane,
+  createLine,
+  createGrid,
+  createCube,
+  createSphere,
+  createJoint,
   createAxis,
+  createShape,
 });
