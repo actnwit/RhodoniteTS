4b278436786f14bcd40deef8b48484aebac0b56d
Yuki Shimada <emadurandal@gmail.com>
2022-04-01 21:21:50 +0900

    test: change test to check the shortcut methods

--- src/foundation/components/Transform/TransformComponent.test.ts
+++ src/foundation/components/Transform/TransformComponent.test.ts
@@ -8,14 +8,17 @@ test('Use translate simply', () => {
   Rn.MemoryManager.createInstanceIfNotCreated(1, 1, 1);
 
   const firstEntity = generateEntity();
-  // console.log(firstEntity);
   const transformComponent = firstEntity.getTransform();
-  // console.log(transformComponent);
   transformComponent.translate = Rn.Vector3.fromCopyArray([1, 0, 0]);
-  // console.log(transformComponent.translateInner);
-  // console.log(transformComponent.translate);
-  console.log(Rn.Vector3.fromCopyArray([1, 0, 0]));
   expect(
     transformComponent.translate.isEqual(Rn.Vector3.fromCopyArray([1, 0, 0]))
+  ).toBe(true);
+  firstEntity.scale = Rn.Vector3.fromCopyArray([2, 1, 1]);
+  expect(
+    transformComponent.translate.isEqual(Rn.Vector3.fromCopyArray([1, 0, 0]))
+  ).toBe(true);
+
+  expect(
+    transformComponent.scale.isEqual(Rn.Vector3.fromCopyArray([2, 1, 1]))
   ).toBe(true);
 });
