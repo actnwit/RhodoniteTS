d739d4f3b173ad97d9ee24c179510ada29e7b53b
Yuki Shimada <emadurandal@gmail.com>
2022-04-16 14:00:26 +0900

    fix: change Config values for mobile devices

--- src/foundation/core/Config.ts
+++ src/foundation/core/Config.ts
@@ -1,17 +1,17 @@
 /* eslint-disable prefer-const */
 import {BoneDataType} from '../definitions/BoneDataType';
+import {MiscUtil} from '../misc';
 
-let byteAlignOfBuffer = 16;
 let maxEntityNumber = 5000;
 let maxLightNumberInShader = 4;
 let maxVertexMorphNumberInShader = 41;
 let maxMaterialInstanceForEachType = 500;
 let maxSkeletonNumber = 33;
-let maxCameraNumber = 15;
+let maxCameraNumber = 20;
 let maxSizeLimitOfNonCompressedTexture = 512;
 
 /// ShaderGlobalStorageSize ///
-let maxSkeletalBoneNumber = 250;
+let maxSkeletalBoneNumber = 300;
 let maxSkeletalBoneNumberForUniformMode = 50; // For Uniform Mode
 let dataTextureWidth = Math.pow(2, 12);
 let dataTextureHeight = Math.pow(2, 12);
@@ -21,6 +21,14 @@ let maxMorphTargetNumber = 4;
 let totalSizeOfGPUShaderDataStorageExceptMorphData = 0;
 let isUboEnabled = false;
 let eventTargetDom: HTMLElement | undefined;
+
+if (MiscUtil.isMobile()) {
+  maxMaterialInstanceForEachType = 28;
+  maxVertexMorphNumberInShader = 4;
+  maxSkeletonNumber = 32;
+  dataTextureWidth = 2 ** 9;
+  dataTextureHeight = 2 ** 9;
+}
 
 export const Config = {
   maxEntityNumber,
