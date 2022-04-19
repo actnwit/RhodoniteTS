542e8e9da246204ecf1df144155a986007aac7ef
Yuki Shimada <emadurandal@gmail.com>
2022-04-20 06:41:22 +0900

    test: simplify RnPromise test

--- src/foundation/misc/RnPromise.test.ts
+++ src/foundation/misc/RnPromise.test.ts
@@ -1,96 +1,90 @@
 import {RnPromise, CallbackObj} from './RnPromise';
 
-test('works with Promise.all', async () => {
-  new Promise(resolve => {
-    const p1 = RnPromise.resolve(1);
-    const p2 = RnPromise.resolve(2);
-    const p3 = RnPromise.resolve(3);
+test('works with RnPromise.all', () => {
+  const p1 = RnPromise.resolve(1);
+  const p2 = RnPromise.resolve(2);
+  const p3 = RnPromise.resolve(3);
 
-    RnPromise.all([p1, p2, p3]).then((results: any) => {
-      // console.log(results); // [1, 2, 3]
-      expect(results).toEqual([1, 2, 3]);
-      resolve();
-    });
-  }) as Promise<void>;
+  RnPromise.all([p1, p2, p3]).then((results: any) => {
+    // console.log(results); // [1, 2, 3]
+    expect(results).toEqual([1, 2, 3]);
+  });
 });
 
-test('RnPromise.resolve of thenable', async () => {
-  new Promise(resolve => {
-    const thenable = {
-      then: function (resolve: any) {
-        resolve('Resolving');
-      },
-    };
-    const p1 = RnPromise.resolve(thenable);
+test('works with Promise.all', () => {
+  const p1 = RnPromise.resolve(1);
+  const p2 = RnPromise.resolve(2);
+  const p3 = RnPromise.resolve(3);
 
-    RnPromise.all([p1]).then((results: any) => {
+  Promise.all([p1, p2, p3]).then((results: any) => {
+    // console.log(results); // [1, 2, 3]
+    expect(results).toEqual([1, 2, 3]);
+  });
+});
+
+test('RnPromise.resolve of thenable', () => {
+  const thenable = {
+    then: function (resolve: any) {
+      resolve('Resolving');
+    },
+  };
+  const p1 = RnPromise.resolve(thenable);
+
+  RnPromise.all([p1]).then((results: any) => {
+    // console.log(results);
+    expect(results).toEqual(['Resolving']);
+  });
+});
+
+test('RnPromise.resolve of resolving rnPromise', () => {
+  const rnPromise = new RnPromise((resolve, reject) => {
+    resolve('resolve');
+  });
+  const p1 = RnPromise.resolve(rnPromise);
+
+  RnPromise.all([p1])
+    .then((results: any) => {
       // console.log(results);
-      expect(results).toEqual(['Resolving']);
-      resolve();
-    });
-  }) as Promise<void>;
+      expect(results).toEqual(['resolve']);
+    })
+    .catch((results: any) => {
+      // console.log(results);
+    })
+    .finally(() => {});
 });
 
-test('RnPromise.resolve of resolving rnPromise', async () => {
-  new Promise(resolve => {
-    const rnPromise = new RnPromise((resolve, reject) => {
-      resolve('resolve');
-    });
-    const p1 = RnPromise.resolve(rnPromise);
+test('RnPromise.resolve of rejecting rnPromise', () => {
+  const rnPromise = new RnPromise((resolve, reject) => {
+    reject('reject');
+  });
+  const p1 = RnPromise.resolve(rnPromise);
 
-    RnPromise.all([p1])
-      .then((results: any) => {
-        // console.log(results);
-        expect(results).toEqual(['resolve']);
-      })
-      .catch((results: any) => {
-        // console.log(results);
-      })
-      .finally(() => {
-        resolve();
-      });
-  }) as Promise<void>;
+  RnPromise.all([p1])
+    .then((results: any) => {
+      // console.log(results);
+    })
+    .catch((results: any) => {
+      // console.log(results);
+      expect(results).toEqual('reject');
+    })
+    .finally(() => {});
 });
 
-test('RnPromise.resolve of rejecting rnPromise', async () => {
-  new Promise(resolve => {
-    const rnPromise = new RnPromise((resolve, reject) => {
-      reject('reject');
-    });
-    const p1 = RnPromise.resolve(rnPromise);
+test('works with Promise.all callback', () => {
+  const p1 = RnPromise.resolve(1);
+  const p2 = RnPromise.resolve(2);
+  const p3 = RnPromise.resolve(3);
 
-    RnPromise.all([p1])
-      .then((results: any) => {
-        // console.log(results);
-      })
-      .catch((results: any) => {
-        // console.log(results);
-        expect(results).toEqual('reject');
-      })
-      .finally(() => {
-        resolve();
-      });
-  }) as Promise<void>;
-});
+  let count = 0;
+  const callback = (obj: CallbackObj) => {
+    console.log(obj);
+    expect(obj.resolvedNum).toEqual(Math.min(++count, 3));
+  };
 
-test('works with Promise.all callback', async () => {
-  new Promise(resolve => {
-    const p1 = RnPromise.resolve(1);
-    const p2 = RnPromise.resolve(2);
-    const p3 = RnPromise.resolve(3);
-
-    let count = 0;
-    const callback = (obj: CallbackObj) => {
-      expect(obj.resolvedNum).toEqual(Math.min(++count, 3));
-    };
-
-    RnPromise.allWithProgressCallback([p1, p2, p3], callback).then(
-      (results: any) => {
-        expect(results).toEqual([1, 2, 3]);
-        p1.then(() => {
-          resolve();
-        });
-      }
-    );
-  }) as Promise<void>;
+  RnPromise.allWithProgressCallback([p1, p2, p3], callback).then(
+    (results: any) => {
+      expect(results).toEqual([1, 2, 3]);
+      p1.then(() => {});
+    }
+  );
 });
