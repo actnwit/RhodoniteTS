5f4fde75ae3a10bac1a5f970eb6f8d55b97a5d7a
Yuki Shimada <emadurandal@gmail.com>
2022-04-22 00:45:13 +0900

    fix: test

--- src/foundation/math/AbstractMatrix.test.ts
+++ src/foundation/math/AbstractMatrix.test.ts
@@ -3,10 +3,10 @@ import {Matrix22} from './Matrix22';
 test('AbstractMatrix::isTheSourceSame', () => {
   const mat_source = new Float32Array([0, 1, 2, 3]);
   const mat_source2 = new Float32Array([-1, -1, -1, -1]);
-  const mat1 = new Matrix22(mat_source, false, true);
-  const mat2 = new Matrix22(mat_source, false, true);
-  const matAnother = new Matrix22(mat_source2, false, true);
-  const matAnother2 = new Matrix22(0, 0, 0, 0);
+  const mat1 = new Matrix22(mat_source);
+  const mat2 = new Matrix22(mat_source);
+  const matAnother = new Matrix22(mat_source2);
+  const matAnother2 = Matrix22.fromCopy4RowMajor(0, 0, 0, 0);
 
   expect(mat1.isTheSourceSame(mat2._v.buffer)).toBe(true);
   expect(mat1.isTheSourceSame(matAnother._v.buffer)).toBe(false);
