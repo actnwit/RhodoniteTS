aa7f6876b2f7213134db5587ca1d16818112cf96
Yuki Shimada <emadurandal@gmail.com>
2022-06-13 20:42:47 +0900

    feat: add sample "GltfImporter-forward-render-pipeline"

---
+++ samples/test_e2e/GltfImporter-forward-render-pipeline/main.ts
@@ -0,0 +1,162 @@
+import Rn from '../../../dist/esm/index.mjs';
+
+const p = document.createElement('p');
+document.body.appendChild(p);
+
+declare const window: any;
+
+(async () => {
+  Rn.Config.isUboEnabled = false;
+  const canvas = document.getElementById('world') as HTMLCanvasElement;
+  await Rn.System.init({
+    approach: Rn.ProcessApproach.FastestWebGL2,
+    canvas,
+  });
+
+  Rn.MeshRendererComponent.isDepthMaskTrueForTransparencies = true;
+
+  // create ForwardRenderPipeline
+  const forwardRenderPipeline = new Rn.ForwardRenderPipeline();
+  forwardRenderPipeline.setup(canvas.width, canvas.height);
+
+  // camera
+  const {cameraComponent, cameraEntity} = createCamera();
+
+  // gltf
+  const mainExpression = await Rn.GltfImporter.import(
+    '../../../assets/gltf/glTF-Sample-Models/2.0/IridescentDishWithOlives/glTF-Binary/IridescentDishWithOlives.glb',
+    {
+      cameraComponent: cameraComponent,
+      defaultMaterialHelperArgumentArray: [
+        {
+          makeOutputSrgb: false,
+        },
+      ],
+    }
+  );
+
+  // env
+  const envExpression = createEnvCubeExpression(
+    './../../../assets/ibl/papermill',
+    cameraEntity
+  );
+
+  const mainRenderPass = mainExpression.renderPasses[0];
+  // cameraController
+  const mainCameraControllerComponent = cameraEntity.getCameraController();
+  const controller =
+    mainCameraControllerComponent.controller as Rn.OrbitCameraController;
+  controller.setTarget(mainRenderPass.sceneTopLevelGraphComponents[0].entity);
+  controller.dolly = 0.83;
+
+  forwardRenderPipeline.setExpressions([envExpression, mainExpression]);
+
+  // lighting
+  setIBL('./../../../assets/ibl/papermill');
+
+  let count = 0;
+  let startTime = Date.now();
+  const draw = function (frame) {
+    if (count > 50) {
+      p.id = 'rendered';
+      p.innerText = 'Rendered.';
+    } else if (count === 1) {
+      p.id = 'started';
+      p.innerText = 'Started.';
+    }
+
+    if (window.isAnimating) {
+      const date = new Date();
+      const time = (date.getTime() - startTime) / 1000;
+      Rn.AnimationComponent.globalTime = time;
+      if (time > Rn.AnimationComponent.endInputValue) {
+        startTime = date.getTime();
+      }
+    }
+
+    Rn.System.process(frame);
+
+    count++;
+  };
+
+  forwardRenderPipeline.startRenderLoop(draw);
+})();
+
+function createCamera() {
+  const cameraEntity = Rn.EntityHelper.createCameraControllerEntity();
+  const cameraComponent = cameraEntity.getCamera();
+  cameraComponent.zNear = 0.1;
+  cameraComponent.zFar = 1000.0;
+  cameraComponent.setFovyAndChangeFocalLength(30.0);
+  cameraComponent.aspect = 1.0;
+  return {cameraComponent, cameraEntity};
+}
+
+function setIBL(baseUri) {
+  const specularCubeTexture = new Rn.CubeTexture();
+  specularCubeTexture.baseUriToLoad = baseUri + '/specular/specular';
+  specularCubeTexture.isNamePosNeg = true;
+  specularCubeTexture.hdriFormat = Rn.HdriFormat.RGBE_PNG;
+  specularCubeTexture.mipmapLevelNumber = 10;
+
+  const diffuseCubeTexture = new Rn.CubeTexture();
+  diffuseCubeTexture.baseUriToLoad = baseUri + '/diffuse/diffuse';
+  diffuseCubeTexture.hdriFormat = Rn.HdriFormat.RGBE_PNG;
+  diffuseCubeTexture.mipmapLevelNumber = 1;
+  diffuseCubeTexture.isNamePosNeg = true;
+
+  const meshRendererComponents = Rn.ComponentRepository.getComponentsWithType(
+    Rn.MeshRendererComponent
+  ) as Rn.MeshRendererComponent[];
+  for (let i = 0; i < meshRendererComponents.length; i++) {
+    const meshRendererComponent = meshRendererComponents[i];
+    meshRendererComponent.specularCubeMap = specularCubeTexture;
+    meshRendererComponent.diffuseCubeMap = diffuseCubeTexture;
+  }
+}
+
+function createEnvCubeExpression(baseuri, cameraEntity) {
+  const environmentCubeTexture = new Rn.CubeTexture();
+  environmentCubeTexture.baseUriToLoad = baseuri + '/environment/environment';
+  environmentCubeTexture.isNamePosNeg = true;
+  environmentCubeTexture.hdriFormat = Rn.HdriFormat.LDR_SRGB;
+  environmentCubeTexture.mipmapLevelNumber = 1;
+  environmentCubeTexture.loadTextureImagesAsync();
+
+  const sphereMaterial = Rn.MaterialHelper.createEnvConstantMaterial();
+  sphereMaterial.setTextureParameter(
+    Rn.ShaderSemantics.ColorEnvTexture,
+    environmentCubeTexture
+  );
+  sphereMaterial.setParameter(
+    Rn.EnvConstantMaterialContent.EnvHdriFormat,
+    Rn.HdriFormat.LDR_SRGB.index
+  );
+
+  const spherePrimitive = new Rn.Sphere();
+  spherePrimitive.generate({
+    radius: 20,
+    widthSegments: 40,
+    heightSegments: 40,
+    material: sphereMaterial,
+  });
+
+  const sphereMesh = new Rn.Mesh();
+  sphereMesh.addPrimitive(spherePrimitive);
+
+  const sphereEntity = Rn.EntityHelper.createMeshEntity();
+  sphereEntity.getTransform().scale = Rn.Vector3.fromCopyArray([-1, 1, 1]);
+  sphereEntity.getTransform().translate = Rn.Vector3.fromCopyArray([0, 0, 0]);
+
+  const sphereMeshComponent = sphereEntity.getMesh();
+  sphereMeshComponent.setMesh(sphereMesh);
+
+  const sphereRenderPass = new Rn.RenderPass();
+  sphereRenderPass.addEntities([sphereEntity]);
+  sphereRenderPass.cameraComponent = cameraEntity.getCamera();
+
+  const sphereExpression = new Rn.Expression();
+  sphereExpression.addRenderPasses([sphereRenderPass]);
+
+  return sphereExpression;
+}
---
+++ samples/test_e2e/GltfImporter-forward-render-pipeline/test.js
@@ -0,0 +1,27 @@
+test('regression test GltfImporter-forward-render-pipeline', async () => {
+
+  const page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(2000000);
+  await page.goto(
+    'http://localhost:8082/samples/test_e2e/GltfImporter-forward-render-pipeline'
+  );
+  await page.setViewport({width: 1000, height: 1000});
+  await page.waitForSelector('p#started', {timeout: 2000000});
+
+  await page.mouse.down();
+  await page.mouse.move(0, 0);
+  await page.mouse.move(0, 100);
+  await page.mouse.move(50, 100);
+  await page.mouse.up();
+
+  await page.waitForSelector('p#rendered', {timeout: 2000000});
+
+  const canvasElement = await page.$('#world');
+  const image = await canvasElement.screenshot();
+  expect(image).toMatchImageSnapshot({
+    failureThreshold: 0.03,
+    failureThresholdType: 'percent',
+  });
+
+  await page.close();
+});
