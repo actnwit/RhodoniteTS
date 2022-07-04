83335923c6eb1fc104a4559ba46230c763e191e4
Yuki Shimada <emadurandal@gmail.com>
2022-07-04 19:05:37 +0900

    feat: add defaultTrackName = 'Default' to AnimationComponent

--- src/foundation/components/Animation/AnimationComponent.ts
+++ src/foundation/components/Animation/AnimationComponent.ts
@@ -96,6 +96,7 @@ export class AnimationComponent extends Component {
 
   // Global animation time in Rhodonite
   public static globalTime = 0;
+  public static defaultTrackName = 'Default';
 
   // Event for pubsub of notifications
   public static readonly Event = {
