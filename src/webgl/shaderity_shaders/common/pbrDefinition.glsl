cf642551c2c36b99a1c3ae1433eb2150fba372f2
Yuki Shimada <emadurandal@gmail.com>
2022-07-08 21:46:21 +0900

    feat: add sheenCharlieVisibility shader function

--- src/webgl/shaderity_shaders/common/pbrDefinition.glsl
+++ src/webgl/shaderity_shaders/common/pbrDefinition.glsl
@@ -228,3 +228,24 @@ float sheenSimpleVisibility(float NdotL, float NdotV) {
   return 1.0 / (4.0 * (NdotL + NdotV - NdotL * NdotV));
 }
 
+// https://github.com/KhronosGroup/glTF/tree/main/extensions/2.0/Khronos/KHR_materials_sheen#sheen-visibility
+float charlieL(float x, float alphaG) {
+  float oneMinusAlphaSq = (1.0 - alphaG) * (1.0 - alphaG);
+  float a = mix(21.5473, 25.3245, oneMinusAlphaSq);
+  float b = mix(3.82987, 3.32435, oneMinusAlphaSq);
+  float c = mix(0.19823, 0.16801, oneMinusAlphaSq);
+  float d = mix(-1.97760, -1.27393, oneMinusAlphaSq);
+  float e = mix(-4.32054, -4.85967, oneMinusAlphaSq);
+  return a / (1.0 + b * pow(x, c)) + d * x + e;
+}
+
+float lambdaSheen(float cosTheta, float alphaG)
+{
+  return abs(cosTheta) < 0.5 ? exp(charlieL(cosTheta, alphaG)) : exp(2.0 * charlieL(0.5, alphaG) - charlieL(1.0 - cosTheta, alphaG));
+}
+
+float sheenCharlieVisibility(float NdotL, float NdotV, float sheenPerceptualRoughness) {
+  float alphaG = sheenPerceptualRoughness * sheenPerceptualRoughness;
+  float sheenVisibility = 1.0 / ((1.0 + lambdaSheen(NdotV, alphaG) + lambdaSheen(NdotL, alphaG)) * (4.0 * NdotV * NdotL));
+  return sheenVisibility;
+}
