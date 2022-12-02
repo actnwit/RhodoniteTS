0a17029c8074c7d3b8575279f18cbb1084c0c984
Yuki Shimada <emadurandal@gmail.com>
2022-12-02 16:39:03 +0900

    fix: simpleMVPPosition.glsl

--- src/webgl/shaderity_shaders/common/simpleMVPPosition.glsl
+++ src/webgl/shaderity_shaders/common/simpleMVPPosition.glsl
@@ -1,13 +1,12 @@
+mat4 worldMatrix = get_worldMatrix(a_instanceInfo);
+
 #ifdef RN_NO_CAMERA_TRANSFORM
-
-gl_Position = worldMatrix * vec4(a_position, 1.0);
-
+  gl_Position = worldMatrix * vec4(a_position, 1.0);
 #else
 
-float cameraSID = u_currentComponentSIDs[/* shaderity: @{WellKnownComponentTIDs.CameraComponentTID} */];
-mat4 worldMatrix = get_worldMatrix(a_instanceInfo);
-mat4 viewMatrix = get_viewMatrix(cameraSID, 0);
-mat4 projectionMatrix = get_projectionMatrix(cameraSID, 0);
-gl_Position = projectionMatrix * viewMatrix * worldMatrix * vec4(a_position, 1.0);
+  float cameraSID = u_currentComponentSIDs[/* shaderity: @{WellKnownComponentTIDs.CameraComponentTID} */];
+  mat4 viewMatrix = get_viewMatrix(cameraSID, 0);
+  mat4 projectionMatrix = get_projectionMatrix(cameraSID, 0);
+  gl_Position = projectionMatrix * viewMatrix * worldMatrix * vec4(a_position, 1.0);
 
 #endif
