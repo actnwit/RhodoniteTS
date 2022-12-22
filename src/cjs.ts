d7e008218c2930f4fd09c7ebe77d63d7bba1456a
Yuki Shimada <emadurandal@gmail.com>
2022-12-22 09:43:16 +0900

    add sideEffects

--- src/cjs.ts
+++ src/cjs.ts
@@ -1,8 +1,3 @@
 import * as Rn from './index';
 
-export const ProcessApproach = Rn.ProcessApproach;
-export const System = Rn.System;
-export const MeshHelper = Rn.MeshHelper;
-export const Vector3 = Rn.Vector3;
-
 export default Rn;
