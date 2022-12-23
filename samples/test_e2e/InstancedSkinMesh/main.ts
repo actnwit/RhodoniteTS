29d4cab9c2414c08475bad28d0052bf2b37301cf
Yuki Shimada <emadurandal@gmail.com>
2022-12-23 16:25:06 +0900

    feat: update Gltf2Importer-skinning sample as InstancedSkinMesh sample

--- samples/test_e2e/InstancedSkinMesh/main.ts
+++ samples/test_e2e/InstancedSkinMesh/main.ts
@@ -1,7 +1,8 @@
-import Rn from '../../../dist/esm/index.js';
+import Rn from '../../../dist/esmdev/index.js';
 let p: any;
 
 declare const window: any;
+declare const Stats: any;
 
 (async () => {
   const promises = [];
@@ -10,10 +11,16 @@ declare const window: any;
 
   //-------------------------------
   Promise.all(promises).then(async () => {
+    Rn.Config.maxEntityNumber = 20000;
+    Rn.Config.dataTextureWidth = Math.pow(2, 13);
+    Rn.Config.dataTextureHeight = Math.pow(2, 13);
+    Rn.Config.maxSkeletonNumber = 200;
     const gl = await Rn.System.init({
-      approach: Rn.ProcessApproach.Uniform,
+      approach: Rn.ProcessApproach.DataTexture,
       canvas: document.getElementById('world') as HTMLCanvasElement,
     });
+
+    const light = Rn.EntityHelper.createLightEntity();
 
     // Camera
     const cameraEntity = Rn.EntityHelper.createCameraControllerEntity();
@@ -23,7 +30,8 @@ declare const window: any;
     cameraComponent.zFar = 1000;
     cameraComponent.setFovyAndChangeFocalLength(45);
     cameraComponent.aspect = 1;
-    cameraEntity.getTransform().localPosition = Rn.Vector3.fromCopyArray([0.0, 0, 0.5]);
+    cameraEntity.localPosition = Rn.Vector3.fromCopyArray([-2.5, 5, 5]);
+    cameraEntity.localEulerAngles = Rn.Vector3.fromCopyArray([-Math.PI / 4, -Math.PI / 4, 0]);
 
     const promise = Rn.Gltf2Importer.importFromUri(
       // '../../../assets/gltf/glTF-Sample-Models/2.0/SimpleSkin/glTF-Embedded/SimpleSkin.gltf'
@@ -37,18 +45,33 @@ declare const window: any;
 
       // CameraComponent
       const cameraControllerComponent = cameraEntity.getCameraController();
-      cameraControllerComponent.controller.setTarget(rootGroup);
+      // cameraControllerComponent.controller.setTarget(rootGroup);
+
+      const groups = [];
+      for (let i = 0; i < 2; i++) {
+        for (let j = 0; j < 2; j++) {
+          const newGroup = Rn.EntityRepository.shallowCopyEntity(rootGroup) as Rn.ISceneGraphEntity;
+          newGroup.getTransform().localPosition = Rn.Vector3.fromCopyArray([i * 2, 0, j * 2]);
+          groups.push(newGroup);
+        }
+      }
 
       // renderPass
       const renderPass = new Rn.RenderPass();
       renderPass.toClearColorBuffer = true;
       renderPass.addEntities([rootGroup]);
+      renderPass.addEntities(groups);
 
       // expression
       const expression = new Rn.Expression();
       expression.addRenderPasses([renderPass]);
 
       Rn.CameraComponent.current = 0;
+
+      const stats = new Stats();
+      stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+      document.body.appendChild(stats.domElement);
+
       let startTime = Date.now();
       const rotationVec3 = Rn.MutableVector3.one();
       let count = 0;
@@ -83,7 +106,10 @@ declare const window: any;
           //rootGroup.getTransform().localPosition = rootGroup.getTransform().localPosition;
         }
 
+        stats.begin();
         Rn.System.process([expression]);
+        stats.end();
+
         count++;
 
         requestAnimationFrame(draw);
--- samples/test_e2e/InstancedSkinMesh/test.js
+++ samples/test_e2e/InstancedSkinMesh/test.js
@@ -1,6 +1,6 @@
 const testCheckPtoDocument = require('../common/testFunc').testCheckPtoDocument;
-const SetURL = 'http://localhost:8082/samples/test_e2e/Gltf2Importer-skinning';
+const SetURL = 'http://localhost:8082/samples/test_e2e/InstancedSkinMesh';
 
-test('regression test Gltf2Importer Skinning', async () => {
+test('regression test Instanced Skin Mesh', async () => {
   await testCheckPtoDocument(jest, browser, SetURL, expect, 0.005);
 });
