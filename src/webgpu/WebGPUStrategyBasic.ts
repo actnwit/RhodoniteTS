5f7707e68299300ab375704bf409e35dea997ed9
Yuki Shimada <emadurandal@gmail.com>
2023-04-08 08:34:26 +0900

    feat: add WebGPUStrategyBasic.ts

--- src/webgpu/WebGPUStrategyBasic.ts
+++ src/webgpu/WebGPUStrategyBasic.ts
@@ -1,1 +1,27 @@
-export class WebGPUStrategyBasic {}
+import { MeshComponent } from '../foundation/components/Mesh/MeshComponent';
+import { MeshRendererComponent } from '../foundation/components/MeshRenderer/MeshRendererComponent';
+import { CGAPIStrategy } from '../foundation/renderer/CGAPIStrategy';
+import { RenderPass } from '../foundation/renderer/RenderPass';
+
+export class WebGPUStrategyBasic implements CGAPIStrategy {
+  $load(meshComponent: MeshComponent): void {
+    throw new Error('Method not implemented.');
+  }
+  $prerender(
+    meshComponent: MeshComponent,
+    meshRendererComponent: MeshRendererComponent,
+    instanceIDBufferUid: number
+  ): void {
+    throw new Error('Method not implemented.');
+  }
+  common_$prerender(): void {
+    throw new Error('Method not implemented.');
+  }
+  common_$render(
+    primitiveUids: Int32Array,
+    renderPass: RenderPass,
+    renderPassTickCount: number
+  ): boolean {
+    throw new Error('Method not implemented.');
+  }
+}
