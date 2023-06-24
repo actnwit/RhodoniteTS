8325f05a5140c44d8af6a6559e43eadc79c5eeee
Yuki Shimada <emadurandal@gmail.com>
2023-06-24 09:06:40 +0900

    feat: add LightGizmo.ts

---
+++ src/foundation/gizmos/LightGizmo.ts
@@ -0,0 +1,118 @@
+import { PrimitiveMode } from '../definitions/PrimitiveMode';
+import { VertexAttribute } from '../definitions/VertexAttribute';
+import { Mesh } from '../geometry/Mesh';
+import { Primitive } from '../geometry/Primitive';
+import { EntityHelper, IMeshEntity } from '../helpers/EntityHelper';
+import { Vector3 } from '../math/Vector3';
+import { Gizmo } from './Gizmo';
+
+export class LightGizmo extends Gizmo {
+  private static __mesh: Mesh;
+  private static __length = 1;
+
+  /**
+   * Constructor
+   * @param target the object which this gizmo belong to
+   */
+  constructor(target: IMeshEntity) {
+    super(target);
+  }
+
+  ///
+  ///
+  /// Accessors
+  ///
+  ///
+
+  get isSetup(): boolean {
+    if (this.__topEntity != null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  ///
+  ///
+  /// Friends Members
+  ///
+  ///
+
+  /**
+   * @internal
+   * setup entities of Gizmo if not done yet
+   */
+  _setup(): void {
+    if (this.__toSkipSetup()) {
+      return;
+    }
+
+    this.__topEntity = EntityHelper.createMeshEntity();
+    this.__topEntity.tryToSetUniqueName(`LightGizmo_of_${this.__target.uniqueName}`, true);
+    this.__topEntity.getSceneGraph()!.toMakeWorldMatrixTheSameAsLocalMatrix = true;
+    this.__target.getSceneGraph()!._addGizmoChild(this.__topEntity!.getSceneGraph());
+
+    const sceneGraphComponent = this.__topEntity!.tryToGetMesh()!;
+    LightGizmo.__mesh = new Mesh();
+    LightGizmo.__mesh.addPrimitive(LightGizmo.__generatePrimitive());
+    sceneGraphComponent.setMesh(LightGizmo.__mesh);
+
+    this.setGizmoTag();
+  }
+
+  /**
+   * @internal
+   * update the transform and etc of the gizmo
+   */
+  _update(): void {
+    if (this.__topEntity == null) {
+      return;
+    }
+    const sg = this.__target.getSceneGraph()!;
+    const aabb = sg.worldAABB;
+    if (aabb.isVanilla()) {
+      this.__topEntity.getTransform()!.localPosition = sg.position;
+    } else {
+      this.__topEntity.getTransform()!.localPosition = aabb.centerPoint;
+    }
+    this.__topEntity.getTransform()!.localScale = Vector3.fromCopyArray([
+      Math.max(1, aabb.isVanilla() ? 1 : aabb.sizeX / 2),
+      Math.max(1, aabb.isVanilla() ? 1 : aabb.sizeY / 2),
+      Math.max(1, aabb.isVanilla() ? 1 : aabb.sizeZ / 2),
+    ]);
+  }
+
+  ///
+  ///
+  /// Private Static Members
+  ///
+  ///
+
+  private static __generatePrimitive(): Primitive {
+    const positions = new Float32Array([
+      // Z axis
+      0,
+      0,
+      0,
+      0,
+      0,
+      this.__length,
+
+      // Arrow
+      0,
+      0,
+      this.__length,
+      0.1,
+      0.1,
+      this.__length - 0.1,
+    ]);
+
+    const primitive = Primitive.createPrimitive({
+      attributeSemantics: [VertexAttribute.Position.XYZ],
+      attributes: [positions],
+      primitiveMode: PrimitiveMode.Lines,
+    });
+
+    return primitive;
+  }
+}
--- src/foundation/gizmos/index.ts
+++ src/foundation/gizmos/index.ts
@@ -1,5 +1,6 @@
 export * from './AABBGizmo';
 export * from './Gizmo';
 export * from './LocatorGizmo';
+export * from './LightGizmo';
 export * from './ScaleGizmo';
 export * from './TranslationGizmo';
