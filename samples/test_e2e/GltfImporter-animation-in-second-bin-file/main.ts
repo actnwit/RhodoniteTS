ad6b1194c7bf49feaf3a6875a2c057fe0e8514e6
Yuki Shimada <emadurandal@gmail.com>
2023-07-29 21:27:55 +0900

    refactor some samples

--- samples/test_e2e/GltfImporter-animation-in-second-bin-file/main.ts
+++ samples/test_e2e/GltfImporter-animation-in-second-bin-file/main.ts
@@ -1,80 +1,74 @@
-import Rn from '../../../dist/esm/index.js';
+import Rn from '../../../dist/esmdev/index.js';
 
 let p: any;
 
 declare const window: any;
 
-(async () => {
-  await Rn.System.init({
-    approach: Rn.ProcessApproach.Uniform,
-    canvas: document.getElementById('world') as HTMLCanvasElement,
-  });
+await Rn.System.init({
+  approach: Rn.ProcessApproach.Uniform,
+  canvas: document.getElementById('world') as HTMLCanvasElement,
+});
 
-  // camera
-  const cameraEntity = Rn.EntityHelper.createCameraControllerEntity();
-  const cameraComponent = cameraEntity.getCamera();
-  cameraComponent.zNear = 0.1;
-  cameraComponent.zFar = 1000.0;
-  cameraComponent.setFovyAndChangeFocalLength(30.0);
-  cameraComponent.aspect = 1.0;
+// camera
+const cameraEntity = Rn.EntityHelper.createCameraControllerEntity();
+const cameraComponent = cameraEntity.getCamera();
+cameraComponent.zNear = 0.1;
+cameraComponent.zFar = 1000.0;
+cameraComponent.setFovyAndChangeFocalLength(30.0);
+cameraComponent.aspect = 1.0;
 
-  // gltf
-  const expression = (
-    await Rn.GltfImporter.importFromUri(
-      '../../../assets/gltf/glTF-Sample-Models/2.0/AnimatedTriangle/glTF-Embedded/AnimatedTriangle.gltf',
-      {
-        cameraComponent: cameraComponent,
-      }
-    )
-  ).unwrapForce();
-
-  // cameraController
-  const mainRenderPass = expression.renderPasses[0];
-  const mainCameraControllerComponent = cameraEntity.getCameraController();
-  const controller = mainCameraControllerComponent.controller as Rn.OrbitCameraController;
-  controller.setTarget(mainRenderPass.sceneTopLevelGraphComponents[0].entity);
-
-  let count = 0;
-  let startTime = Date.now();
-
-  Rn.AnimationComponent.globalTime = 0.05;
-
-  const draw = function () {
-    if (p == null && count > 0) {
-      p = document.createElement('p');
-      p.setAttribute('id', 'rendered');
-      p.innerText = 'Rendered.';
-      document.body.appendChild(p);
+// gltf
+const expression = (
+  await Rn.GltfImporter.importFromUri(
+    '../../../assets/gltf/glTF-Sample-Models/2.0/AnimatedTriangle/glTF-Embedded/AnimatedTriangle.gltf',
+    {
+      cameraComponent: cameraComponent,
     }
+  )
+).unwrapForce();
 
-    if (window.isAnimating) {
-      const date = new Date();
-      const time = (date.getTime() - startTime) / 1000;
-      Rn.AnimationComponent.globalTime = time;
-      if (time > Rn.AnimationComponent.endInputValue) {
-        startTime = date.getTime();
-      }
-    }
+// cameraController
+const mainRenderPass = expression.renderPasses[0];
+const mainCameraControllerComponent = cameraEntity.getCameraController();
+const controller = mainCameraControllerComponent.controller as Rn.OrbitCameraController;
+controller.setTarget(mainRenderPass.sceneTopLevelGraphComponents[0].entity);
 
-    Rn.System.process([expression]);
+let count = 0;
+let startTime = Date.now();
 
-    count++;
+Rn.AnimationComponent.globalTime = 0.05;
 
-    requestAnimationFrame(draw);
-  };
+Rn.System.startRenderLoop(() => {
+  if (p == null && count > 0) {
+    p = document.createElement('p');
+    p.setAttribute('id', 'rendered');
+    p.innerText = 'Rendered.';
+    document.body.appendChild(p);
+  }
 
-  draw();
-
-  function setParameterForMeshComponents(meshComponents, shaderSemantic, value) {
-    for (let i = 0; i < meshComponents.length; i++) {
-      const mesh = meshComponents[i].mesh;
-      if (!mesh) continue;
-
-      const primitiveNumber = mesh.getPrimitiveNumber();
-      for (let j = 0; j < primitiveNumber; j++) {
-        const primitive = mesh.getPrimitiveAt(j);
-        primitive.material.setParameter(shaderSemantic, value);
-      }
+  if (window.isAnimating) {
+    const date = new Date();
+    const time = (date.getTime() - startTime) / 1000;
+    Rn.AnimationComponent.globalTime = time;
+    if (time > Rn.AnimationComponent.endInputValue) {
+      startTime = date.getTime();
     }
   }
-})();
+
+  Rn.System.process([expression]);
+
+  count++;
+});
+
+function setParameterForMeshComponents(meshComponents, shaderSemantic, value) {
+  for (let i = 0; i < meshComponents.length; i++) {
+    const mesh = meshComponents[i].mesh;
+    if (!mesh) continue;
+
+    const primitiveNumber = mesh.getPrimitiveNumber();
+    for (let j = 0; j < primitiveNumber; j++) {
+      const primitive = mesh.getPrimitiveAt(j);
+      primitive.material.setParameter(shaderSemantic, value);
+    }
+  }
+}
--- samples/test_e2e/GltfImporter-draco-extension/main.ts
+++ samples/test_e2e/GltfImporter-draco-extension/main.ts
@@ -1,98 +1,92 @@
-import Rn from '../../../dist/esm/index.js';
+import Rn from '../../../dist/esmdev/index.js';
 
 declare const window: any;
 const p = document.createElement('p');
 document.body.appendChild(p);
 
-(async () => {
-  await Rn.System.init({
-    approach: Rn.ProcessApproach.Uniform,
-    canvas: document.getElementById('world') as HTMLCanvasElement,
-    rnWebGLDebug: true,
-  });
+await Rn.System.init({
+  approach: Rn.ProcessApproach.Uniform,
+  canvas: document.getElementById('world') as HTMLCanvasElement,
+  rnWebGLDebug: true,
+});
 
-  // expressions
-  const expressions = [];
+// expressions
+const expressions = [];
 
-  // camera
-  const cameraEntity = Rn.EntityHelper.createCameraEntity();
-  const cameraComponent = cameraEntity.getCamera();
-  cameraComponent.zNear = 0.1;
-  cameraComponent.zFar = 1000.0;
-  cameraComponent.setFovyAndChangeFocalLength(30.0);
-  cameraComponent.aspect = 1.0;
+// camera
+const cameraEntity = Rn.EntityHelper.createCameraEntity();
+const cameraComponent = cameraEntity.getCamera();
+cameraComponent.zNear = 0.1;
+cameraComponent.zFar = 1000.0;
+cameraComponent.setFovyAndChangeFocalLength(30.0);
+cameraComponent.aspect = 1.0;
 
-  // gltf
-  const mainExpression = (
-    await Rn.GltfImporter.importFromUri(
-      '../../../assets/gltf/glTF-Sample-Models/2.0/BarramundiFish/glTF-Draco/BarramundiFish.gltf',
-      {
-        cameraComponent: cameraComponent,
-        defaultMaterialHelperArgumentArray: [
-          {
-            makeOutputSrgb: false,
-          },
-        ],
-      }
-    )
-  ).unwrapForce();
-  expressions.push(mainExpression);
-
-  // post effects
-  const expressionPostEffect = new Rn.Expression();
-  expressions.push(expressionPostEffect);
-
-  // gamma correction
-  const gammaTargetFramebuffer = Rn.RenderableHelper.createTexturesForRenderTarget(600, 600, 1, {});
-  const mainRenderPass = mainExpression.renderPasses[0];
-  mainRenderPass.setFramebuffer(gammaTargetFramebuffer);
-  mainRenderPass.toClearColorBuffer = true;
-  mainRenderPass.toClearDepthBuffer = true;
-
-  const rootGroup = mainRenderPass.sceneTopLevelGraphComponents[0].entity;
-  const rootTransFormComponent = rootGroup.getTransform();
-  rootTransFormComponent.localEulerAngles = Rn.Vector3.fromCopyArray([0, Math.PI / 2.0, 0.0]);
-  rootTransFormComponent.localPosition = Rn.Vector3.fromCopyArray([0, -0.13, -1.5]);
-
-  const postEffectCameraEntity = createPostEffectCameraEntity();
-  const postEffectCameraComponent = postEffectCameraEntity.getCamera();
-
-  const gammaCorrectionMaterial = Rn.MaterialHelper.createGammaCorrectionMaterial();
-  const gammaCorrectionRenderPass = createPostEffectRenderPass(
-    gammaCorrectionMaterial,
-    postEffectCameraComponent
-  );
-
-  setTextureParameterForMeshComponents(
-    gammaCorrectionRenderPass.meshComponents,
-    Rn.ShaderSemantics.BaseColorTexture,
-    gammaTargetFramebuffer.getColorAttachedRenderTargetTexture(0)
-  );
-
-  expressionPostEffect.addRenderPasses([gammaCorrectionRenderPass]);
-
-  // lighting
-  const lightEntity = Rn.EntityHelper.createLightEntity();
-  const lightComponent = lightEntity.getLight();
-  lightComponent.type = Rn.LightType.Directional;
-  lightComponent.intensity = Rn.Vector3.fromCopyArray([0.5, 0.5, 0.5]);
-
-  let count = 0;
-
-  const draw = function () {
-    if (count > 1) {
-      p.setAttribute('id', 'rendered');
-      p.innerText = 'Rendered.';
+// gltf
+const mainExpression = (
+  await Rn.GltfImporter.importFromUri(
+    '../../../assets/gltf/glTF-Sample-Models/2.0/BarramundiFish/glTF-Draco/BarramundiFish.gltf',
+    {
+      cameraComponent: cameraComponent,
+      defaultMaterialHelperArgumentArray: [
+        {
+          makeOutputSrgb: false,
+        },
+      ],
     }
+  )
+).unwrapForce();
+expressions.push(mainExpression);
 
-    Rn.System.process(expressions);
-    count++;
+// post effects
+const expressionPostEffect = new Rn.Expression();
+expressions.push(expressionPostEffect);
 
-    requestAnimationFrame(draw);
-  };
+// gamma correction
+const gammaTargetFramebuffer = Rn.RenderableHelper.createTexturesForRenderTarget(600, 600, 1, {});
+const mainRenderPass = mainExpression.renderPasses[0];
+mainRenderPass.setFramebuffer(gammaTargetFramebuffer);
+mainRenderPass.toClearColorBuffer = true;
+mainRenderPass.toClearDepthBuffer = true;
 
-  draw();
-})();
+const rootGroup = mainRenderPass.sceneTopLevelGraphComponents[0].entity;
+const rootTransFormComponent = rootGroup.getTransform();
+rootTransFormComponent.localEulerAngles = Rn.Vector3.fromCopyArray([0, Math.PI / 2.0, 0.0]);
+rootTransFormComponent.localPosition = Rn.Vector3.fromCopyArray([0, -0.13, -1.5]);
+
+const postEffectCameraEntity = createPostEffectCameraEntity();
+const postEffectCameraComponent = postEffectCameraEntity.getCamera();
+
+const gammaCorrectionMaterial = Rn.MaterialHelper.createGammaCorrectionMaterial();
+const gammaCorrectionRenderPass = createPostEffectRenderPass(
+  gammaCorrectionMaterial,
+  postEffectCameraComponent
+);
+
+setTextureParameterForMeshComponents(
+  gammaCorrectionRenderPass.meshComponents,
+  Rn.ShaderSemantics.BaseColorTexture,
+  gammaTargetFramebuffer.getColorAttachedRenderTargetTexture(0)
+);
+
+expressionPostEffect.addRenderPasses([gammaCorrectionRenderPass]);
+
+// lighting
+const lightEntity = Rn.EntityHelper.createLightEntity();
+const lightComponent = lightEntity.getLight();
+lightComponent.type = Rn.LightType.Directional;
+lightComponent.intensity = Rn.Vector3.fromCopyArray([0.5, 0.5, 0.5]);
+
+let count = 0;
+
+Rn.System.startRenderLoop(() => {
+  if (count > 1) {
+    p.setAttribute('id', 'rendered');
+    p.innerText = 'Rendered.';
+  }
+
+  Rn.System.process(expressions);
+  count++;
+});
 
 function createPostEffectRenderPass(material: Rn.Material, cameraComponent: Rn.CameraComponent) {
   const boardPrimitive = new Rn.Plane();
