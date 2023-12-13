34d35b2722f377dd5317f1835b7fe752b28003ae
Yuki Shimada <emadurandal@gmail.com>
2023-12-14 00:52:26 +0900

    fix: rename RN_BONE_DATA_TYPE_Mat44x1 to RN_BONE_DATA_TYPE_Mat43x1

--- src/webgl/shaderity_shaders/common/getSkinMatrix.glsl
+++ src/webgl/shaderity_shaders/common/getSkinMatrix.glsl
@@ -68,7 +68,7 @@ highp vec4 unpackedVec2ToNormalizedVec4(highp vec2 vec_xy, highp float criteria)
 
 mat4 getSkinMatrix(float skeletalComponentSID) {
 
-#ifdef RN_BONE_DATA_TYPE_Mat44x1
+#ifdef RN_BONE_DATA_TYPE_Mat43x1
   mat4 skinMat = a_weight.x * mat4(get_boneMatrix(skeletalComponentSID, int(a_joint.x)));
   skinMat += a_weight.y * mat4(get_boneMatrix(skeletalComponentSID, int(a_joint.y)));
   skinMat += a_weight.z * mat4(get_boneMatrix(skeletalComponentSID, int(a_joint.z)));
