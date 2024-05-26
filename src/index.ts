c1a169364b7b93f868c380dfb4b9147b94cdd857
Yuki Shimada <emadurandal@gmail.com>
2024-05-26 23:56:12 +0900

    feat: add display version in console

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,4 @@ export * from './types/index';
 export * from './types/index';
 export * from './effekseer/index';
 export * from './xr/index';
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-export const VERSION = require('./../VERSION-FILE').default;
+export * from './version';
---
+++ src/version.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+export const VERSION = require('./../VERSION-FILE').default;
