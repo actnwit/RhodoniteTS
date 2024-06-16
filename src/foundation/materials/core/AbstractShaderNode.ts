2cfdf5ceb6e57fe00924a290c390ce753441e070
Yuki Shimada <emadurandal@gmail.com>
2024-06-16 19:43:43 +0900

    getShaderCode method to return WGSL code if ProcessApproach.WebGPU.

--- src/foundation/materials/core/AbstractShaderNode.ts
+++ src/foundation/materials/core/AbstractShaderNode.ts
@@ -6,6 +6,8 @@ import { CompositionTypeEnum } from '../../definitions/CompositionType';
 import { ComponentTypeEnum } from '../../definitions/ComponentType';
 import { Socket } from './Socket';
 import { ShaderType, ShaderTypeEnum } from '../../definitions/ShaderType';
+import { SystemState } from '../../system';
+import { ProcessApproach } from '../../definitions';
 
 export type ShaderAttributeOrSemanticsOrString = string | VertexAttributeEnum | ShaderSemanticsEnum;
 
@@ -31,12 +33,13 @@ type ShaderStage = 'Neutral' | 'Vertex' | 'Fragment';
 export abstract class AbstractShaderNode extends RnObject {
   static _shaderNodes: AbstractShaderNode[] = [];
   protected __shaderFunctionName: string;
-  private __codeGLSL?: string;
   protected __inputs: Socket<string, CompositionTypeEnum, ComponentTypeEnum>[] = [];
   protected __outputs: Socket<string, CompositionTypeEnum, ComponentTypeEnum>[] = [];
   protected __inputConnections: ShaderNodeInputConnectionType[] = [];
   private static __invalidShaderNodeCount = -1;
   protected __shaderNodeUid: ShaderNodeUID;
+  private __codeGLSL?: string;
+  private __codeWGSL?: string;
   protected __commonPart?: CommonShaderPart;
   private _shaderStage: ShaderStage = 'Neutral';
 
@@ -51,6 +54,7 @@ export abstract class AbstractShaderNode extends RnObject {
     super();
     this.__shaderFunctionName = shaderNodeName;
     this.__codeGLSL = shader.codeGLSL;
+    this.__codeWGSL = shader.codeWGSL;
     this.__shaderNodeUid = ++AbstractShaderNode.__invalidShaderNodeCount;
     AbstractShaderNode._shaderNodes[AbstractShaderNode.__invalidShaderNodeCount] = this;
     this.__commonPart = shader.commonPart;
@@ -99,13 +103,17 @@ export abstract class AbstractShaderNode extends RnObject {
   }
 
   getShaderCode(shaderStage: ShaderTypeEnum): string {
-    if (this.__codeGLSL != null) {
-      return this.__codeGLSL;
-    } else {
+    if (this.__commonPart != null) {
       if (shaderStage === ShaderType.VertexShader) {
-        return this.__commonPart!.vertexShaderDefinitions;
+        return this.__commonPart.vertexShaderDefinitions;
       } else {
-        return this.__commonPart!.pixelShaderDefinitions;
+        return this.__commonPart.pixelShaderDefinitions;
+      }
+    } else {
+      if (SystemState.currentProcessApproach === ProcessApproach.WebGPU) {
+        return this.__codeWGSL!;
+      } else {
+        return this.__codeGLSL!;
       }
     }
   }
