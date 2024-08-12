931c811c4325d7576cb543c101ccb7cec483fafd
Yuki Shimada <emadurandal@gmail.com>
2024-08-12 16:41:23 +0900

    add IBLfromHDR sample

---
+++ samples/test_e2e/IBLfromHDR/main.ts
@@ -0,0 +1,31 @@
+import Rn from '../../../dist/esmdev/index.js';
+
+declare const window: any;
+
+// Init Rhodonite
+Rn.Config.cgApiDebugConsoleOutput = true;
+await Rn.System.init({
+  approach: Rn.ProcessApproach.DataTexture,
+  canvas: document.getElementById('world') as HTMLCanvasElement,
+});
+
+// Plane
+const planeEntity = Rn.MeshHelper.createPlane();
+planeEntity.localEulerAngles = Rn.Vector3.fromCopy3(Math.PI * 0.5, 0, 0);
+planeEntity.localScale = Rn.Vector3.fromCopy3(0.5, 0.5, 0.5);
+
+// Render Loop
+let count = 0;
+
+Rn.System.startRenderLoop(() => {
+  if (!window._rendered && count > 0) {
+    window._rendered = true;
+    const p = document.createElement('p');
+    p.setAttribute('id', 'rendered');
+    p.innerText = 'Rendered.';
+    document.body.appendChild(p);
+  }
+
+  Rn.System.processAuto();
+  count++;
+});
