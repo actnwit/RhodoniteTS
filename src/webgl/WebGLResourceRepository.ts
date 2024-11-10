7a0bf5ddb9b6b231c41accc0c6caff210a5cf0ef
Yuki Shimada <emadurandal@gmail.com>
2024-11-11 07:19:47 +0900

    fix: use WeakRef

--- src/webgl/WebGLResourceRepository.ts
+++ src/webgl/WebGLResourceRepository.ts
@@ -414,7 +414,7 @@ export class WebGLResourceRepository
     shaderProgram._vertexShaderStr = vertexShaderStr;
     shaderProgram._fragmentShaderStr = fragmentShaderStr;
     shaderProgram._shaderSemanticsInfoMap = new Map();
-    shaderProgram._material = material;
+    shaderProgram._material = new WeakRef(material);
 
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
@@ -2865,7 +2865,7 @@ vec4 fetchVec4FromVec4Block(int vec4Idx) {
   ): boolean {
     // Callback triggered by your engine in case of error. It needs to send the WebGL error to allow the editor to display the error in the gutter.
 
-    const material = this._material;
+    const material = this._material.deref();
     if (Is.not.exist(material)) {
       const warn = 'Material Not found';
       console.warn(warn);
