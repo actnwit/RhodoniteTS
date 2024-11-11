faf96898cf6e56098fc7bdadf0fbe1c27a0ff020
Yuki Shimada <emadurandal@gmail.com>
2024-11-11 17:58:39 +0900

    fix: remove reference to defaultMaterialHelperArgumentArray

--- src/foundation/importer/VrmImporter.ts
+++ src/foundation/importer/VrmImporter.ts
@@ -71,6 +71,11 @@ export class VrmImporter {
     this._readVRMHumanoidInfo(gltfModel as Vrm1, rootGroup);
     this._readExpressions(gltfModel as Vrm1, rootGroup);
     this._readConstraints(gltfModel as Vrm1);
+
+    if (gltfModel.asset.extras?.rnLoaderOptions != null) {
+      // remove reference to defaultMaterialHelperArgumentArray
+      gltfModel.asset.extras.rnLoaderOptions.defaultMaterialHelperArgumentArray = [];
+    }
   }
 
   static _readConstraints(gltfModel: Vrm1) {
