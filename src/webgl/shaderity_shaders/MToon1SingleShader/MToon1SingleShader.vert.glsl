17dec8c402b73a2f982b841013a1d1d7ecbbac34
Yuki Shimada <emadurandal@gmail.com>
2024-12-23 23:58:09 +0900

    implementing MToon1SingleShader.vert.glsl

--- src/webgl/shaderity_shaders/MToon1SingleShader/MToon1SingleShader.vert.glsl
+++ src/webgl/shaderity_shaders/MToon1SingleShader/MToon1SingleShader.vert.glsl
@@ -36,6 +36,9 @@ out vec3 v_binormal_inWorld; // bitangent_inWorld
 
 #pragma shaderity: require(../common/processGeometryWithSkinningOptionally.glsl)
 
+uniform int u_mtoonOutlineWidthType; // initialValue=2
+uniform float u_outlineWidth; // initialValue=1.0
+
 void main(){
 
   #pragma shaderity: require(../common/mainPrerequisites.glsl)
@@ -58,7 +61,35 @@ void main(){
   );
 
   mat4 projectionMatrix = get_projectionMatrix(cameraSID, 0);
+
+#ifdef RN_MTOON_IS_OUTLINE
+  float outlineTex = 1.0;
+
+  if (u_mtoonOutlineWidthType == 1) {
+    float outlineWidth = get_outlineWidth(materialSID, 0);
+    vec3 outlineOffset = 0.01 * outlineWidth * outlineTex * a_normal;
+    vec4 worldOutlineOffset = worldMatrix * vec4(outlineOffset, 0.0);
+    gl_Position = projectionMatrix * viewMatrix * (v_position_inWorld + worldOutlineOffset);
+  } else if (u_mtoonOutlineWidthType == 2) {
+    vec4 vertex = projectionMatrix * viewMatrix * v_position_inWorld;
+
+    vec3 clipNormal = (projectionMatrix * vec4(v_normal_inView, 1.0)).xyz;
+    vec2 projectedNormal = normalize(clipNormal.xy);
+    float outlineScaledMaxDistance = get_outlineScaledMaxDistance(materialSID, 0);
+    projectedNormal *= min(vertex.w, outlineScaledMaxDistance);
+    float aspect = abs(get_aspect(0.0, 0)); //solo datum
+    projectedNormal.x *= aspect;
+
+    float outlineWidth = get_outlineWidth(materialSID, 0);
+    vertex.xy += 0.01 * outlineWidth * outlineTex * projectedNormal * clamp(1.0 - abs(v_normal_inView.z), 0.0, 1.0); // ignore offset when normal toward camera
+
+    gl_Position = vertex;
+  } else { // 0
+    gl_Position = projectionMatrix * viewMatrix * v_position_inWorld;
+  }
+#else
   gl_Position = projectionMatrix * viewMatrix * v_position_inWorld;
+#endif
 
   v_texcoord_0 = a_texcoord_0;
   v_baryCentricCoord = a_baryCentricCoord.xyz;
