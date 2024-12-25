711a5d0c3b1e86674491dfdb99edbbd6b2e90403
Yuki Shimada <emadurandal@gmail.com>
2024-12-25 22:18:16 +0900

    implementing MToon1SingleShader.frag.glsl

--- src/webgl/shaderity_shaders/MToon1SingleShader/MToon1SingleShader.frag.glsl
+++ src/webgl/shaderity_shaders/MToon1SingleShader/MToon1SingleShader.frag.glsl
@@ -18,11 +18,44 @@ in vec4 v_position_inWorld;
 
 #pragma shaderity: require(../common/rt0.glsl)
 
-void main (){
+/* shaderity: @{getters} */
+
+#pragma shaderity: require(../common/opticalDefinition.glsl)
+
+// uniform sampler2D u_baseColorTexture; // initialValue=(1,white)
+// uniform vec4 u_baseColorFactor; // initialValue=(1,1,1,1)
+
+vec3 linearToSrgb(vec3 linearColor) {
+  return pow(linearColor, vec3(1.0/2.2));
+}
+
+vec3 srgbToLinear(vec3 srgbColor) {
+  return pow(srgbColor, vec3(2.2));
+}
+
+#pragma shaderity: require(../common/perturbedNormal.glsl)
+
+void main() {
+  #pragma shaderity: require(../common/mainPrerequisites.glsl)
+
+  // // main color
+  // vec4 baseColorTextureColor = texture(u_baseColorTexture, v_texcoord_0);
+  // vec4 baseColorFactor = get_baseColor(materialSID, 0);
+
+  // // alpha
+  // float alpha = baseColorTextureColor.a * baseColorFactor.a;
+  // #ifdef RN_ALPHATEST_ON
+  //   float cutoff = get_cutoff(materialSID, 0);
+  //   if(alpha < cutoff) discard;
+  // #endif
+
+  // rt0.w = alpha;
+
 #ifdef RN_MTOON_IS_OUTLINE
   rt0 = vec4(0.0, 0.0, 0.0, 1.0);
 #else
   rt0 = vec4(1.0, 0.0, 0.0, 1.0);
 #endif
+
   #pragma shaderity: require(../common/glFragColor.glsl)
 }
