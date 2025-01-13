3249095b73321c08f90209f07a6bda3f37aad127
Yuki Shimada <emadurandal@gmail.com>
2025-01-13 15:26:46 +0900

    enable tests

--- samples/test_e2e/PointLightShadowMap/test.js
+++ samples/test_e2e/PointLightShadowMap/test.js
@@ -1,6 +1,6 @@
 import { testCheckPtoDocument } from '../common/testFunc';
 const SetURL = 'http://localhost:8082/samples/test_e2e/PointLightShadowMap';
 
-test.skip('regression test PointLightShadowMap', async () => {
-  await testCheckPtoDocument(browser, SetURL, expect, 0.01, false, false);
+test('regression test PointLightShadowMap', async () => {
+  await testCheckPtoDocument(jest, browser, SetURL, expect, 0.01, false, false);
 });
