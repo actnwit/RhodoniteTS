a32d05df14f9da9e7b00d4a977f4612543d823cc
Yuki Shimada <emadurandal@gmail.com>
2025-01-16 23:31:38 +0900

    Refactor shadow mapping implementation and enhance sample structure
    - Added a new PointShadowMap class to encapsulate shadow mapping logic, improving code organization and reusability.
    - Updated main.ts to utilize the new PointShadowMap class, replacing the previous shadow map setup with a more modular approach.
    - Restored ignored files for sample scripts in .gitignore to ensure proper tracking and management.
    - Removed redundant shadow map setup code, streamlining the rendering process and enhancing maintainability.
    
    These changes improve the clarity and efficiency of the shadow mapping implementation in the rendering pipeline.

---
+++ samples/test_e2e/PointLightShadowMap/PointShadowMap.ts
@@ -0,0 +1,49 @@
+import Rn from '../../../dist/esmdev/index.js';
+
+export class PointShadowMap {
+  private __shadowMomentFramebuffer: Rn.FrameBuffer;
+  private __shadowMomentFrontMaterial: Rn.Material;
+  private __shadowMomentBackMaterial: Rn.Material;
+
+  constructor() {
+    this.__shadowMomentFramebuffer = Rn.RenderableHelper.createFrameBuffer({
+      width: 1024,
+      height: 1024,
+      textureNum: 1,
+      textureFormats: [Rn.TextureFormat.RGBA16F],
+      createDepthBuffer: true,
+      depthTextureFormat: Rn.TextureFormat.Depth32F,
+    });
+
+    this.__shadowMomentFrontMaterial =
+      Rn.MaterialHelper.createParaboloidDepthMomentEncodeMaterial();
+    this.__shadowMomentFrontMaterial.colorWriteMask = [true, true, false, false];
+
+    this.__shadowMomentBackMaterial = Rn.MaterialHelper.createParaboloidDepthMomentEncodeMaterial();
+    this.__shadowMomentBackMaterial.colorWriteMask = [false, false, true, true];
+    this.__shadowMomentBackMaterial.setParameter('frontHemisphere', false);
+  }
+
+  public getRenderPasses(entities: Rn.ISceneGraphEntity[]) {
+    const shadowMomentFrontRenderPass = new Rn.RenderPass();
+    shadowMomentFrontRenderPass.clearColor = Rn.Vector4.fromCopyArray([1, 1, 1, 1]);
+    shadowMomentFrontRenderPass.toClearColorBuffer = true;
+    shadowMomentFrontRenderPass.toClearDepthBuffer = true;
+    shadowMomentFrontRenderPass.addEntities(entities);
+    shadowMomentFrontRenderPass.setFramebuffer(this.__shadowMomentFramebuffer);
+    shadowMomentFrontRenderPass.setMaterial(this.__shadowMomentFrontMaterial);
+
+    const shadowMomentBackRenderPass = new Rn.RenderPass();
+    shadowMomentBackRenderPass.toClearColorBuffer = false;
+    shadowMomentBackRenderPass.toClearDepthBuffer = true;
+    shadowMomentBackRenderPass.addEntities(entities);
+    shadowMomentBackRenderPass.setFramebuffer(this.__shadowMomentFramebuffer);
+    shadowMomentBackRenderPass.setMaterial(this.__shadowMomentBackMaterial);
+
+    return [shadowMomentFrontRenderPass, shadowMomentBackRenderPass];
+  }
+
+  public getShadowMomentFramebuffer() {
+    return this.__shadowMomentFramebuffer;
+  }
+}
