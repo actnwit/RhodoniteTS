2440bafe871555863d0eaf0cb3c280277bde3eae
Yuki Shimada <emadurandal@gmail.com>
2025-04-30 00:11:48 +0900

    simplify src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
-// ① 中身を取得
-import * as RnNS from './import';
-
-// ② named export をそのまま流す
-export * from './import';
-
-// ③ **静的に** default を宣言  ←★ポイント
-export { RnNS as default };
+import * as Rn from './import';
+export default Rn;
