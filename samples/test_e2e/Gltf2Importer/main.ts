5b06738880b80983a8b13448e76ed0200296efca
Yuki Shimada <emadurandal@gmail.com>
2025-05-21 02:21:56 +0900

    fix: improve Gltf2Importer sample

--- samples/test_e2e/Gltf2Importer/main.ts
+++ samples/test_e2e/Gltf2Importer/main.ts
@@ -13,27 +13,27 @@ await Rn.System.init({
 // Camera
 const cameraEntity = Rn.createCameraControllerEntity();
 const cameraComponent = cameraEntity.getCamera();
-//cameraComponent.type = Rn.CameraTyp]e.Orthographic;
 cameraComponent.zNear = 0.1;
 cameraComponent.zFar = 1000;
 cameraComponent.setFovyAndChangeFocalLength(45);
 cameraComponent.aspect = 1;
-cameraEntity.getTransform().localPosition = Rn.Vector3.fromCopyArray([0.0, 0, 0.5]);
+cameraEntity.position = Rn.Vector3.fromCopyArray([0.0, 0, 0.5]);
 
 // Lights
 const lightEntity = Rn.createLightEntity();
-lightEntity.getTransform().localPosition = Rn.Vector3.fromCopyArray([1.0, 1.0, 100000.0]);
 lightEntity.getLight().color = Rn.Vector3.fromCopyArray([1, 1, 1]);
 lightEntity.getLight().type = Rn.LightType.Directional;
-//lightEntity2.getLight().type = Rn.LightType.Directional;
 
 const response = await Rn.Gltf2Importer.importFromUri(
   '../../../assets/gltf/glTF-Sample-Assets/Models/BoxAnimated/glTF-Binary/BoxAnimated.glb'
 );
-//---------------------------
+
+if (response.isErr()) {
+  console.error('Failed to import glTF-2 model', response.getRnError());
+}
+
 const rootGroup = Rn.ModelConverter.convertToRhodoniteObject(response.unwrapForce());
-//rootGroup.getTransform().localPosition = Rn.Vector3.fromCopyArray([1.0, 0, 0]);
-rootGroup.getTransform().localEulerAngles = Rn.Vector3.fromCopyArray([0, 1.0, 0.0]);
+rootGroup.localEulerAngles = Rn.Vector3.fromCopyArray([0, 1.0, 0.0]);
 
 // CameraComponent
 const cameraControllerComponent = cameraEntity.getCameraController();
@@ -51,9 +51,8 @@ expression.addRenderPasses([renderPass]);
 
 Rn.CameraComponent.current = 0;
 let startTime = Date.now();
-const rotationVec3 = Rn.MutableVector3.one();
 let count = 0;
-const draw = function () {
+Rn.System.startRenderLoop(() => {
   if (p == null && count > 0) {
     p = document.createElement('p');
     p.setAttribute('id', 'rendered');
@@ -63,28 +62,16 @@ const draw = function () {
 
   if (window.isAnimating) {
     const date = new Date();
-    const rotation = 0.001 * (date.getTime() - startTime);
-    //rotationVec3._v[0] = 0.1;
-    //rotationVec3._v[1] = rotation;
-    //rotationVec3._v[2] = 0.1;
     const time = (date.getTime() - startTime) / 1000;
     Rn.AnimationComponent.globalTime = time;
     if (time > Rn.AnimationComponent.endInputValue) {
       startTime = date.getTime();
     }
-    //console.log(time);
-    //      rootGroup.getTransform().scale = rotationVec3;
-    //rootGroup.getTransform().localPosition = rootGroup.getTransform().localPosition;
   }
 
   Rn.System.process([expression]);
   count++;
-
-  requestAnimationFrame(draw);
-};
-
-draw();
-//-----------------
+});
 
 //---------------------
 window.exportGltf2 = function () {
