5f0a36c3a3277bd820cfb7525cb8830a5edda60f
Yuki Shimada <emadurandal@gmail.com>
2025-05-26 15:45:12 +0900

    add parameters to VRMC_vrm_animation

--- src/types/VRMA.ts
+++ src/types/VRMA.ts
@@ -23,12 +23,35 @@ export type HumanBoneNames =
 
 export type NodeId = number;
 
+export type ExpressionPreset =
+  | 'happy'
+  | 'angry'
+  | 'sad'
+  | 'relaxed'
+  | 'surprised'
+  | 'aa'
+  | 'ih'
+  | 'ou'
+  | 'ee'
+  | 'oh'
+  | 'blink'
+  | 'blinkLeft'
+  | 'blinkRight'
+  | 'neutral';
 export interface VRMC_vrm_animation {
   specVersion: string;
   humanoid?: {
     humanBones: Record<HumanBoneNames, { node: number }>;
   };
-};
+  expressions?: {
+    preset?: Record<ExpressionPreset, { node: number }>;
+    custom?: Record<string, { node: number }>;
+  };
+  lookAt?: {
+    node: number;
+    offsetFromHeadBone?: [number, number, number];
+  };
+}
 
 export interface RnM2_VRMC_vrm_animation extends VRMC_vrm_animation {
   humanoidBoneNameMap?: Map<NodeId, HumanBoneNames>;
