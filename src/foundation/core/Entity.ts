d0681f312842f55b71e22a2182f665a43c5e5f19
Yuki Shimada <emadurandal@gmail.com>
2025-06-03 22:29:17 +0900

    add comments to Entity

--- src/foundation/core/Entity.ts
+++ src/foundation/core/Entity.ts
@@ -26,26 +26,137 @@ export interface IEntity extends IRnObject {
   entityUID: EntityUID;
   _isAlive: boolean;
   _myLatestCopyEntityUID: EntityUID;
+
+  /**
+   * Retrieves a component of the specified type from this entity.
+   * @param componentType - The constructor/class of the component to retrieve
+   * @returns The component instance if found, undefined otherwise
+   */
   getComponent(componentType: typeof Component): Component | undefined;
+
+  /**
+   * Retrieves a component by its unique component type identifier.
+   * @param componentTID - The component type identifier
+   * @returns The component instance if found, undefined otherwise
+   */
   getComponentByComponentTID(componentTID: ComponentTID): Component | undefined;
+
+  /**
+   * Sets a component on this entity.
+   * @param componentType - The constructor/class of the component
+   * @param com - The component instance to set
+   * @internal
+   */
   _setComponent(componentType: typeof Component, com: Component): void;
+
+  /**
+   * Checks whether this entity has a component of the specified type.
+   * @param componentType - The constructor/class of the component to check
+   * @returns True if the entity has the component, false otherwise
+   */
   hasComponent(componentType: typeof Component): boolean;
+
+  /**
+   * Removes a component from this entity.
+   * @param componentTID - The component type identifier to remove
+   * @internal
+   */
   _removeComponent(componentTID: ComponentTID): void;
+
+  /**
+   * Attempts to retrieve the BlendShapeComponent from this entity.
+   * @returns The BlendShapeComponent if present, undefined otherwise
+   */
   tryToGetBlendShape(): BlendShapeComponent | undefined;
+
+  /**
+   * Attempts to retrieve the CameraComponent from this entity.
+   * @returns The CameraComponent if present, undefined otherwise
+   */
   tryToGetCamera(): CameraComponent | undefined;
+
+  /**
+   * Attempts to retrieve the CameraControllerComponent from this entity.
+   * @returns The CameraControllerComponent if present, undefined otherwise
+   */
   tryToGetCameraController(): CameraControllerComponent | undefined;
+
+  /**
+   * Attempts to retrieve the LightComponent from this entity.
+   * @returns The LightComponent if present, undefined otherwise
+   */
   tryToGetLight(): LightComponent | undefined;
+
+  /**
+   * Attempts to retrieve the MeshComponent from this entity.
+   * @returns The MeshComponent if present, undefined otherwise
+   */
   tryToGetMesh(): MeshComponent | undefined;
+
+  /**
+   * Attempts to retrieve the MeshRendererComponent from this entity.
+   * @returns The MeshRendererComponent if present, undefined otherwise
+   */
   tryToGetMeshRenderer(): MeshRendererComponent | undefined;
+
+  /**
+   * Attempts to retrieve the PhysicsComponent from this entity.
+   * @returns The PhysicsComponent if present, undefined otherwise
+   */
   tryToGetPhysics(): PhysicsComponent | undefined;
+
+  /**
+   * Attempts to retrieve the SceneGraphComponent from this entity.
+   * @returns The SceneGraphComponent if present, undefined otherwise
+   */
   tryToGetSceneGraph(): SceneGraphComponent | undefined;
+
+  /**
+   * Attempts to retrieve the SkeletalComponent from this entity.
+   * @returns The SkeletalComponent if present, undefined otherwise
+   */
   tryToGetSkeletal(): SkeletalComponent | undefined;
+
+  /**
+   * Attempts to retrieve the TransformComponent from this entity.
+   * @returns The TransformComponent if present, undefined otherwise
+   */
   tryToGetTransform(): TransformComponent | undefined;
+
+  /**
+   * Attempts to retrieve the AnimationComponent from this entity.
+   * @returns The AnimationComponent if present, undefined otherwise
+   */
   tryToGetAnimation(): AnimationComponent | undefined;
+
+  /**
+   * Attempts to retrieve the AnimationStateComponent from this entity.
+   * @returns The AnimationStateComponent if present, undefined otherwise
+   */
   tryToGetAnimationState(): AnimationStateComponent | undefined;
+
+  /**
+   * Attempts to retrieve the VrmComponent from this entity.
+   * @returns The VrmComponent if present, undefined otherwise
+   */
   tryToGetVrm(): VrmComponent | undefined;
+
+  /**
+   * Attempts to retrieve the ConstraintComponent from this entity.
+   * @returns The ConstraintComponent if present, undefined otherwise
+   */
   tryToGetConstraint(): ConstraintComponent | undefined;
+
+  /**
+   * Attempts to retrieve the EffekseerComponent from this entity.
+   * @returns The EffekseerComponent if present, undefined otherwise
+   */
   tryToGetEffekseer(): EffekseerComponent | undefined;
+
+  /**
+   * Destroys this entity and releases all associated resources.
+   * @internal
+   */
   _destroy(): void;
 }
 
@@ -71,15 +182,15 @@ export class Entity extends RnObject implements IEntity {
   _isAlive: boolean;
 
   /**
-   * The constructor of the Entity class.
+   * Creates a new Entity instance.
    *
    * @remarks
    * When creating an Entity, use the createEntity method of the EntityRepository class
    * instead of directly calling this constructor.
    *
-   * @param entityUID - The unique ID of the entity
-   * @param isAlive - Whether this entity alive or not
-   * @param entityComponent - The instance of EntityComponent (Dependency Injection)
+   * @param entityUID - The unique identifier for this entity
+   * @param isAlive - Whether this entity is alive or not
+   * @param components - Optional map of existing components to initialize with
    */
   constructor(entityUID: EntityUID, isAlive: boolean, components?: Map<ComponentTID, Component>) {
     super();
@@ -90,15 +201,22 @@ export class Entity extends RnObject implements IEntity {
   }
 
   /**
-   * Get Unique ID of the entity.
+   * Gets the unique identifier of this entity.
+   * @returns The entity's unique ID
    */
   get entityUID() {
     return this.___entity_uid;
   }
 
   /**
-   * Sets a component to this entity.
-   * @param component The component to set.
+   * Sets a component on this entity.
+   *
+   * @remarks
+   * This method associates a component instance with this entity using the component's type ID.
+   * If a component of the same type already exists, it will be replaced.
+   *
+   * @param componentType - The constructor/class of the component to set
+   * @param component - The component instance to attach to this entity
    *
    * @internal
    */
@@ -107,32 +225,39 @@ export class Entity extends RnObject implements IEntity {
   }
 
   /**
-   * return whether this entity has the component or not
-   * @param componentType - The component to check
-   * @returns
+   * Checks whether this entity has a component of the specified type.
+   *
+   * @param componentType - The constructor/class of the component to check for
+   * @returns True if the entity has a component of the specified type, false otherwise
    */
   hasComponent(componentType: typeof Component): boolean {
     return this.__components.has(componentType.componentTID);
   }
 
   /**
-   * Get the component of the specified type that the entity has
-   * @param componentType
+   * Retrieves a component of the specified type from this entity.
+   *
+   * @param componentType - The constructor/class of the component to retrieve
+   * @returns The component instance if found, undefined if the entity doesn't have this component type
    */
   getComponent(componentType: typeof Component): Component | undefined {
     return this.__components.get(componentType.componentTID);
   }
 
   /**
-   * Gets the component corresponding to the ComponentTID.
-   * @param componentTID - The ComponentTID to get the component.
+   * Retrieves a component by its unique component type identifier.
+   *
+   * @param componentTID - The component type identifier to look up
+   * @returns The component instance if found, undefined otherwise
    */
   getComponentByComponentTID(componentTID: ComponentTID): Component | undefined {
     return this.__components.get(componentTID);
   }
 
   /**
-   * @param componentTID
+   * Removes a component from this entity by its component type identifier.
+   *
+   * @param componentTID - The component type identifier of the component to remove
    *
    * @internal
    */
@@ -145,8 +270,13 @@ export class Entity extends RnObject implements IEntity {
   ///
 
   /**
-   * try to get an Animation Component if the entity has it.
-   * @returns AnimationComponent | undefined
+   * Attempts to retrieve the AnimationComponent from this entity.
+   *
+   * @remarks
+   * This is a convenience method that provides type-safe access to the AnimationComponent
+   * without requiring explicit type casting.
+   *
+   * @returns The AnimationComponent if this entity has one, undefined otherwise
    */
   tryToGetAnimation() {
     return this.getComponentByComponentTID(WellKnownComponentTIDs.AnimationComponentTID) as
@@ -154,84 +284,210 @@ export class Entity extends RnObject implements IEntity {
       | undefined;
   }
 
+  /**
+   * Attempts to retrieve the AnimationStateComponent from this entity.
+   *
+   * @remarks
+   * This is a convenience method that provides type-safe access to the AnimationStateComponent
+   * without requiring explicit type casting.
+   *
+   * @returns The AnimationStateComponent if this entity has one, undefined otherwise
+   */
   tryToGetAnimationState() {
     return this.getComponentByComponentTID(WellKnownComponentTIDs.AnimationStateComponentTID) as
       | AnimationStateComponent
       | undefined;
   }
 
+  /**
+   * Attempts to retrieve the BlendShapeComponent from this entity.
+   *
+   * @remarks
+   * This is a convenience method that provides type-safe access to the BlendShapeComponent
+   * without requiring explicit type casting.
+   *
+   * @returns The BlendShapeComponent if this entity has one, undefined otherwise
+   */
   tryToGetBlendShape() {
     return this.getComponentByComponentTID(WellKnownComponentTIDs.BlendShapeComponentTID) as
       | BlendShapeComponent
       | undefined;
   }
 
+  /**
+   * Attempts to retrieve the CameraComponent from this entity.
+   *
+   * @remarks
+   * This is a convenience method that provides type-safe access to the CameraComponent
+   * without requiring explicit type casting.
+   *
+   * @returns The CameraComponent if this entity has one, undefined otherwise
+   */
   tryToGetCamera() {
     return this.getComponentByComponentTID(WellKnownComponentTIDs.CameraComponentTID) as
       | CameraComponent
       | undefined;
   }
 
+  /**
+   * Attempts to retrieve the CameraControllerComponent from this entity.
+   *
+   * @remarks
+   * This is a convenience method that provides type-safe access to the CameraControllerComponent
+   * without requiring explicit type casting.
+   *
+   * @returns The CameraControllerComponent if this entity has one, undefined otherwise
+   */
   tryToGetCameraController() {
     return this.getComponentByComponentTID(WellKnownComponentTIDs.CameraControllerComponentTID) as
       | CameraControllerComponent
       | undefined;
   }
 
+  /**
+   * Attempts to retrieve the LightComponent from this entity.
+   *
+   * @remarks
+   * This is a convenience method that provides type-safe access to the LightComponent
+   * without requiring explicit type casting.
+   *
+   * @returns The LightComponent if this entity has one, undefined otherwise
+   */
   tryToGetLight() {
     return this.getComponentByComponentTID(WellKnownComponentTIDs.LightComponentTID) as
       | LightComponent
       | undefined;
   }
 
+  /**
+   * Attempts to retrieve the MeshComponent from this entity.
+   *
+   * @remarks
+   * This is a convenience method that provides type-safe access to the MeshComponent
+   * without requiring explicit type casting.
+   *
+   * @returns The MeshComponent if this entity has one, undefined otherwise
+   */
   tryToGetMesh() {
     return this.getComponentByComponentTID(WellKnownComponentTIDs.MeshComponentTID) as
       | MeshComponent
       | undefined;
   }
 
+  /**
+   * Attempts to retrieve the MeshRendererComponent from this entity.
+   *
+   * @remarks
+   * This is a convenience method that provides type-safe access to the MeshRendererComponent
+   * without requiring explicit type casting.
+   *
+   * @returns The MeshRendererComponent if this entity has one, undefined otherwise
+   */
   tryToGetMeshRenderer() {
     return this.getComponentByComponentTID(WellKnownComponentTIDs.MeshRendererComponentTID) as
       | MeshRendererComponent
       | undefined;
   }
 
+  /**
+   * Attempts to retrieve the PhysicsComponent from this entity.
+   *
+   * @remarks
+   * This is a convenience method that provides type-safe access to the PhysicsComponent
+   * without requiring explicit type casting.
+   *
+   * @returns The PhysicsComponent if this entity has one, undefined otherwise
+   */
   tryToGetPhysics() {
     return this.getComponentByComponentTID(WellKnownComponentTIDs.PhysicsComponentTID) as
       | PhysicsComponent
       | undefined;
   }
 
+  /**
+   * Attempts to retrieve the SceneGraphComponent from this entity.
+   *
+   * @remarks
+   * This is a convenience method that provides type-safe access to the SceneGraphComponent
+   * without requiring explicit type casting.
+   *
+   * @returns The SceneGraphComponent if this entity has one, undefined otherwise
+   */
   tryToGetSceneGraph() {
     return this.getComponentByComponentTID(WellKnownComponentTIDs.SceneGraphComponentTID) as
       | SceneGraphComponent
       | undefined;
   }
 
+  /**
+   * Attempts to retrieve the SkeletalComponent from this entity.
+   *
+   * @remarks
+   * This is a convenience method that provides type-safe access to the SkeletalComponent
+   * without requiring explicit type casting.
+   *
+   * @returns The SkeletalComponent if this entity has one, undefined otherwise
+   */
   tryToGetSkeletal() {
     return this.getComponentByComponentTID(WellKnownComponentTIDs.SkeletalComponentTID) as
       | SkeletalComponent
       | undefined;
   }
 
+  /**
+   * Attempts to retrieve the TransformComponent from this entity.
+   *
+   * @remarks
+   * This is a convenience method that provides type-safe access to the TransformComponent
+   * without requiring explicit type casting.
+   *
+   * @returns The TransformComponent if this entity has one, undefined otherwise
+   */
   tryToGetTransform() {
     return this.getComponentByComponentTID(WellKnownComponentTIDs.TransformComponentTID) as
       | TransformComponent
       | undefined;
   }
 
+  /**
+   * Attempts to retrieve the VrmComponent from this entity.
+   *
+   * @remarks
+   * This is a convenience method that provides type-safe access to the VrmComponent
+   * without requiring explicit type casting.
+   *
+   * @returns The VrmComponent if this entity has one, undefined otherwise
+   */
   tryToGetVrm() {
     return this.getComponentByComponentTID(WellKnownComponentTIDs.VrmComponentTID) as
       | VrmComponent
       | undefined;
   }
 
+  /**
+   * Attempts to retrieve the ConstraintComponent from this entity.
+   *
+   * @remarks
+   * This is a convenience method that provides type-safe access to the ConstraintComponent
+   * without requiring explicit type casting.
+   *
+   * @returns The ConstraintComponent if this entity has one, undefined otherwise
+   */
   tryToGetConstraint() {
     return this.getComponentByComponentTID(WellKnownComponentTIDs.ConstraintComponentTID) as
       | ConstraintComponent
       | undefined;
   }
 
+  /**
+   * Attempts to retrieve the EffekseerComponent from this entity.
+   *
+   * @remarks
+   * This is a convenience method that provides type-safe access to the EffekseerComponent
+   * without requiring explicit type casting.
+   *
+   * @returns The EffekseerComponent if this entity has one, undefined otherwise
+   */
   tryToGetEffekseer() {
     return this.getComponentByComponentTID(WellKnownComponentTIDs.EffekseerComponentTID) as
       | EffekseerComponent
@@ -239,7 +495,12 @@ export class Entity extends RnObject implements IEntity {
   }
 
   /**
-   * Mark the entity as destroyed
+   * Destroys this entity and releases all associated resources.
+   *
+   * @remarks
+   * This method calls the destroy method on all components attached to this entity
+   * and marks the entity as no longer alive. After calling this method, the entity
+   * should not be used.
    */
   _destroy() {
     this.__components.forEach((component) => {
