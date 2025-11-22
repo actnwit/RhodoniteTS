55c93cb23fcd5b0bf3a05c4b8a62667ac420f02e
Yuki Shimada <emadurandal@gmail.com>
2025-11-22 17:22:14 +0900

    feat: Enhance morph target processing in WebGL shaders
    - Updated vertex shader functions to incorporate blend shape component IDs for improved morph target handling.
    - Refactored position calculation logic to utilize uniform buffers for morph offsets and weights, enhancing animation fidelity.
    - Added new uniform for current primitive index to facilitate dynamic morph target updates based on the active primitive.

--- src/webgl/shaderity_shaders/common/morphVariables.glsl
+++ src/webgl/shaderity_shaders/common/morphVariables.glsl
@@ -1,5 +1,6 @@
 #ifdef RN_IS_MORPHING
 uniform int u_morphTargetNumber; // initialValue=0, isInternalSetting=true, soloDatum=false, needUniformInDataTextureMode=true
+uniform int u_currentPrimitiveIdx; // initialValue=0, isInternalSetting=true, soloDatum=false, needUniformInDataTextureMode=true
 // uniform int u_dataTextureMorphOffsetPosition[];
 // uniform int u_morphWeights[]; //
 #endif
