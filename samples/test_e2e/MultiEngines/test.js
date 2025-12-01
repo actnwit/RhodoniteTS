949cc00dc2425edcd2835d2e66595153b4d41b37
Yuki Shimada <emadurandal@gmail.com>
2025-12-01 19:54:41 +0900

    feat: Add MultiEngines sample for enhanced rendering tests
    - Introduced a new sample for MultiEngines, including HTML and TypeScript files to facilitate rendering tests with multiple canvases.
    - Updated sample-list.json to include the new MultiEngines entry, improving the variety of available samples.
    - Added regression test for GltfImporter to ensure functionality and reliability.
    - Included a correct PNG image snapshot for visual comparison in tests.

---
+++ samples/test_e2e/MultiEngines/test.js
@@ -0,0 +1,6 @@
+import { testCheckPtoDocument } from '../common/testFunc';
+const SetURL = 'http://localhost:8082/samples/test_e2e/GltfImporter';
+
+test('regression test GltfImporter', async () => {
+  await testCheckPtoDocument(browser, SetURL, expect, 0.03);
+});
