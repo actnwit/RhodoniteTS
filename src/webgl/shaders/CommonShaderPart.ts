1cf0eb93e089a9baa53b9ac3c0790763c468c422
Yuki Shimada <emadurandal@gmail.com>
2025-12-07 06:04:37 +0900

    refactor: Enhance varying declaration handling in CommonShaderPart
    - Introduced a definedVaryings set to prevent duplicate varying declarations in vertex and fragment shaders.
    - Updated varying name generation to include outputNameOfPrev for improved uniqueness and clarity.

--- src/webgl/shaders/CommonShaderPart.ts
+++ src/webgl/shaders/CommonShaderPart.ts
@@ -151,6 +151,7 @@ uniform bool u_vertexAttributesExistenceArray[${VertexAttribute.AttributeTypeNum
       type: string;
       name: string;
     }[] = [];
+    const definedVaryings: Set<string> = new Set();
     for (let i = 0; i < shaderNodes.length; i++) {
       const shaderNode = shaderNodes[i];
       for (let j = 0; j < shaderNode.inputConnections.length; j++) {
@@ -162,9 +163,14 @@ uniform bool u_vertexAttributesExistenceArray[${VertexAttribute.AttributeTypeNum
         const inputNode = AbstractShaderNode.getShaderNodeByUid(inputConnection.shaderNodeUid);
         if (inputNode.getShaderStage() === 'Vertex' && shaderNode.getShaderStage() === 'Fragment') {
           const type = input.compositionType.toWGSLType(input.componentType);
+          const name = `${inputNode.shaderFunctionName}_${inputNode.shaderNodeUid}_${inputConnection.outputNameOfPrev}`;
+          if (definedVaryings.has(name)) {
+            continue;
+          }
+          definedVaryings.add(name);
           varyings.push({
             type: type,
-            name: `${inputNode.shaderFunctionName}_${inputNode.shaderNodeUid}`,
+            name: name,
           });
         }
       }
