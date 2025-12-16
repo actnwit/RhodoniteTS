c5ffd7b5924a73e30760ece33a56c53db2da0fe1
Yuki Shimada <emadurandal@gmail.com>
2025-12-16 23:40:41 +0900

    feat: enhance AttributeTexcoordShaderNode to support multiple texture coordinate indices
    - Updated AttributeTexcoordShaderNode to include a new input socket for texcoordIndex, allowing selection of different texture coordinate attributes.
    - Modified GLSL and WGSL shader functions to handle multiple texture coordinate indices, improving flexibility in shader usage.

--- src/foundation/materials/nodes/AttributeTexcoordShaderNode.ts
+++ src/foundation/materials/nodes/AttributeTexcoordShaderNode.ts
@@ -2,7 +2,9 @@ import AttributeTexcoordShaderityObjectGLSL from '../../../webgl/shaderity_shade
 import AttributeTexcoordShaderityObjectWGSL from '../../../webgpu/shaderity_shaders/nodes/AttributeTexcoord.vert.wgsl';
 import { ComponentType } from '../../definitions/ComponentType';
 import { CompositionType } from '../../definitions/CompositionType';
+import { Scalar } from '../../math/Scalar';
 import { AbstractShaderNode } from '../core/AbstractShaderNode';
+import { Socket } from '../core/Socket';
 
 /**
  * A shader node that provides texture coordinate attributes from vertex data.
@@ -23,6 +25,10 @@ export class AttributeTexcoordShaderNode extends AbstractShaderNode {
     });
 
     this.setShaderStage('Vertex');
+
+    this.__inputs.push(
+      new Socket('texcoordIndex', CompositionType.Scalar, ComponentType.UnsignedInt, Scalar.fromCopyNumber(0))
+    );
 
     this.__outputs.push({
       compositionType: CompositionType.Vec3,
--- src/webgl/shaderity_shaders/nodes/AttributeTexcoord.vert
+++ src/webgl/shaderity_shaders/nodes/AttributeTexcoord.vert
@@ -1,4 +1,10 @@
 
-void attributeTexcoord(out vec2 outValue) {
-  outValue = a_texcoord_0;
+void attributeTexcoord(in uint texcoordIndex, out vec2 outValue) {
+  if(texcoordIndex == 0u) {
+    outValue = a_texcoord_0;
+  } else if(texcoordIndex == 1u) {
+    outValue = a_texcoord_1;
+  } else {
+    outValue = a_texcoord_2;
+  }
 }
