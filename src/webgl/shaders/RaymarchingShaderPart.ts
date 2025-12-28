eec93089cc6adeb060a2718f20ba85f27e8b423c
Yuki Shimada <emadurandal@gmail.com>
2025-12-28 09:16:16 +0900

    refactor: standardize transformation matrix initialization in RaymarchingShaderPart
    - Updated the initialization of transformation and inverse matrices to use uniform values for clarity and consistency.
    - Adjusted the assignment of rotation values in the inverse transformation to ensure correct negation, enhancing the accuracy of transformation calculations.

--- src/webgl/shaders/RaymarchingShaderPart.ts
+++ src/webgl/shaders/RaymarchingShaderPart.ts
@@ -296,7 +296,7 @@ fn rotateXYZ(euler: vec3<f32>) -> mat3x3<f32> {
 
 fn createTransformMatrix(position: vec3<f32>, rotation: vec3<f32>, scale: vec3<f32>) -> mat4x4<f32> {
   let rot = rotateXYZ(rotation);
-  var transform = mat4x4f(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0);
+  var transform = mat4x4f(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
 
   transform[0] = vec4f(rot[0]/scale.x, transform[0].w);
   transform[1] = vec4f(rot[1]/scale.y, transform[1].w);
@@ -308,7 +308,7 @@ fn createTransformMatrix(position: vec3<f32>, rotation: vec3<f32>, scale: vec3<f
 }
 
 fn inverseTransform(m: mat4x4<f32>) -> mat4x4<f32> {
-  var inv_rot_scale = mat3x3f(
+  let inv_rot_scale = mat3x3f(
       vec3f(-m[0].x,-m[1].x,-m[2].x),
       vec3f(-m[0].y,-m[1].y,-m[2].y),
       vec3f(-m[0].z,-m[1].z,-m[2].z)
@@ -316,10 +316,10 @@ fn inverseTransform(m: mat4x4<f32>) -> mat4x4<f32> {
 
   let inv_translation = inv_rot_scale*m[3].xyz;
 
-  var inv = mat4x4f(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0);
-  inv[0] = vec4f(inv_rot_scale[0], inv[0].w);
-  inv[1] = vec4f(inv_rot_scale[1], inv[1].w);
-  inv[2] = vec4f(inv_rot_scale[2], inv[2].w);
+  var inv = mat4x4f(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+  inv[0] = vec4f(-inv_rot_scale[0], inv[0].w);
+  inv[1] = vec4f(-inv_rot_scale[1], inv[1].w);
+  inv[2] = vec4f(-inv_rot_scale[2], inv[2].w);
   inv[3] = vec4f(inv_translation, inv[3].w);
 
   return inv;
