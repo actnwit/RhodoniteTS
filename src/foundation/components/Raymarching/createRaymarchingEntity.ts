8a81cecc88b3b0ad77bb81033dcc60ff0bc0286d
Yuki Shimada <emadurandal@gmail.com>
2025-12-29 16:32:32 +0900

    refactor: replace mesh entity creation with group entity in raymarching
    - Updated the createRaymarchingEntity function to utilize createGroupEntity instead of createMeshEntity, aligning with the new entity structure.
    - This change enhances the organization of entities within the scene graph, improving overall architecture and maintainability.

--- src/foundation/components/Raymarching/createRaymarchingEntity.ts
+++ src/foundation/components/Raymarching/createRaymarchingEntity.ts
@@ -1,10 +1,10 @@
 import type { IRaymarchingEntity } from '../../helpers/EntityHelper';
 import type { Engine } from '../../system/Engine';
-import { createMeshEntity } from '../MeshRenderer/createMeshEntity';
+import { createGroupEntity } from '../SceneGraph/createGroupEntity';
 import { WellKnownComponentTIDs } from '../WellKnownComponentTIDs';
 
 export function createRaymarchingEntity(engine: Engine): IRaymarchingEntity {
-  const entity = createMeshEntity(engine);
+  const entity = createGroupEntity(engine);
   const entityAddedComponent = engine.entityRepository.tryToAddComponentToEntityByTID(
     WellKnownComponentTIDs.RaymarchingComponentTID,
     entity
